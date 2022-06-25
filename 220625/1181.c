7bdb90f8e2fdb09c9be231310981c7e8946f8975
suyeon woo <woosean999@gmail.com>
2022-06-25 15:06:12 +0900

    1181 - 바로 전 단어와 비교 / 시간초과

--- 220625/1181.c
+++ 220625/1181.c
@@ -11,25 +11,30 @@ int len(char *str)
     return (i);
 }
 
-int is_in(int *arr, char *str)
+int compare(char *s1, char *s2)
 {
-    for (int i = 0; i < 51; i += len(str))
+    if (len(s1) < len(s2))
+        return (1);
+    else if (len(s1) > len(s2))
+        return (-1);
+    else 
     {
-        for (int j = 0; j < len(str); ++j)
+        for (int i = 0; i < len(s1); ++i)
         {
-            if (arr[i + j] != str[j])
-                continue;
-            if (j == len(str) - 1)
+            if (s1[i] > s2[i])
+                return (-1);
+            else if (s1[i] < s2[i])
                 return (1);
         }
     }
-    return (0);
+    return (0);  // 완전히 동일하므로 출력하지 말아야 함
 }
 
 int main(void)
 {
     int cnt;
-    char word[51] = {0,};
+    char prevWord[51] = {0, };
+    char currentWord[51] = {0,};
     int **arr;
     
     arr = (int **)malloc(sizeof(int *) * 51);
@@ -44,19 +49,46 @@ int main(void)
     scanf("%d", &cnt);
     for (int i = 0; i < cnt; ++i)
     {
-        scanf("%s", word);
-
-        if (is_in(arr[len(word)], word))
-            continue;
-        int k = 0;
-        while (arr[len(word)][k])
-            ++k;
-        for (int j = 0; j < len(word); ++j)
+        scanf("%s", currentWord);
+        if (i == 0)
         {
-            arr[len(word)][k] = word[j];
-            ++k;
+            for (int j = 0; j < len(currentWord); ++j)
+            {
+                arr[len(currentWord)][j] = currentWord[j];
+            }
         }
-    }
+        else
+        {
+            printf("%d", i);
+            if (compare(prevWord, currentWord) == 1)
+            {
+                int k = 0;
+                while (arr[len(currentWord)][k])
+                    ++k;
+                for (int j = 0; j < len(currentWord); ++j)
+                {
+                    arr[len(currentWord)][j + k] = currentWord[j];
+                }
+            }
+            else if (compare(prevWord, currentWord) == -1)
+            {
+                int k = 0;
+                while (arr[len(currentWord)][k])
+                    ++k;
+                for (int j = 0; j < len(currentWord); ++j)
+                {
+                    arr[len(currentWord)][j+k] = arr[len(currentWord)][j+k - len(currentWord)];
+                    arr[len(currentWord)][j+k - len(currentWord)] = currentWord[j];
+                }
+            }
+            else
+                continue;
+        }
+        for (int i = 0; i < len(currentWord); ++i)
+        {
+            prevWord[i] = currentWord[i];
+        }
+    }  
     for (int i = 0; i < 51; ++i)
     {
         int k = 0;
