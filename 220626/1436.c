a9d3a259f57bbe6c60824a4834084914f7d54dc2
suyeon woo <woosean999@gmail.com>
2022-06-26 22:35:05 +0900

    1436 - 영화감독 숌 / 틀린이유 파악중

---
+++ 220626/1436.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+int tri_six(int num)
+{
+    int cnt;
+    
+    cnt = 0;
+    while (num >= 10)
+    {
+        if (num % 10 == 6)
+            ++cnt;
+        else 
+            cnt = 0;
+        num /= 10;
+        if (cnt == 3)
+            return (1);
+    }
+    if (num == 6)
+        ++cnt;    
+    if (cnt < 3)
+        return (0);
+}
+
+int main(void) {
+    int n;
+    int i;
+    int serial;
+
+    i = 0;
+    serial = 0;
+    scanf("%d", &n);
+    while (1)
+    {
+        if (tri_six(i))
+        {
+            --n;
+            serial = i;
+        }
+        if (n == 0)
+            break;
+        ++i;
+    }
+    printf("%d", serial);
+    return 0;
+}
