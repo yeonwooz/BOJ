e8a7595ab6a6c0bd348d1c7d14b3053f0b6f7edf
suyeon woo <woosean999@gmail.com>
2022-06-27 09:19:09 +0900

    1330 - 두 수 비교하기

---
+++ 220627/1330.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a;
+    int b;
+
+    scanf("%d %d", &a, &b);
+    if (a > b)
+        printf(">");
+    else if (a < b)
+        printf("<"); 
+    else
+        printf("==");
+}
