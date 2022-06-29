1369d1efa14d90093fbd0026275e0c23b5f9e300
suyeon woo <woosean999@gmail.com>
2022-06-29 11:07:38 +0900

    15829 Hasing - 해시값과 계수를 long long 타입으로 변경, 계수 제곱 후 모듈러 연산하여 해시에 곱하는 프로세스 추가

--- 220629/15829.c
+++ 220629/15829.c
@@ -1,33 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define R 31
+#define M 1234567891
 
 int main(void)
 {
     int L;
-    char *str;
-    int H;
-    const int R = 31;
-    const int M = 1234567891;
-    int temp;
+    char str[51];
+    long long H;
+    long long r;
 
     scanf("%d", &L);
-    str = (char *)malloc(sizeof(char) * (L + 1));
     scanf("%s", str);
     str[L+1] = '\0';
     
-    for (int i = 0; i < L; ++i)
-    {
-        str[i] -= 96;
-    }
+    r = 1;
     H = 0;
     for (int i = 0; i < L; ++i)
     {
-        temp = str[i] % M;
-        for (int j = 0; j < i; ++j)
-        {
-            temp *= R % M;
-        }
-        H += temp % M;
+        H += ((str[i] - 'a' + 1) * r) % M;
+        H %= M;
+        r *= R;
+        r %= M;
     }
-    printf("%d", H);
+    printf("%lld", H % M);
+    return (0);
 }
