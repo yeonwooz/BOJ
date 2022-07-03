4a1b9756a3a2485c984dff55f56f78c30d04fc7f
suyeon woo <woosean999@gmail.com>
2022-07-03 17:06:13 +0900

    4153 직각삼각형

---
+++ 4153/2/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int input;
+    int temp;
+    int sides[3] = {0, };
+    
+    while (1)
+    {
+        for (int i = 0; i < 3; ++i)
+        {
+            scanf("%d", &input);
+            if (i == 0)
+                sides[i] = input;
+            else 
+            {
+                if (sides[i - 1] > input) 
+                {
+                    temp = sides[i - 1];
+                    sides[i - 1] = input;
+                    sides[i] = temp;
+                } 
+                else if (sides[i - 1] < input) 
+                {
+                    sides[i] = input;
+                }
+            }
+        }
+        if (sides[0] == 0 && sides[1] == 0 && sides[2])
+            return (0);
+        if (sides[0] * sides[0] + sides[1] * sides[1] == sides[2] * sides[2])
+            printf("right\n");
+        else
+            printf("wrong\n");
+    }
+    return (0);
+}
