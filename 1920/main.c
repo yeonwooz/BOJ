8902f46c831c3a708de202288687d61819c824ef
suyeon woo <woosean999@gmail.com>
2022-07-03 21:26:07 +0900

    1920 수 찾기 - 음수범위 고려 / outofbounds

--- 1920/main.c
+++ 1920/main.c
@@ -3,27 +3,25 @@
 int main(void)
 {
     int N, M, target, num;
-    int arr[100001] = {0, };
+    int arr[200001] = {0, };
 
     scanf("%d", &N);
     for (int i = 0; i < N; ++i)
     {
-        scanf("%d", &arr[i]);
+        scanf("%d", &target);
+        if (target >= 0)
+            arr[target] = 1;
+        else 
+            arr[target * -1 + 100000] = 1;
     }
     scanf("%d", &M);
     for (int i = 0; i < M; ++i)
     {
         scanf("%d", &num);
-        for (int j = 0; j < N; ++j)
-        {
-            if (arr[j] == num)
-            {
-                printf("1");
-                break;
-            }
-            if (j == N - 1)
-                printf("0");
-        }
+        if (num >= 0 && arr[num] == 1 || num < 0 && arr[num * -1 + 100000] == 1)
+            printf("1");
+        else
+            printf("0");
         if (i < M - 1)
             printf("\n");
     }
