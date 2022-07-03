ed37e7a3a6ca810402bb8cfe93836c74b1f649a1
suyeon woo <woosean999@gmail.com>
2022-07-04 08:18:32 +0900

    1920 - out of memory

--- 1920/main.c
+++ 1920/main.c
@@ -1,39 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 int main(void)
 {
     int N, M, target, num;
     int *arr;
+    int has_pos_max = 0;
+    int has_neg_max = 0;
 
-    arr = (int *)malloc(sizeof(int) * 2147483649);
-    // for (int i = 0; i <= 2147483648; ++i)
-    // {
-    //     arr[i] = 0;
-    // }
+    arr = (int *)malloc(sizeof(int) * INT_MAX);
     scanf("%d", &N);
     for (int i = 0; i < N; ++i)
     {
         scanf("%d", &target);
-        if (target == -2147483648)
+        if (target == INT_MAX)
         {
-            if (arr[0] == 0)
-                arr[0] = 2;
-            else 
-                arr[0] = 3;
+            has_neg_max = 1;
+        }
+        else if (target == INT_MIN)
+        {
+            has_pos_max = 1;
         }
         else if (target >= 0)
         {
             if (arr[target] == 0)
                 arr[target] = 1;
-            else if (arr[target] == 1 || arr[target] == 2)
+            else if (arr[target] == 2)
                 arr[target] = 3;
         }
         else 
         {
             if (arr[target * -1] == 0)
                 arr[target * -1] = 2;
-            else if (arr[target * -1] == 1 || arr[target * -1] == 2)
+            else if (arr[target * -1] == 1)
                 arr[target * -1] = 3;
         }
     }
@@ -41,7 +41,7 @@ int main(void)
     for (int i = 0; i < M; ++i)
     {
         scanf("%d", &num);
-        if (num == -2147483648 && (arr[0] == 2 || arr[0] == 3))
+        if (num == INT_MIN && has_neg_max || num == INT_MAX && has_pos_max)
             printf("1");
         else if (num >= 0 && (arr[num] == 1 || arr[num] == 3) || num < 0 && (arr[num * -1] == 2 ||arr[num * -1] == 3))
             printf("1");
