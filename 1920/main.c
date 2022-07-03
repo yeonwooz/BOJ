82efc850ee039e6885c5f0e3d706ac151f09e006
suyeon woo <woosean999@gmail.com>
2022-07-03 21:47:00 +0900

    1920 long long - 메모리초과

--- 1920/main.c
+++ 1920/main.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)
 {
-    int N, M, target, num;
-    int arr[200001] = {0, };
+    long long N, M, target, num;
+    long long *arr;
 
-    scanf("%d", &N);
+    scanf("%lld", &N);
+    arr = (long long *)malloc(sizeof(long) * 2147483648 * 3);
     for (int i = 0; i < N; ++i)
     {
-        scanf("%d", &target);
+        scanf("%lld", &target);
         if (target >= 0)
             arr[target] = 1;
         else 
-            arr[target * -1 + 100000] = 1;
+            arr[target * -1 + 2147483648 * 2] = 1;
     }
-    scanf("%d", &M);
+    scanf("%lld", &M);
     for (int i = 0; i < M; ++i)
     {
-        scanf("%d", &num);
-        if (num >= 0 && arr[num] == 1 || num < 0 && arr[num * -1 + 100000] == 1)
+        scanf("%lld", &num);
+        if (num >= 0 && arr[num] == 1 || num < 0 && arr[num * -1 + 2147483648 * 2] == 1)
             printf("1");
         else
             printf("0");
