17b120db3950b1106cbb172b8dc7dfac958f2e5a
suyeon woo <woosean999@gmail.com>
2022-07-04 08:42:30 +0900

    1920 - malloc으로 정확한 사이즈만큼의 배열을 만들고, 퀵소트와 이분탐색으로 빠르게 탐색

--- 1920/main.c
+++ 1920/main.c
@@ -1,54 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
+int compare(const void *a, const void *b);
 int main(void)
 {
-    int N, M, target, num;
+    int N, M, num;
     int *arr;
-    int has_pos_max = 0;
-    int has_neg_max = 0;
 
-    arr = (int *)malloc(sizeof(int) * INT_MAX);
     scanf("%d", &N);
+    arr = (int *)malloc(sizeof(int) * N);
     for (int i = 0; i < N; ++i)
     {
-        scanf("%d", &target);
-        if (target == INT_MAX)
-        {
-            has_neg_max = 1;
-        }
-        else if (target == INT_MIN)
-        {
-            has_pos_max = 1;
-        }
-        else if (target >= 0)
-        {
-            if (arr[target] == 0)
-                arr[target] = 1;
-            else if (arr[target] == 2)
-                arr[target] = 3;
-        }
-        else 
-        {
-            if (arr[target * -1] == 0)
-                arr[target * -1] = 2;
-            else if (arr[target * -1] == 1)
-                arr[target * -1] = 3;
-        }
+        scanf("%d", &arr[i]);
     }
+    qsort(arr, N, sizeof(int), compare);
     scanf("%d", &M);
     for (int i = 0; i < M; ++i)
     {
         scanf("%d", &num);
-        if (num == INT_MIN && has_neg_max || num == INT_MAX && has_pos_max)
-            printf("1");
-        else if (num >= 0 && (arr[num] == 1 || arr[num] == 3) || num < 0 && (arr[num * -1] == 2 ||arr[num * -1] == 3))
-            printf("1");
-        else
+        int start = 0;
+        int end = M - 1;
+        int mid = (end + start) / 2;
+        int found = 0;;
+        while (end - start >= 0)
+        {
+            if (arr[mid] == num)
+            {
+                found = 1;
+                break;
+            }
+            else if (arr[mid] <= num)
+            {
+                start = mid + 1;
+            }
+            else
+            {
+                end = mid - 1;
+            }
+            mid = (end + start) / 2;
+        }
+        if (found == 0)
             printf("0");
+        else 
+            printf("1");
         if (i < M - 1)
             printf("\n");
     }
+    return (0);
+}
+
+int compare(const void *a, const void *b)
+{
+    int num1 = *(int *)a;
+    int num2 = *(int *)b;
+
+    if (num1 < num2)
+        return (-1);
+    else if (num1 > num2)
+        return (1);
     return (0);
 }
