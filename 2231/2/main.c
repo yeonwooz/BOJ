97c214e5df77a909adc9cb509a6c2f8e78b43170
suyeon woo <woosean999@gmail.com>
2022-07-05 08:33:24 +0900

    2231 코드 개선

--- 2231/2/main.c
+++ 2231/2/main.c
@@ -4,29 +4,22 @@ int compose(int n)
 {   
     int dig_sum = 0;
     int num = n;
-    while (num >= 10)
-    {
+    while (num) {
         dig_sum += num % 10;
         num /= 10;
     }
-    dig_sum += num;
     return (n + dig_sum);
 }
 
 int main() {
-    int arr[1000001] = {0, };
     int n;
     int target;
-    for (int i = 1; i < 1000001; ++i)
-    {
-        arr[i] = compose(i);
-    }
     
     scanf("%d", &n);
     target = 1;
     while (target <= 1000000)
     {
-        if (arr[target] == n)
+        if (compose(target) == n)
         {
             printf("%d", target);
             return (0);
