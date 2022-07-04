26e17c69740ec661b7344faa26aba4945c6b5b1d
suyeon woo <woosean999@gmail.com>
2022-07-04 16:56:09 +0900

    10816 - 숫자 카드 2 / 시간초과

--- 10816/main.c
+++ 10816/main.c
@@ -1,8 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b);
+int binSearch(int *arr, int len, int target);
 
 int main(void)
 {
     // 숫자 범위가 크므로 인덱스 저장하는 것은 비효율적이다
     // qsort 후에 이분탐색으로 찾고, 그 오른쪽으로 같은 수 몇개 있는지 탐색하기 
+    int N, M, num;
+    int *arr; 
+
+    scanf("%d", &N);
+    arr = (int *)malloc(sizeof(int) * N);
+    for (int i = 0; i < N; ++i)
+    {    
+        scanf("%d", &arr[i]);
+    }
+    qsort(arr, N, sizeof(int), compare);
+    scanf("%d", &M);
+    for (int i = 0; i < M; ++i)
+    {
+        scanf("%d", &num);
+        int index = binSearch(arr, N, num);
+        if (index == -1)
+            printf("0");
+        else 
+        {
+            int cnt = 0;
+            while (arr[index] == num)
+            {
+                ++index;
+                ++cnt;
+            }
+            printf("%d", cnt);
+        }
+        if (i < M - 1)
+            printf(" ");
+    }
     return (0);
+}
+
+int compare(const void *a, const void *b)
+{
+    int num1 = *(int *)a;
+    int num2 = *(int *)b;
+
+    if (num1 < num2)
+        return (-1);
+    else if (num1 > num2)
+        return (1);
+    return (0);
+}
+
+int binSearch(int *arr, int len, int target)
+{
+    int start = 0;
+    int end = len - 1;
+    int mid = (start + end) / 2;
+    while (end - start >= 0)
+    {
+        if (arr[mid] == target)
+        {
+            while (arr[mid] == target)
+                --mid;
+            return mid + 1;
+        }
+        else if (arr[mid] < target)
+            start = mid + 1;
+        else if (arr[mid] > target)
+            end = mid - 1;
+        mid = (start + end) / 2;
+    }
+    return (-1);
 }
