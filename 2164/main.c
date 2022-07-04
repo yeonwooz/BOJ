4f718279031b2e6c67555310d01265b980ba8d65
suyeon woo <woosean999@gmail.com>
2022-07-04 12:57:27 +0900

    2164 카드 2 - Queue 의 특징을 이용하여 풀기

--- 2164/main.c
+++ 2164/main.c
@@ -6,26 +6,25 @@ int main(void)
     int N;
     int *arr;
     scanf("%d", &N);
-    arr = (int *)malloc(sizeof(int) * (N + 2));
-    for (int i = 0; i < N + 2; ++i)
+    arr = (int *)malloc(sizeof(int) * (N + 1) * 2);
+    for (int i = 0; i < N + 1; ++i)
     {
         arr[i] = i;
     }
+    int index = N;
     int i = 1;
-    while (N > 1)
-    {        
-        int temp = arr[1];
-        for (int i = 2; i < N + 1; ++i)
+    while (1)
+    {
+        if (arr[index - 1] == 0 && arr[index + 2] == 0)
+            break;
+        if (i % 2 == 0) 
         {
-            arr[i - 1] = arr[i];
+            ++index;
+            arr[index] = arr[i];
         }
-        arr[N] = temp;
-        if (i % 2 != 0)
-        {
-            --N;
-        }
+        arr[i] = 0;
         ++i;
     }
-    printf("%d", arr[1]);
+    printf("%d", arr[index]);    
     return (0);
 }
