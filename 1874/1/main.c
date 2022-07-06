c5c55767a8d594b644f84ac39d6956e9256068d4
suyeon woo <woosean999@gmail.com>
2022-07-06 12:56:25 +0900

    1874 - 스택 수열

--- 1874/1/main.c
+++ 1874/1/main.c
@@ -1,96 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define STACK_SIZE 100000
 
-int push(int *arr, int num, int len);
-int pop(int *arr, int len);
-int is_in(int *arr, int num, int len);
+char result[2 * STACK_SIZE] = "";
+int stack[STACK_SIZE];
+int top_idx = -1;
 
 int main(void)
 {
-    int n, cur, prev = 0, prev_max = 1;
-    int seq[100000];
-    int nums[100000] = {0, };
-    int used[100000] = {0, };
+    int n, cur;
+    int *seq;
+    int result_idx = 0;
     scanf("%d", &n);
+    seq = (int *)malloc(sizeof(int) * n);
+
     for (int i = 0; i < n; ++i)
     {
         scanf("%d", &seq[i]);
-        if (i >= 2)
-        {
-            if (seq[i-2] > seq[i-1] && seq[i-2] > seq[i] && seq[i-1] < seq[i])
-            {
-                printf("NO");
-                return (0);
-            }
-        }
     }
 
-    for (int i = 0; i < n; ++i)
-    {
-        int cur = seq[i];        
-        int j = prev_max;
-        while (j <= cur)
-        {
-            if (!is_in(nums, j, n) && !is_in(used, j, n))
-            {
-                push(nums, j, n);
-                if (j == cur)
-                    pop(nums, n);
-            }
-            ++j;
-        }
-                
-        if (prev > 0 && prev > cur)
-        {
-            int j = prev;
-            while (j >= cur)
-            {   
-                if(is_in(nums, j, n))
-                {
-                    pop(nums, n);
-                }
-                --j;
-            }
-        }
-        
-        prev = cur;
-        if (prev_max < cur)
-            prev_max = cur;
-        used[i] = cur;
-    }
-
-    return (0);
-}
-
-int push(int *arr, int num, int len)
-{
     int i = 0;
-    while (arr[i])
-        ++i;
-    if (i >= len)
-        return (-1);
-    arr[i] = num;
-    printf("+\n");
-    return (1);
-}
-
-int pop(int *arr, int len)
-{
-    int i = len - 1;
-    while (!arr[i])
-        --i;
-    if (i < 0)
-        return (-1);
-    arr[i] = 0;
-    printf("-\n");
-    return (1);
-}
-
-int is_in(int *arr, int num, int len)
-{
-    for (int i = 0; i < len; ++i)
+    int cur_num = 1;
+    while (1)
     {
-        if (arr[i] == num)
-            return (1);
+        if (top_idx == -1 || stack[top_idx] < seq[i])
+        {
+            stack[++top_idx] = cur_num++;
+            result[result_idx++] = '+';
+        }
+        else if (stack[top_idx] == seq[i])
+        {
+            top_idx--;
+            result[result_idx++] = '-';
+            ++i;
+        }
+        else 
+        {
+            printf("NO");
+            return (0);
+        }
+        if (result_idx == n * 2) break;
+    }
+
+    for (int i = 0; i < n * 2; ++i)
+    {
+        printf("%c", result[i]);
+        if (i < n * 2 - 1)
+           printf("\n"); 
     }
     return (0);
 }
