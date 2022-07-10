a0a49c66ed67a21d293aabd4c983f8656e78a05c
suyeon woo <woosean999@gmail.com>
2022-07-10 13:56:32 +0900

    18111  마인크래프트 / C

--- 18111/1/main.c
+++ 18111/1/main.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 void solve(int N, int M, int **ground, int B, int max_block_height, int min_block_height);
 
 int main(void)
 {
-    int N, M, B, max_block_height = 0, min_block_height = 256;  // long long?
+    int N, M, B, max = 0, min = 256;  // long long?
     int **ground;
 
     scanf("%d %d %d", &N, &M, &B);
@@ -16,60 +17,64 @@ int main(void)
         for (int j = 0; j < M; ++j)
         {
             scanf("%d", &ground[i][j]);
-            if (max_block_height < ground[i][j])
-                max_block_height = ground[i][j];
-            if (min_block_height > ground[i][j])
-                min_block_height = ground[i][j];
+            if (max < ground[i][j])
+                max = ground[i][j];
+            if (min > ground[i][j])
+                min = ground[i][j];
         }
     }
-    solve(N, M, ground, B, max_block_height, min_block_height);
+    solve(N, M, ground, B, max, min);
     return (0);
 }
 
-void solve(int N, int M, int **ground, int B, int max_block_height, int min_block_height)
+void solve(int N, int M, int **ground, int B, int max, int min)
 {
-    int cur_time;
-    int min_time = 512 * 500 * 500;
-    int cur_H = min_block_height;
-    int max_H = min_block_height;
+    int cur_time = 0;
+    int min_time = INT_MAX;
+    int cur_H = max;
+    int max_H = 256;
 
-    while (cur_H <= max_block_height)
+    while (cur_H >= min)
     {
         cur_time = 0;
         int inventory = B;
+        int hole = 0;
         for (int i = 0; i < N; ++i)
         {
             for (int j = 0; j < M; ++j)
             {
                 if (ground[i][j] == cur_H)
                     continue;
-                if (ground[i][j] > cur_H)
+                else if (ground[i][j] > cur_H)
                 {
                     int diff = ground[i][j] - cur_H;
                     inventory += diff;
-                    cur_time += 2;
-                }
+                    cur_time += 2 * diff;
+                } 
                 else if (ground[i][j] < cur_H)
                 {
                     int diff = cur_H - ground[i][j];
-                    if (inventory < diff)
-                        continue;
                     inventory -= diff;
-                    ++cur_time;
+                    cur_time += diff;
+                }
+            }
+        }        
+        if (inventory >= hole)
+        {
+            if (cur_time < min_time)
+            {
+                min_time = cur_time;
+                max_H = cur_H;
+            }
+            else if (cur_time == min_time)
+            {
+                if (cur_H > max_H)
+                {
+                    max_H = cur_H;
                 }
             }
         }
-        if (cur_time > 0 && cur_time < min_time)
-        {
-            min_time = cur_time;
-            max_H = cur_H;
-        }
-        else if (cur_time == min_time)
-        {
-            if (cur_H > max_H)
-                max_H = cur_H;
-        }
-        ++cur_H;
+        --cur_H;
     }
     printf("%d %d", min_time, max_H);
 }
