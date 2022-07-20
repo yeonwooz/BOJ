ecd4c0b36e53276caa3e02a732fc8845690c0a3c
suyeon woo <woosean999@gmail.com>
2022-07-20 11:34:08 +0900

    1931 회의실 배정 / C / 그리디, 정렬

--- 1931/main.c
+++ 1931/main.c
@@ -1,78 +1,59 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
 
-int compare(const void *a, const void *b);
-// TODO: 종료시간 기준으로 정렬해보기
-typedef struct 
+// 그리디 알고리즘 / 종료시간 기준으로 정렬 후, 시간이 안겹치는 것들만 선택
+
+typedef struct _Meet
 {
     int start;
-    int duration;
+    int end;
 } Meet;
+void solve(int N);
+int cmp(const void *a, const void *b);
 
 int main(void)
 {
-    int N, start, end, max_cnt = 0, fin_time = 0;
-    Meet *meets;
+    int N;
 
     scanf("%d", &N);
-    meets = (Meet *)malloc(sizeof(meets[0]) * N);
-    for (int i = 0; i < N; ++i)
-    {
-        scanf("%d %d", &start, &end);
-        if (fin_time < end)
-            fin_time = end;
-        meets[i].start = start;
-        meets[i].duration = end - start;
-    }
-    qsort(meets, N, sizeof(meets[0]), compare);
-    
-    // for (int i = 0; i < N; ++i)
-    // {
-    //     printf("%d ", meets[i].start);
-    //     printf("%d \n", meets[i].duration);
-    // }
-
-    int cur_time = 0;
-    int index = 0;
-    while (index < N - 1)
-    {
-        while (meets[index].start > cur_time)
-            ++index;
-        int duration = INT_MAX;
-        while (meets[index].start == cur_time)
-        {
-            if (meets[index].duration < duration)
-            {
-                duration = meets[index].duration;
-                cur_time += duration;
-                ++max_cnt;
-            }
-        }
-        printf("cur_time=%d\n", cur_time);
-        printf("index=%d\n", index);
-        printf("duration=%d\n", duration);
-        ++index;
-    }
-    printf("%d", max_cnt);
+    solve(N);
     return (0);
 }
 
-int compare(const void *a, const void *b)
+void solve(int N)
+{
+    Meet meets[100000] = {0,};
+    for (int i = 0; i < N; ++i)
+    {
+        scanf("%d %d", &meets[i].start, &meets[i].end);    
+    }
+    qsort(meets, N, sizeof(Meet), cmp);
+    
+    int last_end = 0;
+    int cnt = 0;
+    for (int i = 0; i < N; ++i)
+    {
+        if (last_end <= meets[i].start)
+        {
+            last_end = meets[i].end;
+            ++cnt;
+        } 
+    }
+    printf("%d", cnt);
+}
+
+int cmp(const void *a, const void *b)
 {
     Meet meet1 = *(Meet *)a;
     Meet meet2 = *(Meet *)b;
 
+    if (meet1.end < meet2.end)
+        return (-1);
+    if (meet1.end > meet2.end)
+        return (1);
     if (meet1.start < meet2.start)
         return (-1);
-    else if (meet1.start > meet2.start)
+    if (meet1.start > meet2.start)
         return (1);
-    else 
-    {
-        if (meet1.duration < meet2.duration)
-            return (-1);
-        else if (meet1.duration > meet2.duration)
-            return (1);
-    }
     return (0);
 }
