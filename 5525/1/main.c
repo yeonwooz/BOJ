e24a75eab0d4d0c5b9d41fa043184c4335406751
suyeon woo <woosean999@gmail.com>
2022-07-23 11:49:14 +0900

    5525 WIP

--- 5525/1/main.c
+++ 5525/1/main.c
@@ -3,7 +3,7 @@
 
 void solve(int N, int M, char *S);
 int is_valid(char *S, int start, int end);
-
+int valid_until(char *S, int start, int end);
 int main(void)
 {
     int N, M;
@@ -17,27 +17,52 @@ int main(void)
 
 void solve(int N, int M, char *S)
 {
-    int end = 2 * N, cnt = 0;
-    for (int i = end; i < strlen(S); ++i)
+    int cnt = 0;
+    for (int i = 0; i < strlen(S); ++i)
     {
-        // printf("end=%d start=%d\n", end, i - 2 * N);
-        if (is_valid(S, i - 2 * N, i))
-            ++cnt;
+        if (S[i+1] == 'O' && S[i+2] == 'I')
+        {
+            int O = 'O';
+            while (S[i] == 'I' && S[i+1] == 'O')
+            {
+                i += 2;
+                ++O;
+                if (S[i] == 'I' && O == N)
+                {
+                    --O;
+                    ++cnt;
+                }
+            }
+        }
     }
+
     printf("%d", cnt);
 }
 
-int is_valid(char *S, int start, int end)
+// int is_valid(char *S, int start, int end)
+// {
+//     if (S[start] == 'O')
+//         return (0);
+//     int i =  start;
+//     while (i <= end - 1)
+//     {
+//         if (S[i] == S[i+1])
+//             return (0);
+//         ++i;
+//     }
+//     return (1);
+// }
+
+int valid_until(char *S, int start, int end)
 {
     if (S[start] == 'O')
-        return (0);
+        return (start);
     int i =  start;
     while (i <= end - 1)
     {
         if (S[i] == S[i+1])
-            return (0);
+            return (i);
         ++i;
     }
-    return (1);
+    return (i);
 }
-
