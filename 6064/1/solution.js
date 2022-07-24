ea02a36924e3b1cc0a6b96b03d944e1629a20f4e
suyeon woo <woosean999@gmail.com>
2022-07-24 13:44:30 +0900

    6064 term 크기를 M또는 N으로

--- 6064/1/solution.js
+++ 6064/1/solution.js
@@ -16,9 +16,10 @@ function getInputs() {
 
 function solve(data) {
   const [M, N, x, y] = data;
-  let k = Math.max(x, y);
   const gcd = getgcd(M, N);
   const lcm = gcd * (M / gcd) * (N / gcd);
+  let k = Math.min(x, y);
+  let term = x < y ? M : N;
 
   while (1) {
     if (k > lcm || (k % M === M && k % N === N)) {
@@ -29,7 +30,7 @@ function solve(data) {
       console.log(k);
       break;
     }
-    k += gcd;
+    k += term;
   }
 }
 
