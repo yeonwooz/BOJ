1b25761851df5a04af5253124ed9ace2bf34c960
suyeon woo <woosean999@gmail.com>
2022-07-25 10:51:15 +0900

    1966 프린터 큐 / C / 원형 큐

---
+++ 1966/2/main.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void solve();
+
+int main(void)
+{
+    int T;
+
+    scanf("%d", &T);
+    for (int i = 0; i < T; ++i)
+        solve();
+    return (0);
+}
+
+void solve()
+{
+    int N, M;
+    scanf("%d %d", &N, &M);
+    int *papers; // 숫자가 클수록 우선순위 높음
+    papers = (int *)malloc(sizeof(int) * N);
+    for (int i = 0; i < N; ++i)
+    {
+        scanf("%d", &papers[i]);
+    }
+
+    int front = 0;
+    int rear = N - 1;
+    int cnt = 0;
+
+    while (1)
+    {
+        int printable = 1;
+        for (int i = 0; i <= N; ++i)
+        {
+            if (i == front)
+                continue;
+            if (papers[i % N] > papers[front])
+            {
+                printable = 0;
+                break;
+            }
+        }
+        if (printable)
+        {
+            papers[front] = 0;
+            ++cnt;
+            if (front == M)
+            {
+                printf("%d\n", cnt);
+                break;
+            }
+        }
+        else 
+            rear = 0;
+        front = (front + 1) % N;
+    }
+    
+    free(papers);
+}
