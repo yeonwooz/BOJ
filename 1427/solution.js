c4ace6ae4744b77d6a8b089646f6015e91f37850
suyeon woo <woosean999@gmail.com>
2022-08-04 22:51:07 +0900

    1427 소트인사이드 / nodejs / 기본정렬(팀소트) 사용

--- 1427/solution.js
+++ 1427/solution.js
@@ -14,25 +14,25 @@ function getInputs() {
 
 function solve(str) {
   let arr = str.split("");
-  qsort(arr, 0, str.length - 1);
+  arr.sort((a,b) => b-a)
   console.log(arr.join(""));
 }
 
-function qsort(arr, L, R) {
-  let left = L;
-  let right = R;
-  let pivot = arr[Math.floor((left + right) / 2)];
+// function qsort(arr, L, R) {
+//   let left = L;
+//   let right = R;
+//   let pivot = arr[Math.floor((left + right) / 2)];
 
-  while (left <= right) {
-    while (arr[left] > pivot) ++left;
-    while (arr[right] < pivot) --right;
-    if (left <= right) {
-      [arr[left], arr[right]] = [arr[right], arr[left]];
-      ++left;
-      --right;
-    }
-  }
+//   while (left <= right) {
+//     while (arr[left] > pivot) ++left;
+//     while (arr[right] < pivot) --right;
+//     if (left <= right) {
+//       [arr[left], arr[right]] = [arr[right], arr[left]];
+//       ++left;
+//       --right;
+//     }
+//   }
 
-  if (L < right) qsort(arr, L, right);
-  if (left < R) qsort(arr, left, R);
-}
+//   if (L < right) qsort(arr, L, right);
+//   if (left < R) qsort(arr, left, R);
+// }
