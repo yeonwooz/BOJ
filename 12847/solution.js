4d568cddd66424f3c384e7b96ab9e683adcce457
suyeon woo <woosean999@gmail.com>
2022-08-16 13:49:35 +0900

    12847 꿀 아르바이트 / nodejs / 누적합 / 슬라이딩 윈도우 캐싱 적용

--- 12847/solution.js
+++ 12847/solution.js
@@ -17,19 +17,23 @@ function getInputs() {
 function solve(n, m, arr) {
   if (m === 0) return 0;
   if (n === 1) return arr[0];
-  
+
   // 길이 n 인 arr에서 길이 m만큼의 최대 누적합
-  let i = 0;
+  let right = m - 1;
   let maxSum = 0;
-  while (i <= n - m) {
-    let sum = 0;
-    for (idx = i; idx <= i + m - 1; ++idx) {
-      sum += arr[idx];
-    }
+  let sum = 0;
+  for (let i = 0; i <= right; ++i) {
+    sum += arr[i];
+  }
+  maxSum = sum;
+
+  while (right < n) {
+    let left = right + 1 - m;
+    sum = sum - arr[left] + arr[++right];
     if (sum > maxSum) {
       maxSum = sum;
     }
-    ++i;
   }
+
   console.log(maxSum);
 }
