20d03633ec582b8757c640906719dd12b99fe42a
suyeon woo <woosean999@gmail.com>
2022-08-26 16:26:19 +0900

    4158 / 메모리초과 해결을 위해 인풋 함수 단순화, 이분탐색 함수 생성 / 오류 찾는 중

--- 4158/solution.js
+++ 4158/solution.js
@@ -1,45 +1,56 @@
 main();
 
 function main() {
-  const [N, M, arr2] = getInputs();
-  const arr1 = [];
-  for (let i = 0; i < N; ++i) {
-    arr1.push(arr2.shift());
-  }
-  solve(
-    N,
-    M,
-    arr1.map((n) => +n),
-    arr2.map((n) => +n)
-  );
+  const [N, M, inputs] = getInputs();
+
+  let cnt = solve(N, M, inputs);
+  console.log(cnt);
 }
 
 function getInputs() {
   const fs = require("fs");
   const filepath =
     process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
-  let [meta, ...inputs] = fs
-    .readFileSync(filepath)
-    .toString()
-    .trim()
-    .split("\n");
-  meta = meta.split(" ");
+  let inputs = fs.readFileSync(filepath).toString().trim().split("\n");
   inputs.pop();
-  return [+meta[0], +meta[1], inputs];
+  meta = inputs.shift().split(" ");
+  return [+meta[0], +meta[1], inputs.map((n) => +n)];
 }
 
-function solve(N, M, arr1, arr2) {
+function solve(N, M, inputs) {
   let cnt = 0;
-  if (N >= M) {
-    const set1 = new Set(arr1);
-    for (let i = 0; i < M; ++i) {
-      if (set1.has(arr2[i])) ++cnt;
+  if (N <= M) {
+    for (let i = 0; i < N; ++i) {
+      const target = inputs[i];
+      if (binSearch(inputs, N, M + N - 1, target)) {
+        ++cnt;
+      }
     }
   } else {
-    const set2 = new Set(arr2);
-    for (let i = 0; i < N; ++i) {
-      if (set2.has(arr1[i])) ++cnt;
+    for (let i = N; i < N + M; ++i) {
+      const target = inputs[i];
+      if (binSearch(inputs, 0, N - 1, target)) {
+        ++cnt;
+      }
     }
   }
-  console.log(cnt);
+  return cnt;
+}
+
+function binSearch(arr, left, right, target) {
+  let start = left;
+  let end = right;
+  while (start <= end) {
+    let mid = Math.floor((start + end) / 2);
+
+    if (arr[mid] === target) {
+      return true;
+    }
+    if (arr[mid] < target) {
+      start = mid + 1;
+    } else {
+      end = mid - 1;
+    }
+  }
+  return false;
 }
