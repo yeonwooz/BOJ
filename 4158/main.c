93507bd07f471f758304aa03ae465b7941c4c4a3
suyeon woo <woosean999@gmail.com>
2022-08-26 17:43:00 +0900

    입력케이스 여러개 대응

--- 4158/main.c
+++ 4158/main.c
@@ -1,40 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int N = 0;
-int M = 0;
-int cnt = 0;
-int arr1[1000000] = {0,};
-int arr2[1000000] = {0,};
+int N = -1;
+int M = -1;
+int *arr1;
+int *arr2;
 
 int init(void);
 void solve(void);
 
 int main(void)
 {
-    if(init())
-    {
+    while (1) {
+        scanf("%d %d", &N, &M);
+        if (N == 0 && M == 0)
+        {   
+            break;
+        }
+        arr1 = (int *)malloc(sizeof(int) * N);
+        for (int i = 0; i < N; ++i) {
+            scanf("%d", &arr1[i]);
+        }
+        arr2 = (int *)malloc(sizeof(int) * M);
+        for (int i = 0; i < M; ++i) {
+            scanf("%d", &arr2[i]);
+        }
         solve();
+        free(arr1);
+        free(arr2);
     }
-    return (0);
-}
 
-int init(void)
-{
-    scanf("%d %d", &N, &M);
-    for (int i = 0; i < N; ++i) {
-        scanf("%d", &arr1[i]);
-    }
-    for (int i = 0; i < M; ++i) {
-        scanf("%d", &arr2[i]);
-    }
-    return (1);
+    return (0);
 }
 
 void solve(void) {
     int i = 0; 
     int j = 0;
+    int cnt = 0;
+    while (1) {
+        if (i == N || j == M) {
+            break;
+        }
 
-    while (arr1[i] && arr2[j]) {
         if (arr1[i] < arr2[j]) {
             ++i;
         } 
