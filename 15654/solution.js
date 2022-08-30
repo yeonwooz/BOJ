53bfb32801fca0bb1d6408e24ff3c7e762063ede
suyeon woo <woosean999@gmail.com>
2022-08-30 14:53:48 +0900

    [WIP]15654 N과 M (5) / nodejs / 순열, 백트랙킹

---
+++ 15654/solution.js
@@ -0,0 +1,40 @@
+main();
+
+function main() {
+  const [N, M, inputs] = getInputs();
+  solve(N, M, inputs);
+}
+
+function getInputs() {
+  const fs = require("fs");
+  const filepath =
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+  let [meta, inputs] = fs.readFileSync(filepath).toString().trim().split("\n");
+  meta = meta.split(" ");
+  return [+meta[0], +meta[1], inputs.split(" ")];
+}
+
+function solve(N, M, inputs) {
+  inputs.sort();
+  let visitedIdx = Array(N).fill(0);
+  let answer = [];
+  let chosen = [];
+  DFS(0);
+  function DFS(depth) {
+    if (depth === M) {
+      answer.push(chosen.join(" "));
+      return;
+    }
+
+    for (let i = 0; i < N; ++i) {
+      if (!visitedIdx[i]) {
+        chosen[depth] = inputs[i];
+        visitedIdx[i] = 1;
+        DFS(depth + 1);
+        visitedIdx[i] = 0;
+      }
+    }
+  }
+
+  console.log(answer.join("\n"));
+}
