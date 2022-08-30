e2299b1ccb39c943eb6fb5e0653f0dda1ce4769e
suyeon woo <woosean999@gmail.com>
2022-08-30 14:23:10 +0900

    [WIP]15651 N과 M (3) / nodejs / 순열 - 중복 허용해서 고르기 / 시간초과

---
+++ 15651/solution.js
@@ -0,0 +1,32 @@
+main();
+
+function main() {
+  const [N, M] = getInputs();
+  solve(N, M);
+}
+
+function getInputs() {
+  const fs = require("fs");
+  const filepath =
+    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+  const [N, M] = fs.readFileSync(filepath).toString().trim().split(" ");
+  return [+N, +M];
+}
+
+function solve(N, M) {
+  //   let visited = Array(N + 1).fill(0);
+  let chosen = [];
+
+  DFS(0);
+  function DFS(depth) {
+    if (depth === M) {
+      console.log(chosen.join(" "));
+      return;
+    }
+
+    for (let i = 1; i <= N; ++i) {
+      chosen[depth] = i;
+      DFS(depth + 1);
+    }
+  }
+}
