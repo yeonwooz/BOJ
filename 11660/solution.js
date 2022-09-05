b1997f1dadaf1967d6b517afe0cc99cc0724c36b
suyeon woo <woosean999@gmail.com>
2022-09-05 12:13:53 +0900

    [WIP]11660 구간 합 구하기 5 / nodejs / 각 행 합 캐싱

--- 11660/solution.js
+++ 11660/solution.js
@@ -2,7 +2,8 @@ main();
 
 function main() {
   const inputs = getInputs();
-  const [N, M] = inputs.shift().split(" ");
+  let [N, M] = inputs.shift().split(" ");
+  [N, M] = [+N, +M];
   const arr = [];
   for (let i = 0; i < +N; ++i) {
     arr.push(
@@ -12,10 +13,20 @@ function main() {
         .map((n) => +n)
     );
   }
+
+  // 각 행의 합이 누적되는 DP
+  const dp = new Array(N).fill(0);
+
+  for (let i = 0; i < N; ++i) {
+    for (let j = 0; j < N; ++j) {
+      dp[i] += arr[i][j];
+    }
+  }
+
   const answer = [];
-  for (let i = 0; i < +M; ++i) {
+  for (let i = 0; i < M; ++i) {
     const [x1, y1, x2, y2] = inputs.shift().split(" ");
-    answer.push(solve(arr, +N, +x1, +y1, +x2, +y2));
+    answer.push(solve(arr, N, dp, +x1 - 1, +y1 - 1, +x2 - 1, +y2 - 1));
   }
   console.log(answer.join("\n"));
 }
@@ -28,25 +39,29 @@ function getInputs() {
   return inputs;
 }
 
-function solve(arr, N, x1, y1, x2, y2) {
-  const dp = Array(N);
-  for (let i = 0; i < N; ++i) {
-    dp[i] = Array(N).fill(0);
-  }
+// 각 행의 합 구해두자
+function solve(arr, N, dp, x1, y1, x2, y2) {
+  let sum = 0;
 
-  for (let i = x1 - 1; i <= x2 - 1; ++i) {
-    for (let j = y1 - 1; j <= y2 - 1; ++j) {
-      if (j === x1 - 1) {
-        if (i === 0) {
-          dp[i][j] = arr[i][j];
-        } else {
-          dp[i][j] = dp[i - 1][y2 - 1] + arr[i][j];
+  if (x1 + 2 <= x2) {
+    for (let row = x1 + 1; row < x2; ++row) {
+      sum += dp[row];
+      for (let col = 0; col < N; ++col) {
+        if (col < y1 || col > y2) {
+          sum -= arr[row][col];
         }
-      } else {
-        dp[i][j] = dp[i][j - 1] + arr[i][j];
       }
     }
   }
 
-  return dp[x2 - 1][y2 - 1];
+  for (let col = y1; col <= y2; ++col) {
+    if (x1 === x2) {
+      sum += arr[x1][col];
+    } else {
+      sum += arr[x1][col];
+      sum += arr[x2][col];
+    }
+  }
+
+  return sum;
 }
