17b5de3802edd560909e0ebca01e86f0dec206d4
suyeon woo <woosean999@gmail.com>
2022-09-09 22:43:16 +0900

    [WIP] 7662 / C / 시간초과

--- 7662/5/main.c
+++ 7662/5/main.c
@@ -18,8 +18,8 @@ int assign(void);
 void solve();
 void push_max_heap(long long);
 void push_min_heap(long long);
-void max_heapify(long long int *, int, int);
-void min_heapify(long long int *, int, int);
+void max_heapify(int, int);
+void min_heapify(int, int);
 int isInHeap(long long*, long long int);
 
 int main(void)
@@ -76,9 +76,8 @@ void solve(void)
 
         scanf("%s", cmd);
         scanf("%lld", &num);
-
         if (strcmp(cmd, "I") == 0)
-        {   
+        {  
             push_max_heap(num);
             push_min_heap(num);
         }
@@ -93,7 +92,7 @@ void solve(void)
                 if (maxHead == maxTail) continue;
                 max_heap[maxHead] = max_heap[maxTail - 1];
                 --maxTail;
-                max_heapify(max_heap, maxHead, maxTail);  
+                max_heapify(maxHead, maxTail);  
             }
             else 
             {
@@ -104,7 +103,7 @@ void solve(void)
                 if (minHead == minTail) continue;
                 min_heap[minHead] = min_heap[minTail - 1];
                 --minTail;
-                min_heapify(min_heap, minHead, minTail);
+                min_heapify(minHead, minTail);
             }
 
         }
@@ -115,6 +114,7 @@ void solve(void)
         printf("EMPTY\n");
         return ;
     }
+
     printf("%lld %lld\n", max_heap[maxHead], min_heap[minHead]);
 
     if (max_heap)
@@ -123,43 +123,22 @@ void solve(void)
         free(min_heap);
 }
 
-void push_max_heap(long long num)
-{
-    ++maxTail;
-    max_heap[maxTail] = num;
-    int temp_idx = maxTail;
-
-    while (temp_idx >= 2)
-    {
-        long long parent = max_heap[temp_idx / 2];
-        long long child = max_heap[temp_idx];
-
-        if (child > parent) 
-        {
-            max_heap[temp_idx / 2] = child;
-            max_heap[temp_idx] = parent;
-            temp_idx /= 2;
-            continue;
-        }
-        break;
-    }
-}
 
 void push_min_heap(long long num)
 {
-    ++minTail;
-    min_heap[minTail] = num;
-    int temp_idx = minTail;
+    min_heap[minTail++] = num;
+    int temp_idx = minTail - 1;
 
     while (temp_idx >= 2)
     {
-        long long parent = min_heap[temp_idx / 2];
-        long long child = min_heap[temp_idx];
+        long long parent = temp_idx / 2;
+        long long child = temp_idx;
 
-        if (child < parent) 
+        if (min_heap[parent] > min_heap[child]) 
         {
-            min_heap[temp_idx / 2] = child;
-            min_heap[temp_idx] = parent;
+            int temp = min_heap[parent];
+            min_heap[parent] = min_heap[child];
+            min_heap[child] = temp;
             temp_idx /= 2;
             continue;
         }
@@ -167,52 +146,79 @@ void push_min_heap(long long num)
     }
 }
 
-void max_heapify(long long *heap, int start, int idx)
-{   
-    int left = start * 2;
-    int right = start * 2 + 1;
-    int max_idx = start;
+void push_max_heap(long long num)
+{
+    max_heap[maxTail++] = num;
+    int temp_idx = maxTail - 1;
 
-    if (left <= idx && MAX(heap[left], heap[right]) == heap[left] && MAX(heap[left], heap[start]) == heap[left])
-        max_idx = left;
-    if (right <= idx && MAX(heap[left], heap[right]) == heap[right] && MAX(heap[right], heap[start]) == heap[right])
-        max_idx = right;
-
-
-    if (max_idx != start)
+    while (temp_idx >= 2)
     {
-        long long temp = heap[start];
-        heap[start] = heap[max_idx];
-        heap[max_idx] = temp;
-        max_heapify(heap, max_idx, idx);
+        long long parent = temp_idx / 2;
+        long long child = temp_idx;
+
+        if (max_heap[parent] < max_heap[child]) 
+        {
+            int temp = max_heap[parent];
+            max_heap[parent] = max_heap[child];
+            max_heap[child] = temp;
+            temp_idx /= 2;
+            continue;
+        }
+        break;
     }
 }
 
-
-void min_heapify(long long *heap, int start, int idx)
+void min_heapify(int start, int idx)
 {   
+    int min_idx = start;
     int left = start * 2;
     int right = start * 2 + 1;
-    int min_idx = start;
 
-    if (left <= idx && MIN(heap[left], heap[right]) == heap[left] && MIN(heap[left], heap[start]) == heap[left])
+    if (left <= idx && MIN(min_heap[left], min_heap[right]) == min_heap[left] && MIN(min_heap[left], min_heap[start]) == min_heap[left])
+    {
         min_idx = left;
-    if (right <= idx && MIN(heap[left], heap[right]) == heap[right] && MIN(heap[right], heap[start]) == heap[right])
+    }
+    if (right <= idx && MIN(min_heap[left], min_heap[right]) == min_heap[right] && MIN(min_heap[right], min_heap[start]) == min_heap[right])
+    {
         min_idx = right;
+    }
 
     if (min_idx != start)
     {
-        long long temp = heap[start];
-        heap[start] = heap[min_idx];
-        heap[min_idx] = temp;
-        min_heapify(heap, min_idx, idx);
+        long long temp = min_heap[start];
+        min_heap[start] = min_heap[min_idx];
+        min_heap[min_idx] = temp;
+        min_heapify(min_idx, idx);
     }
 }
 
+void max_heapify(int start, int idx)
+{   
+    int max_idx = start;
+    int left = start * 2;
+    int right = start * 2 + 1;
+
+    if (left <= idx && MAX(max_heap[left], max_heap[right]) == max_heap[left] && MAX(max_heap[left], max_heap[start]) == max_heap[left])
+    {
+        max_idx = left;
+    }
+    if (right <= idx && MAX(max_heap[left], max_heap[right]) == max_heap[right] && MAX(max_heap[right], max_heap[start]) == max_heap[right])
+    {
+        max_idx = right;
+    }
+
+    if (max_idx != start)
+    {
+        long long temp = max_heap[start];
+        max_heap[start] = max_heap[max_idx];
+        max_heap[max_idx] = temp;
+        max_heapify(max_idx, idx);
+    }
+}
 
 int isInHeap(long long* heap, long long int num) 
 {
-    int i = 0;
+    int i = 1;
     while (heap[i]) {
         if (heap[i] == num ) {
             return 1;
