862e7c3d5a7a0dd838059293ec64a5a8e10c85ea
suyeon woo <woosean999@gmail.com>
2022-09-11 13:37:55 +0900

    2096 / nodejs의 경우 메모리초과 해결 어려움

--- 2096/1/solution.js
+++ 2096/1/solution.js
@@ -1,45 +1,49 @@
 main();
 
 function main() {
-  const [N, maxDp] = getInputs();
-  //   console.log(N, arr);
-  solve(N, maxDp);
+  const [N, inputs] = getInputs();
+  solve(N, inputs);
 }
 
 function getInputs() {
   const fs = require("fs");
   const filepath =
     process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
-  const [N, ...maxDp] = fs.readFileSync(filepath).toString().trim().split("\n");
+  const [N, ...inputs] = fs
+    .readFileSync(filepath)
+    .toString()
+    .trim()
+    .split("\n");
 
-  return [Number(N), maxDp.map((str) => str.split(" ").map((n) => Number(n)))];
+  return [N, inputs];
 }
 
-function solve(N, maxDp) {
-  // const maxDp = Array(N);
-  const minDp = Array(N);
+function solve(N, inputs) {
+  const maxDp = [
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
+  const minDp = [
+    [0, 0, 0],
+    [0, 0, 0],
+  ];
 
-  for (let i = 0; i < N; ++i) {
-    if (i === 0) {
-      minDp[i] = maxDp[i].slice();
-    } else {
-      minDp[i] = maxDp[i].slice();
-      minDp[i][0] += Math.min(minDp[i - 1][0], minDp[i - 1][1]);
-      minDp[i][1] += Math.min(
-        minDp[i - 1][0],
-        minDp[i - 1][1],
-        minDp[i - 1][2]
-      );
-      minDp[i][2] += Math.min(minDp[i - 1][1], minDp[i - 1][2]);
+  let row = 0;
+  for (let i = 0; i < Number(N); ++i) {
+    let [x, y, z] = inputs[i].split(" ");
 
-      maxDp[i][0] += Math.max(maxDp[i - 1][0], maxDp[i - 1][1]);
-      maxDp[i][1] += Math.max(
-        maxDp[i - 1][0],
-        maxDp[i - 1][1],
-        maxDp[i - 1][2]
-      );
-      maxDp[i][2] += Math.max(maxDp[i - 1][1], maxDp[i - 1][2]);
-    }
+    maxDp[row][0] = Math.max(maxDp[1 - row][0], maxDp[1 - row][1]) + Number(x);
+    maxDp[row][1] =
+      Math.max(maxDp[1 - row][0], maxDp[1 - row][1], maxDp[1 - row][2]) +
+      Number(y);
+    maxDp[row][2] = Math.max(maxDp[1 - row][1], maxDp[1 - row][2]) + Number(z);
+
+    minDp[row][0] = Math.min(minDp[1 - row][0], minDp[1 - row][1]) + Number(x);
+    minDp[row][1] =
+      Math.min(minDp[1 - row][0], minDp[1 - row][1], minDp[1 - row][2]) +
+      Number(y);
+    minDp[row][2] = Math.min(minDp[1 - row][1], minDp[1 - row][2]) + Number(z);
+    row = 1 - row;
   }
-  console.log(Math.max(...maxDp[N - 1]), Math.min(...minDp[N - 1]));
+  console.log(Math.max(...maxDp[1 - row]), Math.min(...minDp[1 - row]));
 }
