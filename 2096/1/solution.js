4b8e4412ccab8713164bf1472aaf0d26dc423c17
suyeon woo <woosean999@gmail.com>
2022-09-11 13:46:17 +0900

    2096 nodejs 메모리 초과 해결불가..

--- 2096/1/solution.js
+++ 2096/1/solution.js
@@ -1,49 +1,66 @@
+const events = require("events");
+const fs = require("fs");
+const readline = require("readline");
+const filepath =
+  process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
+
+const maxDp = [
+  [0, 0, 0],
+  [0, 0, 0],
+];
+const minDp = [
+  [0, 0, 0],
+  [0, 0, 0],
+];
+
 main();
 
 function main() {
-  const [N, inputs] = getInputs();
-  solve(N, inputs);
-}
+  // const [N, inputs] = getInputs();
+  // solve(N, inputs);
 
-function getInputs() {
-  const fs = require("fs");
-  const filepath =
-    process.platform === "linux" ? "/dev/stdin" : __dirname + "/input.txt";
-  const [N, ...inputs] = fs
-    .readFileSync(filepath)
-    .toString()
-    .trim()
-    .split("\n");
+  (async function processLineByLine() {
+    try {
+      const rl = readline.createInterface({
+        input: fs.createReadStream(filepath),
+        crlfDelay: Infinity,
+      });
 
-  return [N, inputs];
-}
+      let row = 0;
+      rl.on("line", (line) => {
+        if (line.length > 1) {
+          let [x, y, z] = line.split(" ");
 
-function solve(N, inputs) {
-  const maxDp = [
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
-  const minDp = [
-    [0, 0, 0],
-    [0, 0, 0],
-  ];
+          maxDp[row][0] =
+            Math.max(maxDp[1 - row][0], maxDp[1 - row][1]) + Number(x);
+          maxDp[row][1] =
+            Math.max(maxDp[1 - row][0], maxDp[1 - row][1], maxDp[1 - row][2]) +
+            Number(y);
+          maxDp[row][2] =
+            Math.max(maxDp[1 - row][1], maxDp[1 - row][2]) + Number(z);
 
-  let row = 0;
-  for (let i = 0; i < Number(N); ++i) {
-    let [x, y, z] = inputs[i].split(" ");
+          minDp[row][0] =
+            Math.min(minDp[1 - row][0], minDp[1 - row][1]) + Number(x);
+          minDp[row][1] =
+            Math.min(minDp[1 - row][0], minDp[1 - row][1], minDp[1 - row][2]) +
+            Number(y);
+          minDp[row][2] =
+            Math.min(minDp[1 - row][1], minDp[1 - row][2]) + Number(z);
+          row = 1 - row;
+        }
+      });
 
-    maxDp[row][0] = Math.max(maxDp[1 - row][0], maxDp[1 - row][1]) + Number(x);
-    maxDp[row][1] =
-      Math.max(maxDp[1 - row][0], maxDp[1 - row][1], maxDp[1 - row][2]) +
-      Number(y);
-    maxDp[row][2] = Math.max(maxDp[1 - row][1], maxDp[1 - row][2]) + Number(z);
+      await events.once(rl, "close");
 
-    minDp[row][0] = Math.min(minDp[1 - row][0], minDp[1 - row][1]) + Number(x);
-    minDp[row][1] =
-      Math.min(minDp[1 - row][0], minDp[1 - row][1], minDp[1 - row][2]) +
-      Number(y);
-    minDp[row][2] = Math.min(minDp[1 - row][1], minDp[1 - row][2]) + Number(z);
-    row = 1 - row;
-  }
-  console.log(Math.max(...maxDp[1 - row]), Math.min(...minDp[1 - row]));
+      // console.log("Reading file line by line with readline done.");
+      // const used = process.memoryUsage().heapUsed / 1024 / 1024;
+      // console.log(
+      //   `The script uses approximately ${Math.round(used * 100) / 100} MB`
+      // );
+
+      console.log(Math.max(...maxDp[1 - row]), Math.min(...minDp[1 - row]));
+    } catch (err) {
+      console.error(err);
+    }
+  })();
 }
