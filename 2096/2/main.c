b9cbb377cd6e2164a47d66c311c560c167b06b26
suyeon woo <woosean999@gmail.com>
2022-09-11 13:26:49 +0900

    2096 / C / 코드 정리

--- 2096/2/main.c
+++ 2096/2/main.c
@@ -31,45 +31,29 @@ int init(void)
 
 void solve(void) 
 {
-    int curRow = 1;
-    int prevRow = 0;
+    int row = 0;
     for (int i = 0; i < N; ++i) {
         for (int j = 0; j < 3; ++j) {
             scanf("%d", &nums[j]);
         }
 
-        if (i == 0) {
-            for (int j = 0; j < 3; ++j) {
-                maxDp[i][j] = nums[j];
-                minDp[i][j] = nums[j];
-            }
-            continue;
-        };
-        if (i % 2 == 1) {
-            curRow = 1;
-            prevRow = 0;
-        } else {
-            curRow = 0;
-            prevRow = 1;
-        }
+        maxDp[row][0] = MAX(maxDp[1-row][0], maxDp[1-row][1]) + nums[0];
+        maxDp[row][1] = MAX(
+            MAX(maxDp[1-row][0], maxDp[1-row][1]), maxDp[1-row][2]
+        ) + nums[1];
+        maxDp[row][2] = MAX(maxDp[1-row][1], maxDp[1-row][2]) + nums[2];
 
-        maxDp[curRow][0] = MAX(maxDp[prevRow][0], maxDp[prevRow][1]) + nums[0];
-        maxDp[curRow][2] = MAX(maxDp[prevRow][1], maxDp[prevRow][2]) + nums[2];
-        int tempMax = MAX(maxDp[prevRow][0], maxDp[prevRow][1]);
-        maxDp[curRow][1] = MAX(tempMax, maxDp[prevRow][2]) + nums[1];
+        minDp[row][0] = MIN(minDp[1-row][0], minDp[1-row][1]) + nums[0];
+        minDp[row][1] = MIN(
+            MIN(minDp[1-row][0], minDp[1-row][1]), minDp[1-row][2]
+        ) + nums[1];
+        minDp[row][2] = MIN(minDp[1-row][1], minDp[1-row][2]) + nums[2];
 
-        minDp[curRow][0] = MIN(minDp[prevRow][0], minDp[prevRow][1]) + nums[0];
-        minDp[curRow][2] = MIN(minDp[prevRow][1], minDp[prevRow][2]) + nums[2];
-        int tempMin = MIN(minDp[prevRow][0], minDp[prevRow][1]);
-        minDp[curRow][1] = MIN(tempMin, minDp[prevRow][2]) + nums[1];
+        row = 1 - row;
     }
 
-    if (N == 1) curRow = 0;
-    int maxScore = MAX(maxDp[curRow][0], maxDp[curRow][1]);
-    maxScore = MAX(maxScore, maxDp[curRow][2]);
-    
-    int minScore = MIN(minDp[curRow][0], minDp[curRow][1]);
-    minScore = MIN(minScore, minDp[curRow][2]);
+    int maxScore = MAX(MAX(maxDp[1-row][0], maxDp[1-row][1]), maxDp[1-row][2]);
+    int minScore = MIN(MIN(minDp[1-row][0], minDp[1-row][1]), minDp[1-row][2]);
 
     printf("%d %d", maxScore, minScore);
 }
