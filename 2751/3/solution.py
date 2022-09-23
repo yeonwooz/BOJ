39a0e2ee1da3261d4f76f81686e7f7f65ceab457
suyeon woo <woosean999@gmail.com>
2022-09-23 23:46:39 +0900

    sys.stdin 으로 인풋을 받으면 기본 sort함수도 통과한다

--- 2751/3/solution.py
+++ 2751/3/solution.py
@@ -1,11 +1,11 @@
-import sys
+import sys 
 N = int(sys.stdin.readline())
 
-list = [0] * 2000001
+list = []
 for i in range(N):
     num = int(sys.stdin.readline())
-    list[num + 1000000] = 1
+    list.append(num)
 
-for i in range(2000001):
-    if list[i] == 1:
-        print(i - 1000000)
+list.sort()
+for num in list:
+    print(num)
