82b76fb01afbce4bf456c0366435765e9992916a
suyeon woo <woosean999@gmail.com>
2022-09-24 10:14:33 +0900

    1181 단어 정렬 / python / sort(key=len)

--- 1181/3/solution.py
+++ 1181/3/solution.py
@@ -1,23 +1,5 @@
 import sys
 
-def quick_sort(array, start, end):
-    if start >= end: return
-    pivot = start
-    left, right = start + 1, end
-
-    while left <= right:
-        while left <= end and len(array[left]) <= len(array[pivot]):
-            left += 1
-        while right > start and len(array[right]) >= len(array[pivot]):
-            right -= 1
-        if left > right:
-            array[right], array[pivot] = array[pivot], array[right]
-        else:
-            array[right], array[left] = array[left], array[right]
-    quick_sort(array, start, right - 1)
-    quick_sort(array, right + 1, end)
-
-
 N = int(sys.stdin.readline())
 
 list = []
@@ -25,7 +7,8 @@ for i in range(N):
     word = sys.stdin.readline().rstrip()
     list.append(word)
 
-quick_sort(list, 0, len(list) - 1)
+list.sort()
+list.sort(key = len)
 
 for i in range(len(list)):
     if i == 0 or list[i-1] != list[i]:
