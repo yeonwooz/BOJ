32d1d362a7dc1a38b0c03a00ccfc457fbf1f53b6
suyeon woo <woosean999@gmail.com>
2022-09-24 11:55:59 +0900

    [WIP]

--- 10971/1/solution.py
+++ 10971/1/solution.py
@@ -1,34 +1,24 @@
 import sys
 
-global min_costs
-min_costs = 1000001
-
-def recur(N, matrix, visited, v, cur_cost):
-    global min_costs
-    if len(visited) == N:
-        cur_cost += matrix[v][visited[0]]
-        if min_costs > cur_cost:
-            min_costs = cur_cost
-        return
-
-    for j in range(len(matrix[v])) :
-        if v != j and matrix[v][j] > 0 and matrix[v][j] <= min_costs  and j not in visited:
-            visited.append(j)
-            cur_cost += matrix[v][j]
-            recur(N, matrix, visited, j, cur_cost)
-            visited.pop()
-
 N = int(sys.stdin.readline())
-matrix = []
-for i in range(N):
-    matrix.append(list(map(int, sys.stdin.readline().split())))
+matrix = [list(map(int,sys.stdin.readline().split())) for i in range(N)]
 
-for i in range(N):
-    visited = [i]
-    for j in range(N) :
-        if i != j and matrix[i][j] > 0 and matrix[i][j] <= min_costs and j not in visited:
-            visited.append(j)
-            recur(N, matrix, visited, j, matrix[i][j])
-            visited.pop()
+visited = [False for _ in range(N)]
 
-print(min_costs)
+min_cost = sys.maxsize
+
+def recur(start, cur, cost):
+    global matrix, visited, min_cost
+    
+    if start == cur and visited.count(False) == 0:
+        min_cost = min(min_cost, cost)
+        return 
+
+    for i in range(N) :
+        if not matrix[cur][i] == 0 and not visited[i]:
+            visited[i] = True
+            recur(start, i, cost + matrix[cur][i])
+            visited[i] = False
+
+recur(0, 0, 0)
+print(min_cost)
