641cb5f03d1c687f3a879d9085066aa6b76f0c71
suyeon woo <woosean999@gmail.com>
2022-09-25 21:31:36 +0900

    10971 외판원 순회 2 / python / 최적의 사이클은 어느 정점에서 시작하든 동일하므로 시작점은 임의로 정해도 된다

--- 10971/2/solution.py
+++ 10971/2/solution.py
@@ -6,26 +6,32 @@ for i in range(len(matrix)):
     matrix[i] = list(map(int, matrix[i].rstrip().split()))
 
 min_cost = 1000000 * N + 1
+cur_cost = 0
+start = 0
+visited = [False] * N
+visited[0] = True
 
-def travel(visited, v, start, cur_cost, has_started):
+def travel(depth, cur, cur_cost):
     global min_cost
-    print(min_cost, cur_cost)
-    if has_started and v == start:
-        # 두번째 점 이후에 시작점으로 돌아왔으면 무조건 종료하되,
-        if False not in visited:
-            # 모든 도시를 돌았을 때만 순회한 경우로 카운트
+    global start
+
+    if depth == N:
+        if matrix[cur][start]:
+            cur_cost += matrix[cur][start]
             min_cost = min(min_cost, cur_cost)
         return
-    
-    for i in range(N):
-        if v != i and visited[i] == False and matrix[v][i] > 0 and matrix[v][i] <= min_cost:
-            visited[i] = True
-            cur_cost += matrix[v][i]
-            travel(visited, i, start, cur_cost, True)
-            visited[i] = False
 
-visited = [False] * N
-travel(visited, 0, 0, 0, False)
+    for next in range(N):
+        if matrix[cur][next]:
+            if visited[next] == False:
+                cur_cost += matrix[cur][next]
+                visited[next] = True
+                travel(depth + 1, next, cur_cost)
+                visited[next] = False
+                cur_cost -= matrix[cur][next]
+
+
+travel(1, start, cur_cost)
 # 최적경로 사이클을 찾는 것이기 때문에, 모든 정점 탐색할 필요 없이 아무 정점에서나 시작해도 된다. 어느 정점에서 시작하든 최적 사이클은 똑같다.
 
 print(min_cost)
