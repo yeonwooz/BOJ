49a75ec3a8e7ef36a9a3968beb766b0603a56452
suyeon woo <woosean999@gmail.com>
2022-09-25 21:03:09 +0900

    [WIP] 10971

--- 10971/2/solution.py
+++ 10971/2/solution.py
@@ -7,25 +7,25 @@ for i in range(len(matrix)):
 
 min_cost = 1000000 * N + 1
 
-def travel(visited, v, cur_cost, start):
+def travel(visited, v, start, cur_cost, has_started):
     global min_cost
-    if cur_cost > 0 and v == start:
-        cur_cost += matrix[v][start]
-        min_cost = min(min_cost, cur_cost)
+    print(min_cost, cur_cost)
+    if has_started and v == start:
+        # 두번째 점 이후에 시작점으로 돌아왔으면 무조건 종료하되,
+        if False not in visited:
+            # 모든 도시를 돌았을 때만 순회한 경우로 카운트
+            min_cost = min(min_cost, cur_cost)
         return
     
     for i in range(N):
         if v != i and visited[i] == False and matrix[v][i] > 0 and matrix[v][i] <= min_cost:
-            visited.append(i)
+            visited[i] = True
             cur_cost += matrix[v][i]
-            travel(visited, i, cur_cost, start)
-            visited.pop()
+            travel(visited, i, start, cur_cost, True)
+            visited[i] = False
 
-visited = []
-for i in range(N):
-    visited.append(i)
-    start = i
-    travel(visited, i, 0, start)
-    visited.pop()
+visited = [False] * N
+travel(visited, 0, 0, 0, False)
+# 최적경로 사이클을 찾는 것이기 때문에, 모든 정점 탐색할 필요 없이 아무 정점에서나 시작해도 된다. 어느 정점에서 시작하든 최적 사이클은 똑같다.
 
 print(min_cost)
