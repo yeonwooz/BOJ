4d8bc13b390dc3788bfc899a52a85fdb15e607cb
suyeon woo <woosean999@gmail.com>
2022-09-25 20:13:06 +0900

    1914 하노이 탑 / python / 하노이 결과값은 공식으로 구해야 시간초과 나지 않음

--- 1914/2/solution.py
+++ 1914/2/solution.py
@@ -1,21 +1,20 @@
 import sys
+import math
 N = int(sys.stdin.readline().strip())
 
 answer = ""
-def Hanoi(disc_cnt, start, mid, end):
+def print_Hanoi(disc_cnt, start, mid, end):
     global answer
     if disc_cnt == 1:
-        if N <= 20:
-            answer += str(start) + " " + str(end) + "\n"
+        print(start, end)
         return 1
 
-    move = Hanoi(disc_cnt - 1, start, end, mid)
+    move = print_Hanoi(disc_cnt - 1, start, end, mid)
+    print(start, end)
     move += 1
-    if N <= 20:
-        answer += str(start) + " " + str(end) + "\n"
-    move += Hanoi(disc_cnt - 1, mid, start, end)
+    move += print_Hanoi(disc_cnt - 1, mid, start, end)
     return move
 
-print(Hanoi(N, 1, 2, 3))
+print(math.floor(math.pow(2, N))- 1)
 if N <= 20:
-    print(answer)
+    print_Hanoi(N,1,2,3)
