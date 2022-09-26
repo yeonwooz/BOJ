32ad299abdcbb53418ef0ed1cb410278a6447694
suyeon woo <woosean999@gmail.com>
2022-09-26 19:51:15 +0900

    버블소트

--- sorts/python/bubble.py
+++ sorts/python/bubble.py
@@ -1,9 +1,21 @@
-list = [6,4,3,7,1,9,8]
+import time
 
-n = len(list)
-for i in range(0, n):
-    for j in range(n-1, i, -1): # 역순 탐색하려면 음수 간격을 지정 
-        if list[i] > list[j]:
-            list[i], list[j] = list[j], list[i]
+def b_sort(list):
+    n = len(list)
+    for i in range(0, n):
+        for j in range(n-1, i, -1): # 역순 탐색하려면 음수 간격을 지정 
+            if list[i] > list[j]:
+                list[i], list[j] = list[j], list[i]
 
-print(list)
+if __name__ == "__main__":
+    ls = [1] * 1000
+    for i in range(len(ls)):
+        ls[i] *= (len(ls) - i)
+
+    start = time.time()
+    sorted = b_sort(ls)
+    print(ls)
+    end = time.time()
+
+    print(f"{end - start:.5f} sec")
+
