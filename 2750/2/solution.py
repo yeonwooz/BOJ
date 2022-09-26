de37de3135da92226933ddd79dbbecd5b3083c20
suyeon woo <woosean999@gmail.com>
2022-09-26 22:57:51 +0900

    [temp] 선택정렬 프린트 추가

--- 2750/2/solution.py
+++ 2750/2/solution.py
@@ -5,9 +5,13 @@ def selection_sort(ls):
     n = len(ls)
     for i in range(n - 1):
         min = i
+        print(f"i가 {i}일 때")
         for j in range(i + 1, n):
+            print(j)
+            print(f"[min]={ls[min]}", f"[i]={ls[i]}", f"[j]={ls[j]}", ls)
             if ls[j] < ls[min]:
                 min = j
+        print(f"[{i}], [{min}] 교체\n")
         ls[i], ls[min] = ls[min], ls[i]
     return
 
