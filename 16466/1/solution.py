22e1d6a1eb46df714a445bcbc2664913eb5ba9f8
suyeon woo <woosean999@gmail.com>
2022-09-28 15:42:50 +0900

    [WIP] 카운팅소트

--- 16466/1/solution.py
+++ 16466/1/solution.py
@@ -1,20 +1,17 @@
 import sys
 
-possible_min = 1
-
-def get_min(ls):
-    global possible_min
-    n = len(ls)
-    for i in range(n - 1):
-        for j in range(n - 1, i, -1):
-            if ls[i] > ls[j]:
-                ls[i], ls[j] = ls[j], ls[i]
-            if ls[i] == possible_min:
-                possible_min += 1
+counts = [0] * 1000001
 
 def solve(N, nums):
-    get_min(nums)
-    print(possible_min)
+    global counts # 참조객체지만 명시적으로 전역변수 사용함을 선언
+
+    for num in nums:
+        counts[num] += 1
+
+    for i in range(1, N + 1):
+        if counts[i] == 0:
+            print(i)
+            break
 
 def main():
     N = int(sys.stdin.readline().rstrip())
