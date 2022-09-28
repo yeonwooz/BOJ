ae14a5b14820e48e1460e67965a0143136a90561
suyeon woo <woosean999@gmail.com>
2022-09-28 23:14:31 +0900

    14613 너의 티어는 ? / python / 브루트포스, 기댓값

--- 14613/1/solution.py
+++ 14613/1/solution.py
@@ -1,26 +1,22 @@
 import sys
-import math
+from math import comb
 
 W, L, D = map(float, sys.stdin.readline().split())
-# print(W,L,D)
 
 cur = 2000
-tiers = [0] * 5
+tiers_prob = [0] * 5
 
 for win in range(21):
-    for lose in range(21):
-        for draw in range(21):
-            if win + lose + draw == 20:
-                new_score = cur + 50 * W * win - 50 * L * lose
-                # 기댓값 = 확률 * 이득
-                if 1000 <= new_score and new_score < 1500:
-                    tiers[0] += 1
-                elif 1500 <= new_score and new_score < 2000:
-                    tiers[1] += 1
-                elif 2000 <= new_score and new_score < 2500:    
-                    tiers[2] += 1
-                elif 2500 <= new_score and new_score < 3000:    
-                    tiers[3] += 1
-                else:
-                    tiers[4] += 1
-print(tiers)
+    for lose in range(21 - win):
+        score = cur + 50 * win - 50 * lose
+        prob = W ** win * L ** lose * D ** (20 - win - lose) * comb(20, win) * comb(20 - win, lose)
+
+        if 1000 <= score < 1500: tiers_prob[0] += prob
+        elif 1500 <= score < 2000: tiers_prob[1] += prob
+        elif 2000 <= score < 2500: tiers_prob[2] += prob
+        elif 2500 <= score < 3000: tiers_prob[3] += prob
+        elif 3000 <= score < 3500: tiers_prob[4] += prob
+
+# print("\n".join(str(round(s, 8)) for s in tiers_prob))
+for prob in tiers_prob:
+    print(f"{prob:.8f}")
