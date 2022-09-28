23fc1623840da96019c9fc4a9192cb9e51bf00ab
suyeon woo <woosean999@gmail.com>
2022-09-28 11:54:25 +0900

    WIP

--- 3221/1/solution.py
+++ 3221/1/solution.py
@@ -1,14 +1,28 @@
 # 3221 개미의 이동
 import sys
+import copy
 
 # L기준으로 방향을 바꾸고 나서 현 시점의 개미의 정렬을 확인
 def move(T, ants_at_T, L):
     for time in range(1, T):
         prev_pos = ants_at_T[time - 1]
-        # if (prev_pos[1] == L || prev_pos[1] == 0):
-        #     ants_at_T[time - 1] = []
-        # else:
+        ants_at_T[time] = copy.deepcopy(prev_pos)
 
+        for idx in range(len(ants_at_T[time])):
+            pos = ants_at_T[time][idx]
+            if pos[1] == L or pos[1] == 0:
+                # 양끝점 도달
+                if pos[2] == 'D':
+                    ants_at_T[time][idx][2] = 'L'
+                    ants_at_T[time][idx][1] -= 1
+                else:
+                    ants_at_T[time][idx][2] = 'D'
+                    ants_at_T[time][idx][1] += 1
+            else:
+                if pos[2] == 'D':
+                    ants_at_T[time][idx][1] += 1
+                else:
+                    ants_at_T[time][idx][1] -= 1
 
 
 if __name__ == "__main__":
@@ -16,13 +30,16 @@ if __name__ == "__main__":
     # L은 끝점
     # T초 후에 각 개미의 위치는?
     N = int(sys.stdin.readline().rstrip())
-    ants_at_T = [[]] * T # 시간대별 [개미번호, 위치, 방향, 플래그]
+    ants_at_T = [] * T # 시간대별 [개미번호, 위치, 방향]
+    for i in range(T):
+        ants_at_T.append([])
+        
     for i in range(N):
         pos, toward = sys.stdin.readline().split()
-        # 네번째 인자: 방향 바꿔야 하는 플래그
-        ants_at_T[0].append([i+1, int(pos), toward, False])
-    print('a', ants_at_T)
-    # move(T, ants_at_T, L)
+        ants_at_T[0].append([i+1, int(pos), toward])
 
+    print(ants_at_T)
+    move(T, ants_at_T, L)
+    print(ants_at_T)
 
 
