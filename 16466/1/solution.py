0f645ed85ad0913f6420133a513c2fea6302beb2
suyeon woo <woosean999@gmail.com>
2022-09-28 15:26:57 +0900

    [WIP]16466 콘서트 / python / 버블소트 / 시간초과

--- 16466/1/solution.py
+++ 16466/1/solution.py
@@ -2,35 +2,18 @@ import sys
 
 possible_min = 1
 
-def q_sort_and_get_min(ls, left, right):
+def get_min(ls):
     global possible_min
-
-    pl = left
-    pr = right
-    pivot = ls[(left + right) // 2]
-
-    while pl <= pr:
-        while ls[pl] < pivot:
-            if ls[pl] == possible_min:
+    n = len(ls)
+    for i in range(n - 1):
+        for j in range(n - 1, i, -1):
+            if ls[i] > ls[j]:
+                ls[i], ls[j] = ls[j], ls[i]
+            if ls[i] == possible_min:
                 possible_min += 1
-            pl += 1
-
-        while ls[pr] > pivot:
-            pr -= 1
-        
-        if pl <= pr:
-            if ls[pl] == possible_min:
-                possible_min += 1
-            ls[pl], ls[pr] = ls[pr], ls[pl]
-            pl += 1
-            pr -= 1
-
-    if left < pr : q_sort_and_get_min(ls, left, pr)
-    if pl < right: q_sort_and_get_min(ls, pl, right)
 
 def solve(N, nums):
-    q_sort_and_get_min(nums, 0, len(nums) - 1)
-    print(nums)
+    get_min(nums)
     print(possible_min)
 
 def main():
