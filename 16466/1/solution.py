f6b5580990e2ee4e29cf4ba1ed9d0ab3866d1843
suyeon woo <woosean999@gmail.com>
2022-09-28 15:50:32 +0900

    빈 자리 없는 경우 예외처리 추가

--- 16466/1/solution.py
+++ 16466/1/solution.py
@@ -1,21 +1,19 @@
 import sys
 
-counts = [0] * 1000001
+# counts = [False] * 1000001
 
 def solve(N, nums):
-    global counts # 참조객체지만 명시적으로 전역변수 사용함을 선언
-
-    for num in nums:
-        counts[num] += 1
-
-    for i in range(1, N + 1):
-        if counts[i] == 0:
+    # global counts # 참조객체지만 명시적으로 전역변수 사용함을 선언
+    for i in range(1, N+1):
+        if nums[i-1] != i:
             print(i)
-            break
-
+            sys.exit()
+    print(N+1)
+    
 def main():
     N = int(sys.stdin.readline().rstrip())
     nums = list(map(int, sys.stdin.readline().split()))
+    nums.sort()
     solve(N, nums)
 
 if __name__ == "__main__":
