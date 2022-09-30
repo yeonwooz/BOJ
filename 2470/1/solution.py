1b6cf009fca86fbe9f1303a349b29c8397d9a98d
suyeon woo <woosean999@gmail.com>
2022-09-30 16:00:43 +0900

    2470 두 용액 / 두 포인터 / python

--- 2470/1/solution.py
+++ 2470/1/solution.py
@@ -28,29 +28,27 @@ q_sort(arr, 0, N - 1)
 # print(arr) # [-99, -2, -1, 4, 98]
 
 # binary search
-sum_nearest_zero = 1e9
+sum_nearest_zero = 1000000000  * 100000 + 1 # 최댓값 주의
 answer = []
 if N == 2: 
     sum_nearest_zero = arr[0] + arr[1]
-    answer = [arr[0] + arr[1]]
+    answer = [arr[0], arr[1]]
 else:
     l_ptr = 0
     r_ptr = N - 1
     while l_ptr < r_ptr:
         sum = arr[l_ptr] + arr[r_ptr]
         
-        if abs(sum) < abs(sum_nearest_zero):
-            sum_nearest_zero = sum
+        if abs(sum) < sum_nearest_zero:
+            sum_nearest_zero = abs(sum)
             answer = [arr[l_ptr], arr[r_ptr]]
-        else:
-            break
 
-        mid = (l_ptr + r_ptr) // 2
         if sum == 0:
             answer = [arr[l_ptr], arr[r_ptr]]
+            break
         elif sum > 0:
-            r_ptr = arr[mid] - 1
+            r_ptr -= 1
         else:
-            l_ptr = arr[mid] - 1
+            l_ptr += 1
 
-print(answer)
+print(" ".join(str(s) for s in answer))
