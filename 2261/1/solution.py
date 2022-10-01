50f5b6e87f9b4ace325bf4953e101fcf0d8aee49
suyeon woo <woosean999@gmail.com>
2022-10-01 10:53:01 +0900

    2261 가장 가까운 두 점 / 파이썬

--- 2261/1/solution.py
+++ 2261/1/solution.py
@@ -2,19 +2,35 @@
 import sys
 # 두 점의 (x좌표차이) ** 2 + (y좌표차이) **2 중 최소
 
-def recur(dots, visited, cur, depth, n):
-    global min_distance
-    if n == depth:
-        return
-    
-    for v in range(n):  
-        if cur != v and visited[v] == False:
-            distance = abs(dots[v]['x'] - dots[cur]['x']) ** 2 + abs(dots[v]['y'] - dots[cur]['y']) ** 2
-            min_distance = min (min_distance, distance)
-            visited[v] = True
-            recur(dots, visited, v, depth + 1, n)
-            visited[v] = False
+def getDist(p1, p2):
+    return (p1['x'] - p2['x'])**2 + (p1['y'] - p2['y'])**2
 
+def dac(start, end):
+    if start == end:
+        return 0
+    if end - start == 1:
+        return getDist(dots[start], dots[end])
+
+    # 분할정복 : 왼쪽영역, 오른쪽영역 
+    mid = (start + end) //2
+    min_distance = min(dac(start, mid), dac(mid, end))
+
+    targets = []
+    for i in range(start, end + 1):
+        if (dots[mid]['x'] - dots[i]['x']) ** 2 < min_distance: 
+            # x_diff가 최소거리보다 작아서 일단 후보에 등록
+            targets.append(dots[i])
+    
+    targets = sorted(targets, key=lambda d: d['y'])
+
+    tl = len(targets)
+    for i in range(tl - 1):
+        for j in range(i + 1, tl):
+            if (targets[i]['y'] - targets[j]['y']) ** 2 < min_distance:
+                min_distance = min(min_distance, getDist(targets[i],targets[j]))
+            else:
+                break
+    return min_distance
 
 if __name__ == "__main__":
     n = int(sys.stdin.readline())
@@ -25,13 +41,11 @@ if __name__ == "__main__":
             'x': x,
             'y': y
         })
+    dots = sorted(dots, key=lambda d: d['x'])
+
     global min_distance 
     min_distance = (20000 ** 2) * 2
 
-    for i in range(n):
-        visited = [False] * n
-        visited[i] = True
-        recur(dots, visited, i, 1, n)
-        visited[i] = False
+     # index
 
-    print(min_distance)
+    print(dac(0, n-1))
