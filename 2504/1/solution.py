330f45770685ba50c31a0269a2af9ce7ae2cc0f3
suyeon woo <woosean999@gmail.com>
2022-10-01 15:42:38 +0900

    2504 괄호의 값 / python / 스택 /  문자열 받을 때 마지막 개행 주의

--- 2504/1/solution.py
+++ 2504/1/solution.py
@@ -1,65 +1,41 @@
 #started at 2:12
 import sys
-global pairs
-pairs = {
-    '(':')',
-    '[':']'
-}
-
-def validate(pars):
-    stack = []
-    top = -1
-    # print(pars)
-    for i in range(len(pars)):
-        if pars[i] == '(' or pars[i] == '[':
-            stack.append(pars[i])
-            top += 1
-        else: 
-            if top == -1:
-                # print("====1====")
-                return False
-            
-            if pars[i] == ')' and pars[i-1] == '[' or pars[i] == ']' and pars[i-1] =='(':
-                # print("====2====")
-                return False
-            
-            stack.append(pars[i])
-            top += 1
-    return top % 2 == 0 # 하나 더 올라간 채로 끝나기 때문에
-
-def calc(pars_arr, cnt):
-    if len(pars_arr) == 0:
-        # 문제 조건 상 맨 처음에 길이가 0인 경우는 없다 
-        return cnt 
-    if pars_arr[0] in pairs and pairs[pars_arr[0]] == pars_arr[-1]:
-        if pars_arr[0] == '(':
-            cnt *= 2
-        else:
-            cnt *= 3
-        pars_arr.pop(0)
-        pars_arr.pop()
-        return calc(pars_arr, cnt)
-
-    for i in range(len(pars_arr)):
-        sliced = pars_arr[:i]
-        if validate(sliced):
-            sum = calc(sliced, cnt) + calc(pars_arr[i:], cnt)
-            cnt *= sum
-            return cnt
-    return cnt
 
 def solve(pars):
-    is_valid = validate(pars)
-    if is_valid == False:
+
+    stack = []
+    answer = 0
+    tmp = 1 # 연산결과 임시 저장공간
+    for i in range(len(pars)):
+        if pars[i] == '(':
+            stack.append(pars[i])
+            tmp *= 2
+        elif pars[i] == '[':
+            stack.append(pars[i])
+            tmp *= 3
+        elif pars[i] == ')':
+            if not stack or stack[-1] == '[': # 스택이 비어있거나 마지막이 '[' 이라면
+                answer = 0
+                break
+            if pars[i - 1] == '(': 
+                answer += tmp
+            stack.pop()
+            tmp //= 2 # 이미 정답결과에 연산되었기 떄문에 다시 나누어서  원래상태로 되돌림
+        else:
+            if not stack or stack[-1] == '(':
+                answer = 0
+                break
+            if pars[i - 1] == '[':
+                answer += tmp
+            stack.pop()
+            tmp //= 3
+            
+    if stack: # 스택에 괄호가 남아있다면 올바른 괄호가 아니다
         return 0
-    # return calc(pars.split(''), 1) # 구분자가 이미 문장 내에 존재할 때만 split 활용가능
-    arr = list(pars)
-    arr.pop()
-    return calc(arr, 1)
+    return answer
 
 if __name__ == "__main__":
-    pars = sys.stdin.readline()
+    pars = sys.stdin.readline().rstrip()
     print(solve(pars))
 
-
-#finished at 
+#finished at 3:35
