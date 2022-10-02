8e5c98d2762d669023a610b7683b0dd4a0bb8f75
suyeon woo <woosean999@gmail.com>
2022-10-02 11:32:08 +0900

    [WIP]2812 틀린 이유 찾는 중

--- 2812/1/solution.py
+++ 2812/1/solution.py
@@ -1,29 +1,16 @@
 #started at 10:17
 N, K = map(int, input().split(' '))
-arr = list(map(int, input()))
+s = list(input())
 
 stack = []
-idx = 0
-while idx < N - 1:
-    print(stack)
-    if K == 0: break
-    if arr[idx] < arr[idx + 1]:
-        idx += 1
-        continue
-    else:
-        if not stack: 
-            stack.append(arr[idx])
-        elif stack and stack[-1] < arr[idx]:
-            stack.pop()
-            stack.append(arr[idx])
-        else:
-            stack.append(arr[idx])
-        K -= 1        
-    idx += 1
+for i in range(N):
+    while K > 0 and stack and stack[-1] < s[i]:
+        # stack에 값이 있는데 이번에 넣을 수보다 작다면 빼준 후에 이번 숫자를 넣어준다
+        # K > 0 조건도 while 문 안에 같이 있어야 함
+        stack.pop()
+        K -= 1
+    stack.append(s[i])
 
+print("".join(stack))
 
-
-print(stack)
-
-
-#finished at 
+#finished at 11:27
