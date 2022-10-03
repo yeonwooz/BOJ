6b18f3a498cef72d386e35dc5f3c694eb4640ac6
suyeon woo <woosean999@gmail.com>
2022-10-03 22:57:54 +0900

    6549 히스토그램에서 가장 큰 직사각형 / python / 스택

--- 6594/1/solution.py
+++ 6594/1/solution.py
@@ -1,28 +1,43 @@
 #started at 9:38
+# https://hooongs.tistory.com/330
 import sys
 from collections import deque
 
-
-
-def solve(n, ls):
-    # 너비들을 배열로 관리
-    min_height = 1000000000
+def solve(n, rec):
     stack = deque()
-    while len(ls) > 0:
-        popped =  ls.popleft()
-        stack.append(popped)
+    answer = 0
 
-    print(stack)
+    for i in range(n):
+        # 스택에 세로로 채울 것
+        # 스택에 마지막으로 차있는 값은 현재까지 탐색한 rec의 값중 가장 큰 값이다
+        while len(stack) > 0 and rec[stack[-1]] > rec[i]:
+            tmp = stack.pop()
+            
+            if len(stack) == 0:
+                width = i
+            else:
+                width = i - stack[-1] - 1
+            answer =  max(answer, width * rec[tmp])
+        stack.append(i)
+
+    while len(stack) > 0:
+        tmp = stack.pop()
+
+        if len(stack) == 0:
+            width = n
+        else:
+            width = n- stack[-1] - 1
+        answer = max(answer, width * rec[tmp])
+
+    print(answer)
 
 if __name__ == "__main__":
     while True:
         try:
-            inputs = list(map(int, sys.stdin.readline().split()))
-            n = inputs[0]
-            if n > 0:
-                solve(n, deque(inputs[1:]))
+            rec = list(map(int, sys.stdin.readline().split()))
+            n = rec.pop(0)
+            if n == 0:
+                break
+            solve(n, rec)
         except Exception as e:
-            print(e)
             break
-
-
