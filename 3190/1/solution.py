4ba751975a6311a9a2158f8107610c6331bb4952
suyeon woo <woosean999@gmail.com>
2022-10-03 15:38:18 +0900

    [WIP]

--- 3190/1/solution.py
+++ 3190/1/solution.py
@@ -1,54 +1,73 @@
 #started at 1:35
-#상하좌우 큐를 만들자
+# row_q, col_q
+import sys
+N = int(sys.stdin.readline())
+K = int(sys.stdin.readline())
+apples = []
+for _ in range(K):
+    r,c = map(int, sys.stdin.readline().split())
+    apples.append({'r':r, 'c':c})
 
-def crashed(cur_direction, ) -> bool:
-    if 
+apples.sort(key=lambda d:d['c'])
 
+change_cnt = int(sys.stdin.readline())
 
-if __name__ == "__main__":
-    N = int(input())
-    K = int(input())
-    apples = []
-    for _ in range(K):
-        r,c = map(int, input().split())
-        apples.append({'r':r, 'c':c})
-
-    apples.sort(key=lambda d:d['c'])
-
-    cd = int(input())
-    cmds = []
-
-
-    time = 0
-
-    pos = {'r':1, 'c':1}
-
-    prev_direction = 1
-    cur_direction = 1
-    q1 = [pos] # 궤적을 기억하거나 자르면서 진행
-    q2 = []
-    q3 = []
-    q4 = []
-
-    for _ in range(K):
-        X, C = input().split()
+pos = {'r':1, 'c':1}
+row_q = [1]
+col_q = []
+time = 0
+cur_direction = 1
+crashed = False
+for i in range(change_cnt):
+        X, C = sys.stdin.readline().split()
         X = int(X)
         while time <= X:
-            time += 1
+            time += 1 # 시간이 1초 흘러서 움직인다
 
             if cur_direction == 1:
-                pos['c'] += 1
-            elif cur_direction == 2:
-                pos['r'] += 1
-            elif cur_direction == 3:
-                pos['c'] -= 1
-            elif cur_direction == 4:
-                pos['r'] -= 1
+                if pos['r'] + 1 > N and time < X: 
+                    # 아직 방향바꿀시간이 안됐는데 벽에 부딪힘 => 게임 끝
+                    crashed = True
+                    break
+                elif time == X and (C == 'D' or C == 'L'):
+                    # 방향을 2번째방향으로 바꾸어서, 기존에 row_q가 있으면 뺴고, 없으면 쌓기 시작
+                    cur_direction = 2
+                    if row_q:
+                        row_q.pop()
+                    else:    
+                        row_q.append(pos['r'] + 1)
+                else:
+                    # 방향바꿀 시간이 안됐고, 벽에 부딪히지 않았음. 진행가능.
+                    # col_q를 쌓는다 
+                    col_q.append(pos['c'] + 1)
 
+
+            if cur_direction == 2:
+                if pos['c'] + 1 > N and time < X: 
+                    # 아직 방향바꿀시간이 안됐는데 벽에 부딪힘 => 게임 끝
+                    crashed = True
+                    break
+                elif time == X and (C == 'D' or C == 'L'):
+                    # 방향을 3번째방향으로 바꾸어서, 기존에 col_q가 있으면 뺴고, 없으면 쌓기 시작
+                    cur_direction = 3
+                    if row_q:
+                        row_q.pop()
+                    else:    
+                        row_q.append(pos['r'] + 1)
             
+                else:
+                    # 방향바꿀 시간이 안됐고, 벽에 부딪히지 않았음. 진행가능.
+                    # row_q 쌓는다 
+                    col_q.append(pos['r'] + 1)
 
-            if time == X:
-                print("special_t: ", time)
-            else:
-                print("general_t: ", time)
-            print("pos is", pos)
+
+
+
+            # TODO 사과 먹어서 pop(0) 하는 조건 추가
+
+            # TODO 벽을 만나거나, 4 -> 
+        if crashed == True:
+            break
+
+
+print(time)
