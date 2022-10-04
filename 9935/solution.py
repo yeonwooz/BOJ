0a0450e239e4a0082af5dc90ae6e5e98d51f6e11
suyeon woo <woosean999@gmail.com>
2022-10-04 15:10:59 +0900

    [WIP] 9935 문자열 폭발

--- 9935/solution.py
+++ 9935/solution.py
@@ -11,25 +11,36 @@ answer = "FRULA"
 stack = deque()
 for i in range(len(s)) :
     cha = s[i]
-    if  boom_ptr < len(boom)  and boom[boom_ptr + 1] == cha:
+    # print("cha", cha, 'boom_ptr', boom_ptr, 'boom[boom_ptr + 1]', boom[boom_ptr + 1])
+
+    if  boom_ptr < len(boom) - 1 and boom[boom_ptr + 1] == cha:
         # 이번 문자가 폭발문자열의 다음 문자와 같다면, boom_ptr을 올려준다.
         boom_ptr += 1
+        # print("cut>>", cha, boom_ptr)
     stack.append(cha)
 
-    # 폭발문자열의 포인터가 마지막을 통과했다면 pop
-    if boom_ptr == len(boom):
-        boom_ptr = -1
-        for _ in range(boom_ptr):
+    # 폭발문자열의 포인터가 마지막을 통과했다면 stack에서 그만큼 pop
+    if boom_ptr + 1 == len(boom):
+        for _ in range(boom_ptr + 1):
             stack.pop()
+        boom_ptr = -1
+
+st_ptr = 0
+boom_ptr = -1
+while(st_ptr < len(stack)):
+    # 스택에 남아있는 값들 처리 
+    # print('stack[st_ptr]',stack[st_ptr], 'boom_ptr', boom_ptr, 'boom[boom_ptr]', boom[boom_ptr])
+    if stack[st_ptr] == boom[boom_ptr + 1]:
+        boom_ptr += 1
     
-    # while(stack):
-    #     if len(stack) >= len(boom) and 
-    # 기존 스택 값들 빼주기
-    # if stack[]
+    if boom_ptr + 1 == len(boom):
+        for _ in range(boom_ptr + 1):
+            stack.pop()
+        boom_ptr = -1  
 
+    st_ptr += 1
 
-
-
-
+if stack:
+    answer = "".join(stack)
 print(answer)
-#finished at
+#finished at 3:10 -> 시간초과
