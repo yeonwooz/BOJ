606cddab93809e6395d1702bb356b79f8e890f0e
suyeon woo <woosean999@gmail.com>
2022-10-05 20:01:00 +0900

    [WIP]

--- 2261/3/solution.py
+++ 2261/3/solution.py
@@ -4,7 +4,6 @@ import sys
 
 def recur(start, end):# end 포함
     global dots, min_distance
-    mid = (start + end) // 2
     if start == end:
         return 0
 
@@ -12,6 +11,7 @@ def recur(start, end):# end 포함
         d = (dots[start][0] - dots[end][0]) ** 2 + (dots[start][1] - dots[end][1]) ** 2 
         return min(min_distance, d)
 
+    mid = (start + end) // 2
     # 분할정복으로 두 영역 각각의 점들의 최소거리를 받아와서 둘중 최소값확인
     d1 = recur(start, mid)
     d2 = recur(mid, end)
@@ -21,13 +21,13 @@ def recur(start, end):# end 포함
     # mid랑 모든 점 거리 비교.
     # x기준 정렬된 것에서 후보 추림
     filtered = []
-    for i in range(start, end):
+    for i in range(start, end + 1):
         d = (dots[mid][0] - dots[i][0]) ** 2 + (dots[mid][1] - dots[i][1]) ** 2 
         if d <= min_distance:
             filtered.append(dots[i])
 
     #  y기준 정렬 후 min_distance 파악
-    filtered.sort(key=lambda l:l[1])
+    filtered = sorted(filtered, key=lambda l:l[1])
     for i in range(len(filtered) - 1):
         for j in range(i + 1, len(filtered)):
             d = (filtered[i][0] - filtered[j][0]) ** 2 + (filtered[i][1] - filtered[j][1]) ** 2 
@@ -43,7 +43,6 @@ for i in range(n):
     x, y = map(int, sys.stdin.readline().split())
     dots.append([x,y])
 
-dots.sort(key = lambda l:l[0])
-min_distance = 20000 ** 2 *2 
-min_distance = recur(0, n - 1)
-print(min_distance)
+dots = sorted(dots, key = lambda l:l[0])
+min_distance = (20000 ** 2) * 2 
+print(recur(0, n - 1))
