cc1a607957f99f8abd49ae96ae46e1bbf278559a
suyeon woo <woosean999@gmail.com>
2022-10-05 14:31:25 +0900

    8983 사냥꾼 / python / 이분탐색

--- 8983/3/solution.py
+++ 8983/3/solution.py
@@ -9,28 +9,33 @@ M,N,L = map(int, sys.stdin.readline().split())
 shooters = list(map(int, sys.stdin.readline().split()))
 shooters.sort()
 answer = 0
+
 for i in range(N):
     animal = list(map(int, sys.stdin.readline().split()))
     dx, dy = animal
 
+    if dy > L: continue
+
     start = 0
     end = M - 1
 
-    # Lower Bound: 가장 왼쪽사대?
     while start < end:
         mid = (start + end) // 2
 
-        if dx <= shooters[mid]:
-            # 더 왼쪽으로 가본다 
-            end = mid
+        if dx == shooters[mid]:
+            start = mid
+            break
+        if dx < shooters[mid]:
+            # 동물보다 오른쪽 좌표에 사대가 있다 -> 더 왼쪽에 있는지 탐색
+            end = mid - 1
         else:
-            # 오른쪽으로 가야 함
+            # 동물보다 왼쪽 좌표에 사대가 있다 -> 더 오른쪽에 있는지 탐색
             start = mid + 1
 
     # print(start, mid, end, dx)
         
     if catch(animal, shooters[start], L): answer += 1
-    elif start > 1 and catch(animal, shooters[start - 1], L): answer += 1
+    elif start > 0 and catch(animal, shooters[start - 1], L): answer += 1
     elif start < M - 1 and catch(animal, shooters[start + 1], L): answer += 1
 
 print(answer)
