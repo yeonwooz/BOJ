53369bedb95cafa4e4598c8d3d8dd6019092a25c
suyeon woo <woosean999@gmail.com>
2022-10-07 22:22:54 +0900

    [WIP]21606 아침산책 / 메모리 초과

--- 21606/1/solution.py
+++ 21606/1/solution.py
@@ -1,9 +1,10 @@
 #started at 5:02
 import sys
 input = sys.stdin.readline
-
+sys.setrecursionlimit(10 ** 6)
 
 def DFS(v, visited):
+    # print("visited", v)
     global A, cnt
     in_or_out = A[v]
     visited[v] = in_or_out  # 해당 점이 실내인지 실외인지 기록
@@ -31,10 +32,21 @@ for _ in range(N - 1):
     arr[v].append(u)
 
 cnt = 0
+iti_cnt = 0
 for start in range(1, N+1):
     visited = [False] * (N+1)
-    if A[start] == 1: 
-        # 시작점이 실내일 때만 DFS 시작
+    if A[start] == 0: 
+        # 시작점이 실외일 때만 DFS 시작
         DFS(start, visited)
+    else:
+        # print('start', start, )
+        # 시작점이 실내라면 실내로 가는 개수만 확인해서 *2해서 cnt에 더해주기
+        for end in range(1, N+1):
+            if start != end and end in arr[start] and A[end] == 1:
+                # print('end', end)
+                iti_cnt += 1
+        # cnt += iti_cnt * 2
 
-print(cnt)
+# print('iti_cnt', iti_cnt)
+
+print(cnt * (cnt - 1) + iti_cnt)
