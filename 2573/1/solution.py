8d77d332aa4b69d28ccfe389cfe5edf1da7a3a3c
suyeon woo <woosean999@gmail.com>
2022-10-08 19:55:46 +0900

    2573 / BFS 완성

--- 2573/1/solution.py
+++ 2573/1/solution.py
@@ -1,66 +1,54 @@
 import sys
 from collections import deque
 
-def visit_BFS(i, j):
-    global group_num
-    visited[i][j] = group_num
-    # queue =  deque()
-    # queue.append((i,j))
-
-    # while(queue):
-    #     r,c = queue.popleft()
-
-    #     for idx in range(4):
-    #         next_i = r+dr[idx]
-    #         next_j = c+dc[idx]
-    #         if next_i < 0 or next_j < 0 or next_i >= N or next_j >= M:
-    #             continue
-
-    #         if visited[next_i][next_j] == False and arr[next_i][next_j] == 0:
-    #             visit_BFS(next_i, next_j)
-
-def melt_BFS(i, j, visited):
-    global group_num
+def melt(i, j, visited):
     queue = deque()
-    queue.append((i, j))
-    seaList = []
+    queue.append((i,j))
 
+    visited[i].append(j)
+    melting_list = []
     while queue:
-        u,v = queue.popleft()
-
-        # 바다라면 스킵
-        if arr[u][v] == 0: continue
+        r,c =  queue.popleft()
         
-        # 빙산이면 바다 면적만큼 빼기 (음수전까지)
-        melt_cnt = 0
+        sea_cnt = 0
         for idx in range(4):
-            next_i = u + dr[idx]
-            next_j = v + dc[idx]
+            next_i = r + dr[idx]
+            next_j = c + dc[idx]
             if next_i < 0 or next_j < 0 or next_i >= N or next_j >= M:
                 continue
 
-            if visited[next_i][next_j] == False and arr[next_i][next_j] == 0:
-                melt_cnt += 1
-            elif visited[next_i][next_j] == False and arr[next_i][next_j] > 0:
-                queue.qppend((next_i, next_j))
-                visited[next_i][next_j] = 1
-        if melt_cnt > 0:
-            seaList.append((u,v,melt_cnt))
-    
-    for u,v,melt_cnt in seaList:
-        arr[u][v] = max(0, arr[u][v] - melt_cnt)
-                
+            if next_j not in visited[next_i]:
+                if arr[next_i][next_j] == 0:
+                    # 주변이 바다일 때
+                    sea_cnt += 1
+                else: 
+                    # 인접한 곳으로 이동할 수 있을 때, 더 갈수 있을때까지 이동
+                    # melt(next_i, next_j, visited)
+                    queue.append((next_i, next_j))
+                    visited[next_i].append(next_j)
+
+        if sea_cnt > 0:
+            melting_list.append((i,j, sea_cnt))
+
+    ## 인근 탐색이 끝나고 나면 빙산 높이를 깎는다 
+    for u,v,cnt in melting_list:
+        arr[u][v] = max(0, arr[u][v] - cnt)
+
     return 1
 
-def divided():
-    return
+
+
 
 N,M = map(int, sys.stdin.readline().split())
 
 arr = []
+ice = []
 for i in range(N):
     row = list(map(int, sys.stdin.readline().split()))
     arr.append(row)
+    for j in range(M):
+        if row[j] > 0: 
+            ice.append((i,j))
 
 year = 0
 group_num = 1
@@ -69,14 +57,15 @@ dc = [-1, 0, +1, 0]
 
 while True:
     print(arr)
-    # if divided():
-    #     break
+
     # if group_num >= 2: break
-    if  year == 2: break
+    if  year == 3: break
 
     year += 1
     # 1년 뒤 녹음
-    visited = [[False] * M for _ in range(N)]
-    group_num += melt_BFS(0,0, visited)
+    # visited = [[False] * M for _ in range(N)]
+    visited = [[] * M for _ in range(N)] 
+    for i, j in ice:
+        group_num += melt(i, j, visited)
     
 print(year)
