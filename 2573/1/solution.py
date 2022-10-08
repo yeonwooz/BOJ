7a19e6c30f2901914105bc17314db374375667de
suyeon woo <woosean999@gmail.com>
2022-10-08 16:58:22 +0900

    WIP

--- 2573/1/solution.py
+++ 2573/1/solution.py
@@ -19,28 +19,38 @@ def visit_BFS(i, j):
     #         if visited[next_i][next_j] == False and arr[next_i][next_j] == 0:
     #             visit_BFS(next_i, next_j)
 
-def melt(visited):
+def melt_BFS(i, j, visited):
     global group_num
+    queue = deque()
+    queue.append((i, j))
+    seaList = []
 
-    for i in range(N):
-        for j in range(M):
-            
-            # 바다라면 스킵
-            if arr[i][j] == 0: continue
-            
-            # 빙산이면 바다면적만큼 빼기 (음수전까지)
-            melt_cnt = 0
-            for idx in range(4):
-                next_i = i+dr[idx]
-                next_j = j+dc[idx]
-                if next_i < 0 or next_j < 0 or next_i >= N or next_j >= M:
-                    continue
-                if visited[next_i][next_j] == False and arr[next_i][next_j] == 0:
-                    melt_cnt += 1
-            
-            arr[i][j] = max(0, arr[i][j] - melt_cnt)
-            visit_BFS(i, j)
-            group_num += 1
+    while queue:
+        u,v = queue.popleft()
+
+        # 바다라면 스킵
+        if arr[u][v] == 0: continue
+        
+        # 빙산이면 바다 면적만큼 빼기 (음수전까지)
+        melt_cnt = 0
+        for idx in range(4):
+            next_i = u + dr[idx]
+            next_j = v + dc[idx]
+            if next_i < 0 or next_j < 0 or next_i >= N or next_j >= M:
+                continue
+
+            if visited[next_i][next_j] == False and arr[next_i][next_j] == 0:
+                melt_cnt += 1
+            elif visited[next_i][next_j] == False and arr[next_i][next_j] > 0:
+                queue.qppend((next_i, next_j))
+                visited[next_i][next_j] = 1
+        if melt_cnt > 0:
+            seaList.append((u,v,melt_cnt))
+    
+    for u,v,melt_cnt in seaList:
+        arr[u][v] = max(0, arr[u][v] - melt_cnt)
+                
+    return 1
 
 def divided():
     return
@@ -61,13 +71,12 @@ while True:
     print(arr)
     # if divided():
     #     break
-    if group_num >= 2: break
-    if  year ==2: break
+    # if group_num >= 2: break
+    if  year == 2: break
 
     year += 1
     # 1년 뒤 녹음
     visited = [[False] * M for _ in range(N)]
-    melt(visited)
+    group_num += melt_BFS(0,0, visited)
     
-
 print(year)
