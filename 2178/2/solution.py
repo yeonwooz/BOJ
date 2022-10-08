87b0328dac00a1a20b46094548eabfa8952d5173
suyeon woo <woosean999@gmail.com>
2022-10-08 23:16:28 +0900

    2178 미로탐색 / python / 최소경로 기록하면서 BFS 해야함

--- 2178/2/solution.py
+++ 2178/2/solution.py
@@ -15,17 +15,18 @@ def BFS(i,j):
             next_c = c + dy[i]
             if next_r < 0 or next_c < 0 or next_r >= N or next_c >= M: 
                 continue
+            if arr[next_r][next_c] == 0:
+                continue
 
-            if arr[next_r][next_c] == 1 and next_c not in visited[next_r]:
-                print(next_r, next_c)
-                cnt += 1
+            if next_c not in visited[next_r]:
+                arr[next_r][next_c] = arr[r][c] + 1    ## 현재까지의 최소경로 기록
                 q.append((next_r,next_c))
                 visited[next_r].append(next_c)
-    print(cnt)
+    print(arr[N-1][M-1])
 
 N,M = map(int, input().split())
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+dx = [0,+1,0,-1]
+dy = [+1,0,-1,0]
 
 arr = []
 visited = [[] * M for _ in range(N)]
