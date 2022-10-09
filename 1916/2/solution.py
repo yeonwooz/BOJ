dd5505ee4f336d048b86d538c102e452db288bb8
suyeon woo <woosean999@gmail.com>
2022-10-09 19:32:04 +0900

    WIP

--- 1916/2/solution.py
+++ 1916/2/solution.py
@@ -1,30 +1,30 @@
 #started at 6:05
-import sys
+import sys, heapq
 from collections import deque
 input = sys.stdin.readline
 
 def BFS(start):
-    q = deque()
-    q.append((start, 0))
-    visited[start] = True
+    q = []
+    heapq.heappush(q, (0, start))
     costs[start] = 0
-    # total = 0
-    while q:
-        v, cur_cost = q.popleft()
-        print('v', v,'cur_cost', cur_cost)
-        for to, cost in arr[v]:
-            print('to', to, 'cost', cost)
-            if visited[to] == False:
-                visited[to] = True
-                q.append((to, cost))
-                costs[to] = costs[v] + cost
-                print(costs)
 
+    while q:
+        v, cur_cost = heapq.heappop(q)
+
+        print(new_cost)
+        if costs[v] < cur_cost:
+            continue
+        
+        for to, cost in arr[v]:
+            new_cost = cur_cost + cost
+            if new_cost < costs[to]:
+                costs[to] = new_cost
+                heapq.heappush(q(new_cost, to))
 
 N = int(input())
 M = int(input())
 arr = [[] * (N+1) for _ in range(N+1)]
-costs = [0] * (N+1)
+costs = [int(1e9)] * (N+1)
 
 for _ in range(3, M+3):
     fr, to, cost = map(int, input().split())
