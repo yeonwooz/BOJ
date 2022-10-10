15634755d4e957ce2095d5c81fc2c23b4a5021f3
suyeon woo <woosean999@gmail.com>
2022-10-10 21:55:06 +0900

    [WIP] 2294 동전 2 / 인덱스 에러 반례 못찾는중

--- 2294/1/solution.py
+++ 2294/1/solution.py
@@ -1,52 +1,33 @@
 # DP로 풀고 , BFS로 풀기 
 #started at 7:30
-import sys,heapq
+import sys
 from collections import deque
 input = sys.stdin.readline
 MAX = 100
 
-def BFS(v):
+def BFS():
     global k, min_cnt, found_combination
-    if coins[v] == k:
-        found_combination = True
-        min_cnt = 1
-        return
-
-    q = deque()
-    check[coins[v]] = 1
-    q.append((v, 1, coins[v]))
+    q =  deque()
+    for i in range(n):
+        if coins[i] <= k and check[coins[i]] == 0:
+            check[coins[i]] = 1
+            q.append((i, 1, coins[i]))
 
     while q:
         popped, cnt, cur = q.popleft()
         if cur == k:
-            found_combination = True
-            min_cnt = min(min_cnt, cnt)
-            return
+            return min(min_cnt, cnt)
 
-        for i in range(len(coins)):
+        for i in range(n):
             if cur + coins[i] <= k and check[cur+coins[i]] == 0:
                 check[cur+coins[i]] = 1
                 q.append((i, cnt+1, cur + coins[i]))
+    
     return -1
 
-n,k = map(int, input().split())
-coins = set()
-
-for _ in range(n):
-    coins.add(input().rstrip())
-
-coins = sorted(coins, reverse = True)
-coins = list(map(int, coins))
+n, k = map(int, input().split())
+coins=list(set(int(input()) for _ in range(n)))
+check = [0] * (k+1)
 
 min_cnt = MAX
-found_combination  = False
-check = [0] * (k+1)
-for i in range(len(coins)):
-    BFS(i)
-    # print("found_combination", found_combination)
-
-if found_combination:
-    print(min_cnt)
-else:
-    print(-1)
-# 메모리 초과
+print(BFS())
