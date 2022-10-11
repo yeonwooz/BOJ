3737ea2bae58809153303a49b71a179d86a63aea
suyeon woo <woosean999@gmail.com>
2022-10-11 15:42:27 +0900

    13549 순간이동 고려

--- 13549/1/solution.py
+++ 13549/1/solution.py
@@ -20,28 +20,29 @@ def dijkstra(n):
 
     while q:
         cost, pos = heapq.heappop(q)
+        print("pos", pos)
         tele_pos = pos * dtel
 
         if pos == K or tele_pos == K:
             print(cost)
             should_print = False
             break
-
-        if costs[pos] < cost:
-            continue
         
         range_s =  min(N,K)
         range_e =  max(N,K)
         for i in range(2):
-            n_pos = pos + dwalk[i] # 바로 갈 경우
+            n_pos1 = pos + dwalk[i] # 바로 갈 경우
+            n_pos2 = pos * 2 + dwalk[i] # 순간이동 후 이동
 
-            if range_s <= n_pos < range_e:
-                if costs[n_pos] > cost + 1:
-                    costs[n_pos] = cost + 1
-                    heapq.heappush(q,(cost + 1, n_pos)) 
-                    if range_s <= n_pos * 2 <= range_e:
-                        heapq.heappush(q,(cost + 1, n_pos * 2)) 
-    
+            if range_s <= n_pos1 <= range_e:
+                if costs[n_pos1] > cost + 1:
+                    costs[n_pos1] = cost + 1
+                    heapq.heappush(q,(cost + 1, n_pos1)) 
+
+            if range_s <= n_pos2 <= range_e:
+                if costs[n_pos2] > cost + 1:
+                    costs[n_pos2] = cost + 1
+                    heapq.heappush(q,(cost + 1, n_pos2)) 
 
 dijkstra(N)
 
