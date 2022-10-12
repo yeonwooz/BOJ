2e8e4d523b5daa65ebe12d9f43a6ec2f3b6fdea6
suyeon woo <woosean999@gmail.com>
2022-10-12 11:39:51 +0900

    1432 그래프 수정 / python / 위상 정렬

--- 1432/1/solution.py
+++ 1432/1/solution.py
@@ -1,30 +1,34 @@
 import sys
-from collections import deque
+from heapq import heappush, heappop
 
 N = int(sys.stdin.readline())
 graph = [[] for _ in range(N+1)] # 인접행렬 그래프
 inDegree = [0] * (N+1)
 connections = [[] for _ in range(N+1)]  # 각 출발 노드에서 연결된 목적 노드들
 answer = [0] * (N+1)
+
 for i in range(1, N+1):
     row = list(map(int, list("0" + sys.stdin.readline().rstrip())))
     graph[i] = row
 
     for j in range(1, N+1):
         if graph[i][j] == 1:
-            connections[i].append(j)
-            inDegree[j] += 1
+            connections[j].append(i)   # 진입방향 바꿔서 입력
+            inDegree[i] += 1  # 바뀐 진입방향에 대한 진입차수 기록
 
 def top_sort():
-    q = deque()
-    for i in range(1,N+1):
+    q = []
+    for i in range(1, N+1):
         if inDegree[i] == 0:
-            q.append(i)
+            heappush(q, -i)   # 우선순위를 반대로 입력
 
-    order = 1
+    order = N
     while q:
-        popped = q.popleft()
-        answer[popped] = order
+        # popped = q.popleft()
+        now = -heappop(q)
+        # print(popped)
+        answer[now] = order
+
         '''
         1. 예를 들어 [[], [5], [4], [], [5], [3]] 이렇게 생긴 connections 기준으로, 진입차수가 0인 것은 1,2 (아무도 1,2를 안 가리킴)
        
@@ -41,11 +45,17 @@ def top_sort():
         7. 3 출력. 3이 가리키는 노드는 없다. 함수 종료
         
         '''
-        for i in connections[popped]:
+        for i in connections[now]:
             inDegree[i] -= 1
             if inDegree[i] == 0:
-                q.append(i)
-        order += 1
+                # q.append(i)
+                heappush(q, -i)
+        order -= 1
 
 top_sort()
-print(answer)
+if answer.count(0) == N+1:
+    print(-1)
+else:
+    print(*answer[1:])
+    # print(" ".join(i for i in range(N)))
+    # print(" ".join(i for i in range(N, 0, -1)))
