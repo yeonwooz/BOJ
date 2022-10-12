8f31f80b2841286dbfe404088338abcbb64e16d0
suyeon woo <woosean999@gmail.com>
2022-10-12 23:42:39 +0900

    [WIP]9204 체스

--- 9204/1/solution.py
+++ 9204/1/solution.py
@@ -9,20 +9,23 @@ dc = [-1,+1,-1,+1]
 imp_r = [-1,+1,0,0]
 imp_c = [0,0,-1,+1]
 
-def BFS(n,visited, x1, x2, y1, y2):
+def BFS(n, x1, x2, y1, y2):
     distances = [[1e9] * (n+1) for _ in range(n+1)]
     answer = []
-    cnt = 0
+    answer.append(chr(x1+64))
+    answer.append(n + 1 -x2)
+
     q = deque()
     q.append((0, x1,x2))
+
     distances[x2][x1] = 0
 
     while q:
         cnt, c,r = q.popleft()
         # print('r,c',r,c)
         if r == y2 and c == y1:
-            answer.append(chr(c+64))
-            answer.append(n + 1 -r)
+            # answer.append(chr(c+64))
+            # answer.append(n + 1 -r)
             print(cnt, *answer)
             return
 
@@ -35,8 +38,7 @@ def BFS(n,visited, x1, x2, y1, y2):
             n_r = r + dr[idx]
             n_c = c + dc[idx]
             if 0 < n_r < n + 1 and 0 < n_c < n + 1:
-                if not visited[n_r][n_c] and distances[n_r][n_c] > cnt + 1:
-                    visited[n_r][n_c] = 1 
+                if distances[n_r][n_c] > cnt + 1:
                     distances[n_r][n_c] = cnt + 1
                     q.append((cnt + 1, n_c,n_r))
                     answer.append(chr(n_c+64))
@@ -49,6 +51,6 @@ for _ in range(T):
     x1,x2,y1,y2 = input().split()
     x2,y2 = (n+1) - int(x2), (n+1) - int(y2)
     x1,y1 = ord(x1) - 64, ord(y1) - 64
-    visited = [[0] * (n+1) for _ in range(n+1)]
+    # visited = [[0] * (n+1) for _ in range(n+1)]
     # print("start")
-    BFS(n, visited, x1,x2,y1,y2)
+    BFS(n, x1,x2,y1,y2)
