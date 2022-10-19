be2dd56cb2f6b68bda89d28b6e16c671985c1fd6
suyeon woo <woosean999@gmail.com>
2022-10-19 12:58:31 +0900

    [WIP]1041

--- 1041/1/solution.py
+++ 1041/1/solution.py
@@ -5,7 +5,20 @@ input = sys.stdin.readline
 n = int(input())
 nums = list(map(int, input().split()))
 nums.sort()
+if n == 1:
+    answer = sum(nums) - nums[5]
+    print(answer)
+    exit()
 
-sum = ((n-2) ** 2) * 5 * nums[0] + (n-2) * 8 * (nums[0] + nums[1]) + (n-2) * 4 * (nums[0]) + 4 * (nums[0] + nums[1] + nums[2]) + 4 * (nums[0] + nums[1])
-print(sum)
+min_answer = 50 * 6 * (1000000 ** 3)
+for num in nums:
+    arr = nums[0:]
+    arr.remove(num)
+
+    answer = ((n-2) ** 2) * 5 * arr[0] + (n-2) * 8 * (arr[0] + arr[1]) + (n-2) * 4 * (arr[0]) + 4 * (arr[0] + arr[1] + arr[2]) + 4 * (arr[0] + arr[1])
+    min_answer = min(min_answer, answer)
+
+print(min_answer)
+
 #finished at 11:24 => 반례를 찾아야 한다
+#12:58 ???
