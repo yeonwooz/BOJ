6c15403f6396380210fea9fb7c69a7ea34ad1668
suyeon woo <woosean999@gmail.com>
2022-10-19 22:03:02 +0900

    [WIP]3033

--- 3033/1/solution.py
+++ 3033/1/solution.py
@@ -1,21 +1,15 @@
 # https://blog.koderpark.dev/145
 import sys, functools
 
-t = 0
-n = 0
-sfa = [0] * 200000
-lcp = [0] * 200000
-tmp = [0] * 200000
-g = [0] * 200000
-ng = [0] * 200000
-
 def compare(a, b):
     if g[a] == g[b]:
         return g[a+t] < g[b+t]
     return g[a] < g[b]
 
 def make_sfa(str):
-    global t, n
+    global t
+    global n
+    
     t = 1
     n = len(str)
 
@@ -36,30 +30,42 @@ def make_sfa(str):
         
         for i in range(n):
             g[i] = ng[i]
-            t *= 2
+        t *= 2
 
 def LCP():
     for i in range(n):
         tmp[sfa[i]] = i
-        len = 0
-        for i in range(n):
-            if tmp[i]:
-                j = sfa[tmp[i] - 1]
 
-                while s[j+len] == s[i+len]:
-                    len += 1
-                    lcp[tmp[i]] = len
+    len = 0
+    for i in range(n):
+        if tmp[i]:
+            j = sfa[tmp[i] - 1]
 
-                    if len:
-                        len -= 1
+            while s[j+len] == s[i+len]:
+                len += 1
+            lcp[tmp[i]] = len
 
-n = int(sys.stdin.readline())
-s = sys.stdin.readline().rstrip()
+            if len:
+                len -= 1
 
-make_sfa(s)
-LCP()
+if __name__ == "__main__":
+    MAX = 234567
+    n = 0
+    t = 0
 
-ans = -1
-for i in range(n):
-    ans = max(ans, lcp[i])
-print(ans)
+    sfa = [0] * MAX
+    lcp = [0] * MAX
+    tmp = [0] * MAX
+    g = [0] * MAX
+    ng = [0] * MAX
+
+    N = int(sys.stdin.readline())
+    s = sys.stdin.readline().rstrip()
+
+    make_sfa(s)
+    LCP()
+
+    ans = -1
+    for i in range(N):
+        ans = max(ans, lcp[i])
+    print(ans)
