19d8cf1bbc41edd1fc76a3d1e242f5da28aa0ae5
suyeon woo <woosean999@gmail.com>
2022-10-31 09:37:22 +0900

    11282 한글

---
+++ 11282/solution.py
@@ -0,0 +1,30 @@
+# import sys
+
+# def getInput():
+#     return sys.stdin.readline().rstrip()
+
+# n = int(getInput())
+
+# cho = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# jung = ['ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ', 'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ', 'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ', 'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ', 'ㅣ']
+
+# jong = ['', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ', 'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ', 'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ', 'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
+
+# cnt = n-1
+
+# for a in range(len(cho)):
+#     for b in range(len(jung)):
+#         for c in range(len(jong)):
+#             if cnt == 0:
+#                 print(cho[a]+jung[b]+jong[c])
+#                 break
+#             else: 
+#                 cnt -= 1
+#         if cnt == 0:
+#             break
+#     if cnt == 0:
+#         break
+
+KO_UNICODE_START = 44031
+print(chr(KO_UNICODE_START + int(input())))
