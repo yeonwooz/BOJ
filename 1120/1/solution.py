cd8766d9213ad14670f78566d9417fd521f24faf
suyeon woo <woosean999@gmail.com>
2022-11-03 23:30:04 +0900

    WIP

--- 1120/1/solution.py
+++ 1120/1/solution.py
@@ -3,20 +3,21 @@ A, B = input().rstrip().split()
 Alen = len(A)
 Blen = len(B)
 
-min_diff = 51
+min_diff = Alen
 idx = 0
 
 for i in range(Blen):
-    if B[i] == A[0]:
-        diff = Alen - 1
-        idx = 1
-        # print('B[i]', B[i])
-        while idx < Alen and i+idx < Blen:
-            # print('B[i+idx]', B[i+idx])
-            # print("diff", diff)
-            if B[i+idx] == A[idx]:
-                diff -= 1
-            idx += 1
-        min_diff = min(diff, min_diff)
+    for pos in range(Alen):
+        if A[pos] in B:
+            diff = Alen - 1
+            idx = pos + 1
+            # print('B[i]', B[i])
+            while idx < Alen and i+idx < Blen:
+                # print('B[i+idx]', B[i+idx])
+                # print("diff", diff)
+                if B[i+idx] == A[idx]:
+                    diff -= 1
+                idx += 1
+            min_diff = min(diff, min_diff)
 
 print(min_diff)
