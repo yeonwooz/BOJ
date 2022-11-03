a829c6fb959c50d7791123039f53188b51065aff
suyeon woo <woosean999@gmail.com>
2022-11-03 23:27:40 +0900

    WIP

--- 1120/1/solution.py
+++ 1120/1/solution.py
@@ -6,19 +6,17 @@ Blen = len(B)
 min_diff = 51
 idx = 0
 
-diff = 0
-while (idx < Alen):
-    if A[idx] != B[idx]:
-        diff += 1
-    idx += 1
-min_diff = min(min_diff, diff)
+for i in range(Blen):
+    if B[i] == A[0]:
+        diff = Alen - 1
+        idx = 1
+        # print('B[i]', B[i])
+        while idx < Alen and i+idx < Blen:
+            # print('B[i+idx]', B[i+idx])
+            # print("diff", diff)
+            if B[i+idx] == A[idx]:
+                diff -= 1
+            idx += 1
+        min_diff = min(diff, min_diff)
 
-diff = 0
-idx = -1
-while (idx >= -Alen):
-    if A[idx] != B[idx]:
-        diff += 1
-    idx -= 1
-
-min_diff = min(min_diff, diff)
 print(min_diff)
