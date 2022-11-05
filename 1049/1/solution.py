7ab75225f9c32c63d3a188c62a203965219f66ad
suyeon woo <woosean999@gmail.com>
2022-11-05 14:14:27 +0900

    섞어서 살수 있다 / 이왜틀,,?

--- 1049/1/solution.py
+++ 1049/1/solution.py
@@ -1,11 +1,17 @@
 from sys import stdin
 N,M = map(int, stdin.readline().split())
 
-cashes = []
+packs = []
+eaches = []
 for _ in range(M):
     packprice, eachprice = map(int, stdin.readline().split())
-    cash1 = N // 6 * packprice + N % 6 * eachprice
-    cash2 = (N // 6 + 1) * packprice
-    cashes.append(min(cash1, cash2))
+    packs.append(packprice)
+    eaches.append(eachprice)
 
-print(min(cashes))
+min_pack_price = min(packs)
+min_each_price = min(eaches)
+
+cash = (N // 6 * min_pack_price) + (N % 6 * min_each_price)
+if (N % 6 != 0):
+    cash = min(cash,  (N // 6 + 1) * min_pack_price)
+print(cash)
