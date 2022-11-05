45fac330a5650b0aeaee70b17c6970f0c1d02845
suyeon woo <woosean999@gmail.com>
2022-11-05 14:07:29 +0900

    브랜드 섞어서 살때

--- 1049/1/solution.py
+++ 1049/1/solution.py
@@ -5,12 +5,14 @@ packs = []
 eaches = []
 for _ in range(M):
     packprice, eachprice = map(int, stdin.readline().split())
-    # cash = N // 6 * packprice + N % 6 * eachprice
     packs.append(packprice)
     eaches.append(eachprice)
+
 
 min_pack_price = min(packs)
 min_each_price = min(eaches)
 
+cash1 = N // 6 * min_pack_price + N % 6 * min_each_price
+cash2 = (N // 6 + 1) * min_pack_price 
 
-
+print(min(cash1, cash2))
