f5b28164488f7ac50fab7e9c848b840481ec6e9a
suyeon woo <woosean999@gmail.com>
2022-11-05 14:42:56 +0900

    1049 기타줄

--- 1049/1/solution.py
+++ 1049/1/solution.py
@@ -11,15 +11,11 @@ for _ in range(M):
 pack_p = min(packs)
 each_p = min(eaches)
 
-if pack_p <= each_p * 6:
-    # 팩으로 사는게 6개 낱개보다 저렴
-    if N % 6 == 0:
-        print(pack_p * (N // 6 ))
+if pack_p < each_p * 6:
+    if pack_p < N % 6 * each_p:
+        print(N//6 * pack_p + pack_p)
     else:
-        print(pack_p * (N // 6 + 1))
-elif pack_p > each_p * 6:
-    # 팩 1개가 6개 낱개로 사는 것보다 비쌈
-    print(each_p * N)
-else:
-    # 나머지는 낱개로 사는게 팩보다 저렴               
-    print(pack_p * (N // 6) + each_p * (N % 6))
+        print(N//6 * pack_p + N%6 * each_p)
+elif pack_p >= each_p * 6:
+    print(N * each_p)
+
