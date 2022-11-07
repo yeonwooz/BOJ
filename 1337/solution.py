cc9fa317389b9a4a26899c6de97c2fe5495283a0
suyeon woo <woosean999@gmail.com>
2022-11-07 11:06:30 +0900

    1337 올바른 배열

--- 1337/solution.py
+++ 1337/solution.py
@@ -1,38 +1,21 @@
 from sys import stdin
 N = int(stdin.readline())
 
+total = 5
 arr = []
 for i in range(N):
     arr.append(int(stdin.readline()))
 
-arr.sort()
-# start = arr[0]
-# end = arr[N-1]
-
-max_cnt = 0
 for i in range(N):
-    cnt = 1
-    space = 0
-    for j in range(i+1, N):
-        print('arr[j-1]',arr[j-1], 'arr[j]',arr[j])
-        if cnt == 5:
-            max_cnt = cnt
-            break
-
-        prev = j-1
-        cur = j
-
-        if arr[prev] + 1 == arr[cur]:
-            cnt += 1
-        else:
-            space += 1
-            max_cnt = max(cnt, max_cnt)
-            cnt = 1
-        
-
-    max_cnt = max(cnt, max_cnt, 5-space)
-
-    if max_cnt == 5:
-        break
-    
-print(5-max_cnt)
+    cnt1=4
+    cnt2=4
+    for j in range(N):
+        if 0 < arr[j] - arr[i] < 5:
+            #앞이 뒤보다 작고 차이가 5미만
+            cnt1-=1
+        elif 0 < arr[i] - arr[j] < 5:
+            # 앞이 뒤보다 크고 차이가 5미만
+            cnt2-=1
+    total = min(total, cnt1, cnt2)
+print(total)
+      
