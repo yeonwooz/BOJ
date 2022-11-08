2abc4cdb3633c608bb44a0f59dbc1a88e2b971ce
suyeon woo <woosean999@gmail.com>
2022-11-08 15:57:22 +0900

    WIP

--- 20115/1/solution.py
+++ 20115/1/solution.py
@@ -2,16 +2,16 @@
 from sys import stdin
 N = int(stdin.readline())
 drinks = list(map(int, stdin.readline().split()))
-drinks.sort()
+drinks.sort(reverse=True)
 
-amount = 0
+# 큰 컵일수록 양이 최대한 유지되어야 함 
 for i in range(N-1):
     left = drinks[i]
     right = drinks[i+1]
-    while (left > 0.000001):
-        left = left / 2
-        right += left
-    amount = right
+    while (right > 0.000001):
+        right = right / 2
+        left += right
+    drinks[i+1] = left
 
-print(amount)
+print(drinks[N-1])
     
