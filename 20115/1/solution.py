4f1d76e736112be050aa8c9b06638cdec39ae137
suyeon woo <woosean999@gmail.com>
2022-11-08 15:59:49 +0900

    20115

--- 20115/1/solution.py
+++ 20115/1/solution.py
@@ -4,13 +4,13 @@ N = int(stdin.readline())
 drinks = list(map(int, stdin.readline().split()))
 drinks.sort(reverse=True)
 
+
 # 큰 컵일수록 양이 최대한 유지되어야 함 
 for i in range(N-1):
     left = drinks[i]
     right = drinks[i+1]
-    while (right > 0.000001):
-        right = right / 2
-        left += right
+    right = right / 2  # 반을 넣으면 반은 흘려서 없어짐
+    left += right
     drinks[i+1] = left
 
 print(drinks[N-1])
