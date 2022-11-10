741d5e5cb864218253800aa83dd34ae1e63a7444
suyeon woo <woosean999@gmail.com>
2022-11-10 23:24:18 +0900

    11286 절댓값 힙 / python

--- 11286/2/solution.py
+++ 11286/2/solution.py
@@ -1,3 +1,4 @@
+#started at 11:05
 from heapq import heappush, heappop
 from sys import stdin
 
@@ -13,5 +14,8 @@ for _ in range(n):
         # x가 0일 때 => 절댓값이 가장 작은 값을 출력한다. 그게 여러개라면 가장 작은 수를 출력하고 힙에서 제거한다
         if heap:
             popped = heappop(heap)
+            print(popped[1])    
+        else:
+            print(0)
 
-            print(popped[1])
+#finished at 11:24
