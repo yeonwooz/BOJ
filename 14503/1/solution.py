5d43c4da53a837bb84c91226cdd53172cb5d2036
suyeon woo <woosean999@gmail.com>
2022-12-30 11:17:20 +0900

    WIP

--- 14503/1/solution.py
+++ 14503/1/solution.py
@@ -9,50 +9,46 @@ dc = [0, 1, 0, -1]
 
 N,M = map(int, getInput())
 r,c,d = map(int, getInput())
+r -= 1
+c -= 1
 
 board = []
 for i in range(N):
     board.append(list(map(int, getInput())))
 
 cnt = 1
+board[r][c] = 2
 idx = d
 while True:
+    print(r,c, idx)
+    print(board)
+    print()
     moved = False
     for i in range(4):
         idx = (idx - 1) % 4
         nr = r + dr[idx]
         nc = c + dc[idx]
 
-        if 0 <= nr < N and 0 <= nc < M and board[nr][nc] == 0:
-            if board[nr][nc] == 0:
-                board[nr][nc] = 2    #청소완료
-                cnt += 1
-                moved = True
-                r = nr
-                c = nc
-                break
-            else:
-                continue
+        # if 0 <= nr < N and 0 <= nc < M and board[nr][nc] == 0:
+        if board[nr][nc] == 0:
+            board[nr][nc] = 2    #청소완료
+            cnt += 1
+            moved = True
+            r = nr
+            c = nc
+            break
+        else:
+            continue
 
     if moved == False:
-        # if idx == 0: # 현재 북쪽 바라봄
-        #     idx = 2
-        # elif idx == 1: # 현재 동쪽 바라봄
-        #     idx = 3
-        # elif idx == 2: # 현재 남쪽 바라봄
-        #     idx = 0
-        # elif idx == 3: # 현재 서쪽 바라봄
-        #     idx = 1
-        idx = (idx + 2) % 4
-        nr = r + dr[idx]
-        nc = c + dc[idx]
-        if 0 <= nr < N and 0 <= nc < M and board[nr][nc] == 0:
-            if board[nr][nc] == 0:
-                r = nr
-                c = nc
-            else:
-                break
-        else:
+        next_pos = (idx + 2) % 4
+        nr = r + dr[next_pos]
+        nc = c + dc[next_pos]
+        if board[nr][nc] == 1:
             break
+            
+        else:
+            r = nr
+            c = nc
 
 print(cnt)
