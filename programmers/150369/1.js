69562f93019a7d7ef3395bcbb9dfff9e04cc2328
suyeon woo <woosean999@gmail.com>
2023-02-02 10:55:29 +0900

    택배 배달과 수거하기 - 시간초과 해결

--- programmers/150369/1.js
+++ programmers/150369/1.js
@@ -6,30 +6,20 @@ function solution(cap, n, deliveries, pickups) {
     pickups.reduce((a, b) => a + b, 0) > 0
   ) {
     for (let i = n - 1; i >= 0; --i) {
-      let space = cap;
       // 배달 수거 모두 없는 집 건너뜀
-      if (deliveries[i] === 0 && pickups[i] === 0) continue;
-      // i번째 집까지 간다
-      const dest = i;
-      for (let j = dest; j >= 0; --j) {
-        if (!space) break;
-        while (space && deliveries[j]) {
-          deliveries[j] -= 1;
-          space -= 1;
-        }
+      while (deliveries[i] > 0 || pickups[i] > 0) {
+        deliveries[i] -= cap;
+        pickups[i] -= cap;
+        answer += 2 * (i + 1);
       }
 
-      space = cap;
-      // i번째 집에서 물류창고로 돌아온다
-      for (let j = dest; j >= 0; --j) {
-        if (!space) break;
-        while (space && pickups[j]) {
-          pickups[j] -= 1;
-          space -= 1;
-        }
+      if (deliveries[i] < 0) {
+        deliveries[i - 1] += deliveries[i];
       }
 
-      answer += 2 * (dest + 1);
+      if (pickups[i] < 0) {
+        pickups[i - 1] += pickups[i];
+      }
     }
   }
 
