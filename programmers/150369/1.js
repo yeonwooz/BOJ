b8756362105fa8d660f2c59431e902a17f6f1e14
suyeon woo <woosean999@gmail.com>
2023-02-02 10:38:07 +0900

    [WIP] 택배 배달과 수거하기 - while 조건 추가 / 오답

--- programmers/150369/1.js
+++ programmers/150369/1.js
@@ -1,33 +1,36 @@
 function solution(cap, n, deliveries, pickups) {
   let answer = 0;
   // 갈때 최대한 멀리 많이 배달(deliveries), 돌아올때 최대한 멀리 많이 수거(pickups)
-
-  for (let i = n - 1; i >= 0; --i) {
-    console.log(deliveries, pickups);
-    let space = cap;
-    // 배달 수거 모두 없는 집 건너뜀
-    if (deliveries[i] === 0 && pickups[i] === 0) continue;
-    // i번째 집까지 간다
-    const dest = i;
-    for (let j = dest; j >= 0; --j) {
-      if (!space) break;
-      while (space && deliveries[j]) {
-        deliveries[j] -= 1;
-        space -= 1;
+  while (
+    deliveries.reduce((a, b) => a + b, 0) > 0 ||
+    pickups.reduce((a, b) => a + b, 0) > 0
+  ) {
+    for (let i = n - 1; i >= 0; --i) {
+      let space = cap;
+      // 배달 수거 모두 없는 집 건너뜀
+      if (deliveries[i] === 0 && pickups[i] === 0) continue;
+      // i번째 집까지 간다
+      const dest = i;
+      for (let j = dest; j >= 0; --j) {
+        if (!space) break;
+        while (space && deliveries[j]) {
+          deliveries[j] -= 1;
+          space -= 1;
+        }
       }
-    }
 
-    space = cap;
-    // i번째 집에서 물류창고로 돌아온다
-    for (let j = dest; j >= 0; --j) {
-      if (!space) break;
-      while (space && pickups[j]) {
-        pickups[j] -= 1;
-        space -= 1;
+      space = cap;
+      // i번째 집에서 물류창고로 돌아온다
+      for (let j = dest; j >= 0; --j) {
+        if (!space) break;
+        while (space && pickups[j]) {
+          pickups[j] -= 1;
+          space -= 1;
+        }
       }
-    }
 
-    answer += 2 * (dest + 1);
+      answer += 2 * (dest + 1);
+    }
   }
 
   return answer;
