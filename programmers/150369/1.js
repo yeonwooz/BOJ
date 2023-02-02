fecba1f2af3740a7659cb573b7d64f61b9d23ebb
suyeon woo <woosean999@gmail.com>
2023-02-03 00:08:35 +0900

    택배 배달과 수거하기 - 불필요한 최외곽 while 문 제거

--- programmers/150369/1.js
+++ programmers/150369/1.js
@@ -1,25 +1,21 @@
 function solution(cap, n, deliveries, pickups) {
   let answer = 0;
   // 갈때 최대한 멀리 많이 배달(deliveries), 돌아올때 최대한 멀리 많이 수거(pickups)
-  while (
-    deliveries.reduce((a, b) => a + b, 0) > 0 ||
-    pickups.reduce((a, b) => a + b, 0) > 0
-  ) {
-    for (let i = n - 1; i >= 0; --i) {
-      // 배달 수거 모두 없는 집 건너뜀
-      while (deliveries[i] > 0 || pickups[i] > 0) {
-        deliveries[i] -= cap;
-        pickups[i] -= cap;
-        answer += 2 * (i + 1);
-      }
 
-      if (deliveries[i] < 0) {
-        deliveries[i - 1] += deliveries[i];
-      }
+  for (let i = n - 1; i >= 0; --i) {
+    // 배달 수거 모두 없는 집 건너뜀
+    while (deliveries[i] > 0 || pickups[i] > 0) {
+      deliveries[i] -= cap;
+      pickups[i] -= cap;
+      answer += 2 * (i + 1);
+    }
 
-      if (pickups[i] < 0) {
-        pickups[i - 1] += pickups[i];
-      }
+    if (deliveries[i] < 0) {
+      deliveries[i - 1] += deliveries[i];
+    }
+
+    if (pickups[i] < 0) {
+      pickups[i - 1] += pickups[i];
     }
   }
 
