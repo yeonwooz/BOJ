5010073f5ce68104308ab1ffb3249e69d1aabf99
suyeon woo <woosean999@gmail.com>
2023-02-04 00:09:20 +0900

    표현 가능한 이진트리 - 풀이참고, log를 통한 사이즈 감소

--- programmers/150367/1.js
+++ programmers/150367/1.js
@@ -1,25 +1,23 @@
 function solution(numbers) {
   let answer = [];
   for (let num of numbers) {
-    const binstr = num.toString(2);
-    answer.push(check(0, binstr.length - 1, binstr) ? 1 : 0);
+    let binstr = num.toString(2);
+    let size = Math.pow(2, Math.floor(Math.log2(binstr.length)) + 1) - 1;
+    binstr = "0".repeat(size - binstr.length) + binstr;
+    answer.push(check(binstr));
   }
   return answer;
 }
 
-function check(start, end, str) {
-  if (start === end) return str[start];
+function check(str) {
+  const mid = (str.length - 1) / 2;
+  if (mid === 0) return 1;
+  const left = str.substr(0, mid);
+  const right = str.substr(mid + 1);
 
-  const mid = parseInt((start + end) / 2);
-  if (mid === start || mid === end) return str[mid];
-  const left = check(start, mid - 1, str);
-  if (!left) return false;
-  if (str[mid] === "0" && left === "1") return false;
-
-  const right = check(mid + 1, end, str);
-  if (!right) return false;
-  if (str[mid] === "0" && right === "1") return false;
-
-  if (str[mid] === "0" && left === "0" && right === "0") return "0";
-  return "1";
+  if (str[mid] === "0") {
+    if (left * 1 === 0 && right * 1 === 0) return 1;
+    return 0;
+  }
+  return check(left) && check(right);
 }
