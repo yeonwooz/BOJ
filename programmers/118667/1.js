9153e1822caca7e5c5c73302532b30773e80fb89
suyeon woo <woosean999@gmail.com>
2023-02-06 10:50:32 +0900

    두 큐 합 같게 만들기 - 시간초과로 인해 투포인터 풀이로 변경

--- programmers/118667/1.js
+++ programmers/118667/1.js
@@ -3,25 +3,22 @@ function solution(queue1, queue2) {
   let sum2 = queue2.reduce((prevsum, newelement) => prevsum + newelement, 0);
   if ((sum1 + sum2) % 2) return -1;
 
-  const maxCnt = (queue1.length + queue2.length) * 2 + 1;
+  // 시간초과로 인해 투포인터 풀이로 변경
+  let allQueue = [...queue1, ...queue2];
+  let ptr1 = 0;
+  let ptr2 = queue1.length;
+  const half = (sum1 + sum2) / 2;
+  const maxCnt = allQueue.length * 3;
+
   let cnt = 0;
-  while (sum1 !== sum2) {
-    if (cnt >= maxCnt) {
-      cnt = -1;
-      break;
-    }
-    if (sum1 < sum2) {
-      const popped = queue2.shift();
-      queue1.push(popped);
-      sum1 += popped;
-      sum2 -= popped;
-    } else if (sum2 < sum1) {
-      const popped = queue1.shift();
-      queue2.push(popped);
-      sum2 += popped;
-      sum1 -= popped;
-    }
+  while (cnt < maxCnt) {
+    if (sum1 === half) return cnt;
+    sum1 =
+      sum1 > half
+        ? sum1 - allQueue[ptr1++ % allQueue.length]
+        : sum1 + allQueue[ptr2++ % allQueue.length];
+
     cnt++;
   }
-  return cnt;
+  return -1;
 }
