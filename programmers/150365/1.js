372636b1145c4612744f7d7a0e1f45b9ddf93a91
suyeon woo <woosean999@gmail.com>
2023-02-07 23:54:43 +0900

    다른 분 풀이 참고

--- programmers/150365/1.js
+++ programmers/150365/1.js
@@ -1,46 +1,46 @@
 function solution(n, m, x, y, r, c, k) {
-  const board = Array(n)
-    .fill()
-    .map(() => Array(m).fill(0));
-  const memoBoard = Array(n)
-    .fill()
-    .map(() => Array(m).fill(""));
+  let minDist = mDist(x, y, r, c);
+  let res = k - minDist;
+  let alp = ["d", "l", "r", "u"];
 
-  // S: x-1, y-1
-  // E: r-1, c-1
-  const dx = [-1, 0, 1, 0];
-  const dy = [0, 1, 0, -1];
-  let done = false;
+  if (minDist % 2 !== k % 2 || res < 0) return "impossible";
 
-  function DFS(cur_x, cur_y, dist, cur_route) {
-    if (dist === k) {
-      if (cur_x === r - 1 && cur_y === c - 1) done = true;
-      return;
-    }
-    memoBoard[cur_x][cur_y] = cur_route;
+  let answer = "";
 
-    for (let i = 0; i < 4; ++i) {
-      const next_x = cur_x + dx[i];
-      const next_y = cur_y + dy[i];
+  while (minDist < k) {
+    let temp = move(n, m, x, y, r, c);
+    answer += alp[temp];
+    if (temp === 0) x++;
+    else if (temp === 1) y--;
+    else if (temp === 2) y++;
+    else x--;
 
-      if (next_x < 0 || next_x >= n || next_y < 0 || next_y >= m) continue;
-
-      if (memoBoard[next_x][next_y] <= memoBoard[cur_x][cur_y]) {
-        if (i === 0) cur_route += "u";
-        if (i === 1) cur_route += "r";
-        if (i === 2) cur_route += "d";
-        if (i === 3) cur_route += "l";
-        dist++;
-
-        const cur_memo = memoBoard[cur_x][cur_y];
-        const next_memo = DFS(next_x, next_y, dist, cur_route);
-        memoBoard[cur_x][cur_y] = cur_memo < next_memo ? cur_memo : next_memo;
-      }
-    }
-    return cur_route;
+    minDist = mDist(x, y, r, c);
+    k--;
   }
 
-  DFS(x - 1, y - 1, 0, "");
+  if (x < r) answer += "d".repeat(r - x);
+  if (y > c) answer += "l".repeat(y - c);
+  if (y < c) answer += "r".repeat(c - y);
+  if (x > r) answer += "u".repeat(x - r);
 
-  return done ? memoBoard[r - 1][c - 1] : "impossible";
+  return answer;
 }
+
+const mDist = (x, y, r, c) => {
+  return Math.abs(x - r) + Math.abs(y - c);
+};
+
+const move = (n, m, x, y, r, c) => {
+  let dx = [1, 0, 0, -1];
+  let dy = [0, -1, 1, 0];
+
+  for (let i = 0; i < 4; i++) {
+    let nx = x + dx[i];
+    let ny = y + dy[i];
+
+    if (nx >= 1 && nx <= n && ny >= 1 && ny <= m) {
+      return i;
+    }
+  }
+};
