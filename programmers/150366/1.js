60425f679dabb89c5d90ed36d7ac42cd55908050
suyeon woo <woosean999@gmail.com>
2023-02-09 23:49:59 +0900

    다른 분의 풀이 참고

--- programmers/150366/1.js
+++ programmers/150366/1.js
@@ -1,125 +1,116 @@
 function solution(commands) {
-  const valuemap = new Map(); // ex. value1, ["2_3", "3_4"] -> indexof, splice로 제거 / push로 삽입
+  let answer = [];
+  const cell = Array(51)
+    .fill()
+    .map((_) => Array(51).fill(""));
+  const parent = Array(51)
+    .fill()
+    .map((_, i) =>
+      Array(51)
+        .fill()
+        .map((_, j) => [i, j])
+    );
 
-  let table = Array(51);
+  for (const commandStr of commands) {
+    const [command, ...rest] = commandStr.split(" ");
 
-  for (var i = 0; i < 51; i++) {
-    table[i] = new Array(51).fill([""]);
-  }
-
-  const answer = [];
-
-  for (let str of commands) {
-    let cmd, r, c, r1, c1, r2, c2, value, value1, value2;
-
-    const arr = str.split(" ");
-    switch (arr[0]) {
-      case "PRINT":
-        [cmd, r, c] = arr;
-        const currentCell = table[r][c];
-        const currentValue = currentCell[currentCell.length - 1];
-        answer.push(currentValue === "" ? "EMPTY" : currentValue);
-        break;
-
+    switch (command) {
       case "UPDATE":
-        if (arr.length === 4) {
-          [cmd, r, c, value] = arr;
-          table[r][c].pop();
-          table[r][c].push(value);
-
-          const curCell = r + "_" + c;
-          if (!valuemap.get(value)) {
-            valuemap.set(value, [curCell]);
-          } else {
-            valuemap.get(value).push(curCell);
-          }
-        } else if (arr.length === 3) {
-          const [cmd, value1, value2] = arr;
-
-          const valArr = valuemap.get(value1);
-          if (!valArr) break;
-          for (let i = valArr.length - 1; i >= 0; --i) {
-            const val = valArr[i];
-            const [r, c] = val.split("_").map((x) => parseInt(x));
-            table[r][c].pop();
-            table[r][c].push(value2);
-            valArr.splice(i, 1);
-
-            if (!valuemap.get(value2)) {
-              valuemap.set(value2, [val]);
-            } else {
-              valuemap.get(value2).push(val);
-            }
-          }
-        }
+        if (rest.length === 3) update(rest);
+        else replace(rest);
         break;
-
       case "MERGE":
-        [cmd, r1, c1, r2, c2] = arr;
-
-        if (r1 === r2 && c1 === c2) break;
-        const cell1 = table[r1][c1];
-        const cell2 = table[r2][c2];
-
-        const value1 = cell1[cell1.length - 1];
-        const value2 = cell2[cell2.length - 1];
-
-        if (value1 && !value2) {
-          table[r2][c2].push(value1);
-
-          const curCell = r2 + "_" + c2;
-          if (!valuemap.get(value1)) {
-            valuemap.set(value1, [curCell]);
-          } else {
-            valuemap.get(value1).push(curCell);
-          }
-        } else if (!value1 && value2) {
-          table[r1][c1].push(value2);
-
-          const curCell = r1 + "_" + c1;
-          if (!valuemap.get(value2)) {
-            valuemap.set(value2, [curCell]);
-          } else {
-            valuemap.get(value2).push(curCell);
-          }
-        } else if (value1 && value2) {
-          table[r2][c2].push(value1);
-
-          const curCell = r2 + "_" + c2;
-          if (!valuemap.get(value1)) {
-            valuemap.set(value1, [curCell]);
-          } else {
-            valuemap.get(value1).push(curCell);
-          }
-
-          // value2 맵에서 curCell 제거
-          const index = valuemap.get(value2).indexOf(curCell);
-          valuemap.get(value2).splice(index, 1);
-        }
-
+        merge(rest);
         break;
-
       case "UNMERGE":
-        [cmd, r, c] = arr;
-        const targetCellVal = table[r][c];
-        if (targetCellVal.length === 0) break;
-
-        const curVal = targetCellVal[targetCellVal.length - 1];
-
-        const valArr = valuemap.get(curVal);
-        if (!valArr) break;
-        for (let i = valArr.length - 1; i >= 0; --i) {
-          const val = valArr[i];
-          const [x, y] = val.split("_").map((s) => parseInt(s));
-          if (x === r && y === c) continue;
-
-          const cellVal = table[x][y];
-          table[x][y] = [""];
-          valArr.splice(i, 1);
-        }
-
+        unmerge(rest);
+        break;
+      case "PRINT":
+        answer = [...answer, print(rest)];
         break;
     }
   }
+
   return answer;
+
+  function find(coord) {
+    const [r, c] = coord;
+
+    if ([r, c].toString() === parent[r][c].toString()) {
+      return [Number(r), Number(c)];
+    }
+
+    parent[r][c] = find(parent[r][c]);
+
+    return parent[r][c];
+  }
+
+  function updateAll(target, value, type) {
+    const _target = target.toString();
+
+    for (let i = 0; i < 51; i++) {
+      for (let j = 0; j < 51; j++) {
+        if (parent[i][j].toString() === _target) {
+          if (type === "value") cell[i][j] = value;
+          else parent[i][j] = value;
+        }
+      }
+    }
+  }
+
+  function update(param) {
+    const [r, c, value] = param;
+    const target = find([r, c]);
+    updateAll(target, value, "value");
+  }
+
+  function replace(param) {
+    const [value1, value2] = param;
+    for (let i = 0; i < 51; i++) {
+      for (let j = 0; j < 51; j++) {
+        if (cell[i][j] === value1) {
+          cell[i][j] = value2;
+        }
+      }
+    }
+  }
+
+  function merge(param) {
+    const [r1, c1, r2, c2] = param;
+    if (r1 === r2 && c1 === c2) return;
+
+    const value = cell[r1][c1] !== "" ? cell[r1][c1] : cell[r2][c2];
+    const parent1 = find([r1, c1]);
+    const parent2 = find([r2, c2]);
+
+    if (parent1.toString() !== parent2.toString()) {
+      parent[r2][c2] = parent1;
+    }
+
+    updateAll(parent2, parent1, "parent");
+    updateAll(parent1, value, "value");
+  }
+
+  function unmerge(param) {
+    const [r, c] = param;
+    const value = cell[r][c];
+    const _target = find([r, c]).toString();
+
+    for (let i = 0; i < 51; i++) {
+      for (let j = 0; j < 51; j++) {
+        if (parent[i][j].toString() === _target) {
+          cell[i][j] = "";
+          parent[i][j] = [i, j];
+        }
+      }
+    }
+
+    cell[r][c] = value;
+  }
+
+  function print(param) {
+    const [r, c] = param;
+
+    return cell[r][c] === "" ? "EMPTY" : cell[r][c];
+  }
 }
