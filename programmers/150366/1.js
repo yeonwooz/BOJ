d46ea0689fe248cfc4996ae6b004d7fe191e359d
suyeon woo <woosean999@gmail.com>
2023-02-09 23:22:40 +0900

    WIP - 표 병합

--- programmers/150366/1.js
+++ programmers/150366/1.js
@@ -1,23 +1,27 @@
 function solution(commands) {
   const valuemap = new Map(); // ex. value1, ["2_3", "3_4"] -> indexof, splice로 제거 / push로 삽입
 
-  const table = Array(51).fill(Array(51).fill({ cur: "", history: [] }));
+  const table = Array(51).fill(Array(51).fill([""]));
 
   const answer = [];
-  let cmd, r, c, r1, c1, r2, c2, value, value1, value2;
 
   for (let str of commands) {
+    let cmd, r, c, r1, c1, r2, c2, value, value1, value2;
+
     const arr = str.split(" ");
     switch (arr[0]) {
       case "PRINT":
         [cmd, r, c] = arr;
-        answer.push(table[r][c].cur === "" ? "EMPTY" : table[r][c].cur);
+        const currentCell = table[r][c];
+        const currentValue = currentCell[currentCell.length - 1];
+        answer.push(currentValue === "" ? "EMPTY" : currentValue);
         break;
 
       case "UPDATE":
         if (arr.length === 4) {
           [cmd, r, c, value] = arr;
-          table[r][c].cur = value;
+          table[r][c].pop();
+          table[r][c].push(value);
 
           const curCell = r + "_" + c;
           if (!valuemap.get(value)) {
@@ -26,13 +30,15 @@ function solution(commands) {
             valuemap.get(value).push(curCell);
           }
         } else if (arr.length === 3) {
-          [cmd, value1, value2] = arr;
+          const [cmd, value1, value2] = arr;
 
           const valArr = valuemap.get(value1);
+
           for (let i = valArr.length - 1; i >= 0; --i) {
             const val = valArr[i];
             const [r, c] = val.split("_").map((x) => parseInt(x));
-            table[r][c].cur = value2;
+            table[r][c].pop();
+            table[r][c].push(value2);
             valArr.splice(i, 1);
 
             if (!valuemap.get(value2)) {
@@ -46,41 +52,31 @@ function solution(commands) {
 
       case "MERGE":
         [cmd, r1, c1, r2, c2] = arr;
+
         if (r1 === r2 && c1 === c2) break;
+        const cell1 = table[r1][c1];
+        const cell2 = table[r2][c2];
 
-        table[r1][c1].history.push(table[r1][c1].cur);
-        table[r2][c2].history.push(table[r2][c2].cur);
+        const value1 = cell1[cell1.length - 1];
+        const value2 = cell2[cell2.length - 1];
 
-        value1 = table[r1][c1].cur;
-        value2 = table[r2][c2].cur;
+        if ((value1 && !value2) || (value1 && value2)) {
+          table[r2][c2].push(value1);
 
-        if (value1 && !value2) {
-          table[r2][c2].cur = value1;
-
+          const curCell = r2 + "_" + c2;
           if (!valuemap.get(value1)) {
-            valuemap.set(value, [r2 + "_" + c2]);
+            valuemap.set(value1, [curCell]);
           } else {
-            valuemap.get(value).push(r2 + "_" + c2);
+            valuemap.get(value1).push(curCell);
           }
-        }
+        } else if (!value1 && value2) {
+          table[r1][c1].push(value2);
 
-        if (!value1 && value2) {
-          table[r1][c1].cur = value2;
-
+          const curCell = r1 + "_" + c1;
           if (!valuemap.get(value2)) {
-            valuemap.set(value, [r1 + "_" + c1]);
+            valuemap.set(value2, [curCell]);
           } else {
-            valuemap.get(value).push(r1 + "_" + c1);
-          }
-        }
-
-        if (value1 && value2) {
-          table[r2][c2].cur = value1;
-
-          if (!valuemap.get(value1)) {
-            valuemap.set(value, [r2 + "_" + c2]);
-          } else {
-            valuemap.get(value).push(r2 + "_" + c2);
+            valuemap.get(value2).push(curCell);
           }
         }
 
@@ -88,10 +84,20 @@ function solution(commands) {
 
       case "UNMERGE":
         [cmd, r, c] = arr;
-        const history = table[r][c].history;
-        if (history.length > 1) {
-          table[r][c].cur = history[0];
-          table[r][c].history = [];
+        const targetCellVal = table[r][c];
+
+        if (targetCellVal.length === 0) break;
+
+        const curVal = targetCellVal[targetCellVal.length - 1];
+
+        const valArr = valuemap.get(curVal);
+        for (let i = valArr.length - 1; i >= 0; --i) {
+          const val = valArr[i];
+          const [x, y] = val.split("_").map((s) => parseInt(s));
+          const cellVal = table[x][y];
+          table[x][y] = [cellVal[0]];
+
+          valArr.splice(i, 1);
         }
 
         break;
