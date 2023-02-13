5e3307d6a15f32d65d6e18c3991cfa95b56ee8d5
suyeon woo <woosean999@gmail.com>
2023-02-13 20:06:10 +0900

    구명보트

--- programmers/42885/1.js
+++ programmers/42885/1.js
@@ -1,36 +1,19 @@
 function solution(people, limit) {
-  const idxArr = people.map((_, idx) => idx);
-  function combination(arr, num) {
-    const result = [];
-    if (num === 1) return arr.map((v) => [v]);
+  var answer = 0;
+  people.sort((a, b) => b - a);
+  let l = 0;
+  let r = people.length - 1;
 
-    arr.forEach((value, index, arr) => {
-      const fixed = value;
-      const restArr = arr.slice(index + 1);
-      const combinationArr = combination(restArr, num - 1);
-      let combined = combinationArr
-        .map((v) => [fixed, ...v])
-        .filter((comb) => {
-          return (
-            comb.reduce((prevSum, curI) => prevSum + people[curI], 0) <= limit
-          );
-        });
-      if (combined.length) result.push(...combined);
-    });
-    return result;
-  }
-  const peopleLen = people.length;
-
-  const combs = combination(idxArr, 2);
-  if (combs.length === 0) return peopleLen;
-  let solo = 0;
-  for (let i = 0; i < peopleLen; ++i) {
-    // 아무 조합에도 못 속한 사람 카운트
-    for (let comb of combs) {
-      if (comb[0] !== i && comb[1] !== i) solo++;
+  while (l < r) {
+    var sum = people[l] + people[r];
+    if (sum > limit) {
+      l++;
+    } else {
+      l++;
+      r--;
     }
+    answer++;
   }
-  const boatCnt = solo + Math.ceil((peopleLen - solo) / 2);
-
-  return boatCnt;
+  if (l == r) answer++;
+  return answer;
 }
