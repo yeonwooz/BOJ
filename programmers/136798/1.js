68bbf2f83f910d672871bf9d85f42a59d325d3c6
suyeon woo <woosean999@gmail.com>
2023-02-15 10:27:01 +0900

    기사단원의 무기

---
+++ programmers/136798/1.js
@@ -0,0 +1,26 @@
+function solution(number, limit, power) {
+  /*
+    let answer = 0
+     for (i=1번기사부터 number번 기사에 대해)
+     answer += ((i의 약수개수) <= limit ? (i의 약수개수)  : power)
+    */
+  let answer = 0;
+  for (let i = 1; i <= number; ++i) {
+    let cnt = getDivisorCnt(i);
+    answer += cnt <= limit ? cnt : power;
+  }
+  return answer;
+}
+
+function getDivisorCnt(num) {
+  if (num === 1) return 1;
+  let cnt = 1;
+  let i = 2;
+  while (i * i < num) {
+    if (num % i === 0) cnt++;
+    i++;
+  }
+  cnt *= 2;
+  if (i * i === num) cnt++;
+  return cnt;
+}
