40f7ee902b9855aed67011178ab6508b6c6fb34a
suyeon woo <woosean999@gmail.com>
2023-02-15 16:26:33 +0900

    WIP

--- programmers/133499/1.js
+++ programmers/133499/1.js
@@ -1,28 +1,36 @@
 function solution(babbling) {
-  let cnt = 0;
   for (let bab of babbling) {
+    console.log("bab", bab);
     const dict = new Set();
     dict.add("aya");
     dict.add("ye");
     dict.add("woo");
     dict.add("ma");
-    // console.log(dict.keys())
-    let flag = false;
+    let cnt = 0;
+    let flag = true;
 
-    for (let word of dict.keys()) {
-      if (bab.length === 1) break;
-      const splitted = bab.split(word);
-      const len = splitted.length;
-      if (len === 1) continue;
-      if (len >= 3) break;
-
-      bab = splitted.join("");
-      if (!bab) break;
-      dict.delete(word);
-      flag = true;
+    while (bab) {
+      for (let word of dict.keys()) {
+        if (!bab) break;
+        // bab이 이 단어를 포함하는가
+        const splitted = bab.split(word);
+        const splitLen = splitted.length;
+        if (splitLen === 2) {
+          cnt++;
+          bab = splitted.join("");
+          if (bab.length === 1 && !dict.has(bab)) {
+            flag = false;
+            break;
+          }
+          dict.delete(word);
+        }
+        if (splitLen === 3) {
+          flag = false;
+          break;
+        }
+      }
+      if (!flag) break;
     }
-
-    if (flag) cnt++;
+    return cnt;
   }
-  return cnt;
 }
