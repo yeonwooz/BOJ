d660ce909261069408ae4d56740bfe6a8af125d1
suyeon woo <woosean999@gmail.com>
2023-02-16 21:43:33 +0900

    체육복 - Lost 기준으로 재풀이

--- programmers/42862/1.js
+++ programmers/42862/1.js
@@ -1,20 +1,24 @@
 function solution(n, lost, reserve) {
-  // let cnt = n - lost.length  // 체육복 도난당하지 않은 학생의 수
   // reserve 의 번호중에서 lost에도 해당하는 학생이 있다면 제거
   const filteredReserve = reserve.filter((value) => !lost.includes(value));
   lost = lost.filter((value) => !reserve.includes(value));
   reserve = filteredReserve;
 
-  for (let i = 0; i < reserve.length; ++i) {
-    const num = reserve[i];
-    if (lost.includes(num - 1)) {
-      lost.splice(lost.indexOf(num - 1), 1);
-      // cnt++
-    } else if (lost.includes(num + 1)) {
-      lost.splice(lost.indexOf(num + 1), 1);
-      // cnt++
+  lost.sort((a, b) => a - b);
+  const lostLen = lost.length;
+  let cnt = n - lostLen;
+
+  for (let i = 0; i < lostLen; ++i) {
+    if (reserve.length === 0) break;
+    const num = lost[i];
+    if (reserve.includes(num - 1)) {
+      reserve.splice(reserve.indexOf(num - 1), 1);
+      cnt++;
+    } else if (reserve.includes(num + 1)) {
+      reserve.splice(reserve.indexOf(num + 1), 1);
+      cnt++;
     }
   }
 
-  return n - lost.length;
+  return cnt;
 }
