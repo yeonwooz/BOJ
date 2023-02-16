fe528964003bb9ab275220a0903c13d15b285144
suyeon woo <woosean999@gmail.com>
2023-02-16 21:28:17 +0900

    WIP ) 체육복 오답케이스 2개

--- programmers/42862/1.js
+++ programmers/42862/1.js
@@ -1,9 +1,10 @@
 function solution(n, lost, reserve) {
   // let cnt = n - lost.length  // 체육복 도난당하지 않은 학생의 수
   // reserve 의 번호중에서 lost에도 해당하는 학생이 있다면 제거
-  const newReserve = reserve.filter((value) => !lost.includes(value));
+  const filteredReserve = reserve.filter((value) => !lost.includes(value));
   lost = lost.filter((value) => !reserve.includes(value));
-  reserve = newReserve
+  reserve = filteredReserve;
+
   for (let i = 0; i < reserve.length; ++i) {
     const num = reserve[i];
     if (lost.includes(num - 1)) {
