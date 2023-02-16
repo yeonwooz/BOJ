757d49f25673fb17cb94f665aef016a801f89b0c
suyeon woo <woosean999@gmail.com>
2023-02-16 12:31:33 +0900

    수정) map 대신  카운팅소트 두번 사용

--- programmers/131128/1.js
+++ programmers/131128/1.js
@@ -1,29 +1,21 @@
 function solution(a, b) {
-  const map = new Map();
+  let charsArray1 = Array(10).fill(0); // 해당 인덱스에 해당하는 수가 몇개인가
   for (let i = 0; i < a.length; ++i) {
-    if (!map.get(a[i])) {
-      map.set(a[i], 1);
-    } else {
-      map.set(a[i], map.get(a[i]) + 1);
-    }
+    charsArray1[+a[i]]++;
   }
-  let charsArray = Array(10).fill(0); // 해당 인덱스에 해당하는 수가 몇개인가
+  let charsArray2 = Array(10).fill(0); // 해당 인덱스에 해당하는 수가 몇개인가
   for (let i = 0; i < b.length; ++i) {
-    if (map.get(b[i])) {
-      const num = parseInt(b[i]);
-      charsArray[num]++;
-      map.set(b[i], map.get(b[i]) - 1);
-    }
+    charsArray2[+b[i]]++;
   }
-  let answer = "";
 
+  let answer = "";
   for (let i = 9; i >= 1; --i) {
-    const num = charsArray[i];
+    const num = Math.min(charsArray1[i], charsArray2[i]);
     if (num) {
       answer += String(i).repeat(num);
     }
   }
-  if (charsArray[0]) answer += "0";
+  if (charsArray1[0] && charsArray2[0]) answer += "0";
 
   return answer || "-1";
 }
