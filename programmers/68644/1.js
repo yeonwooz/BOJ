ab283e283bda9068a99a6ce17cde4f587dd3885d
suyeon woo <woosean999@gmail.com>
2023-02-16 12:58:49 +0900

    두 개 뽑아서 더하기

---
+++ programmers/68644/1.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+  const set = new Set();
+  for (let i = 0; i < numbers.length - 1; ++i) {
+    for (let j = i + 1; j < numbers.length; ++j) {
+      set.add(numbers[i] + numbers[j]);
+    }
+  }
+  return Array.from(set.keys()).sort((a, b) => a - b);
+}
