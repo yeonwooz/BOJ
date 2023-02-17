d72360d34df5754e42e10b33aff834c3f54b0e82
suyeon woo <woosean999@gmail.com>
2023-02-17 11:41:05 +0900

    시소 짝꿍 - 캐싱 로직 추가 및 시간복잡도 반으로 줄임

--- programmers/152996/1.js
+++ programmers/152996/1.js
@@ -1,25 +1,18 @@
 function solution(weights) {
-  const dists = [
-    [2, 3],
-    [3, 4],
-    [2, 4],
-  ];
-  const wlen = weights.length;
+  weights.sort((a, b) => b - a);
+  const rates = [1, 3 / 2, 2, 4 / 3]; // 내림차순이니깐
+  const store = {};
   let cnt = 0;
-  for (let i = 0; i < wlen - 1; ++i) {
-    for (let j = i + 1; j < wlen; ++j) {
-      const left = weights[i];
-      const right = weights[j];
-      if (left === right) {
-        cnt++;
-        continue;
+  for (let i = 0; i < weights.length; ++i) {
+    const w = weights[i];
+    rates.forEach((rate) => {
+      const comp = rate * w;
+      if (comp && store[comp]) {
+        cnt += store[comp];
       }
-      for (let [dist1, dist2] of dists) {
-        // 만약 left * a === right * b -> cnt++
-        if (left * dist1 === right * dist2 || left * dist2 === right * dist1)
-          cnt++;
-      }
-    }
+    });
+    if (!store[w]) store[w] = 1;
+    else store[w]++;
   }
   return cnt;
 }
