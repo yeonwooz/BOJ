f7e63cf894e7e093aba11a78900326edd136b82a
suyeon woo <woosean999@gmail.com>
2023-02-18 17:10:43 +0900

    heapify 로직 수정

--- programmers/91086/1.js
+++ programmers/91086/1.js
@@ -37,43 +37,39 @@ class MaxHeap {
   heapify() {
     // 루트부터 끝까지 재정렬
     let rootIdx = 1;
+    let leftIdx = rootIdx * 2;
+    let rightIdx = leftIdx + 1;
 
-    while (rootIdx <= this.heap.length - 1) {
-      let leftIdx = rootIdx * 2;
-      let rightIdx = leftIdx + 1;
-      let maxNodeIdx = rootIdx;
-      let newRootIdx = this.tail * 2;
-      // 왼쪽자식이 없을 때 => 종료
-      if (!this.heap[leftIdx]) break;
-
-      // 오른쪽 자식이 없거나, 둘다 있고 왼쪽자식이 오른쪽 자식보다 크거나 같을 때 => 왼쪽서브트리 선택해서 비교
-      if (!this.heap[rightIdx] || this.heap[leftIdx] >= this.heap[rightIdx]) {
-        // 현재루트와 left노드 비교
-        if (this.heap[maxNodeIdx] < this.heap[leftIdx]) {
-          maxNodeIdx = leftIdx;
-          newRootIdx = leftIdx;
-        }
-      }
-
+    while (
+      this.heap[rootIdx] < this.heap[leftIdx] ||
+      this.heap[rootIdx] < this.heap[rightIdx]
+    ) {
       // 오른쪽자식이 존재하고 왼쪽보다 오른쪽이 클 때 => 오른쪽서브트리 선택해서 비교
       if (this.heap[rightIdx] && this.heap[leftIdx] < this.heap[rightIdx]) {
         // 현재루트와 right노드 비교
-        if (this.heap[maxNodeIdx] < this.heap[rightIdx]) {
-          maxNodeIdx = rightIdx;
-          newRootIdx = rightIdx;
+        if (this.heap[rootIdx] < this.heap[rightIdx]) {
+          [this.heap[rightIdx], this.heap[rootIdx]] = [
+            this.heap[rootIdx],
+            this.heap[rightIdx],
+          ];
+          rootIdx = rightIdx;
         }
       }
 
-      // rootIdx = 자식 둘 중 값이 존재하고, 더 큰것
-      // maxNodeIdx !== rootIdx 이면 스왑
-      if (this.heap[maxNodeIdx] != this.heap[rootIdx]) {
-        [this.heap[maxNodeIdx], this.heap[rootIdx]] = [
-          this.heap[rootIdx],
-          this.heap[maxNodeIdx],
-        ];
+      // 오른쪽 자식이 없거나, 둘다 있고 왼쪽자식이 오른쪽 자식보다 크거나 같을 때 => 왼쪽서브트리 선택해서 비교
+      else {
+        // 현재루트와 left노드 비교
+        if (this.heap[rootIdx] < this.heap[leftIdx]) {
+          [this.heap[leftIdx], this.heap[rootIdx]] = [
+            this.heap[rootIdx],
+            this.heap[leftIdx],
+          ];
+          rootIdx = leftIdx;
+        }
       }
 
-      rootIdx = newRootIdx;
+      leftIdx = rootIdx * 2;
+      rightIdx = leftIdx + 1;
     }
   }
 }
