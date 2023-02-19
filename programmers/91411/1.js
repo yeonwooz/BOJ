103bb2dc20a9342f1b069337d6519d91c82617ff
suyeon woo <woosean999@gmail.com>
2023-02-19 19:37:48 +0900

    WIP

--- programmers/91411/1.js
+++ programmers/91411/1.js
@@ -81,35 +81,35 @@ function solution(N, road, K) {
   // 방문배열
   const visited = Array(N + 1).fill(0);
 
-  // 큐
-  let q = [];
   dijkstra(1);
 
   return costs.filter((x) => x <= K).length;
 
-  function dijkstra(v) {
-    visited[v] = 1;
+  function dijkstra(start) {
     const minheap = new MinHeap();
-
-    for (let next = 1; next <= N; ++next) {
-      // next에 방문한 적 없고 popped -> next 서로 연결되어 있으면
-      // 정점과 연결된 점들 모두 탐색하며 우선순위큐 생성
-      if (!q.includes(next) && !visited[next] && board[v][next]) {
-        costs[next] = Math.min(costs[next], costs[v] + board[v][next]);
-        minheap.insert({
-          nodeNum: next,
-          cost: costs[next],
-        });
-      }
-    }
+    minheap.insert({
+      nodeNum: start,
+      cost: 0,
+    });
+    visited[start] = 1;
 
     while (minheap.heap.length > 1) {
-      q.push(minheap.pop()?.nodeNum);
-    }
-
-    if (q.length) {
-      const popped = q.shift();
-      dijkstra(popped);
+      const popped = minheap.pop().nodeNum;
+      for (let next = 1; next <= N; ++next) {
+        // next에 방문한 적 없고 popped -> next 서로 연결되어 있으면
+        // 정점과 연결된 점들 모두 탐색하며 우선순위큐 생성
+        if (!visited[next] && board[popped][next]) {
+          visited[next] = 1;
+          costs[next] = Math.min(
+            costs[next],
+            costs[popped] + board[popped][next]
+          );
+          minheap.insert({
+            nodeNum: next,
+            cost: costs[next],
+          });
+        }
+      }
     }
   }
 }
