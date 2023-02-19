5ce3d2c4663cb0ba5e328da3f2e72f23a70cf08f
suyeon woo <woosean999@gmail.com>
2023-02-19 16:29:13 +0900

    소수 찾기 - 에라토스테네스의 체

---
+++ programmers/91385/1.js
@@ -0,0 +1,15 @@
+function solution(n) {
+  return eratos(n).length;
+}
+
+function eratos(n) {
+  const prime = [false, false, ...Array(n - 1).fill(true)];
+  for (let i = 2; i * i <= n; ++i) {
+    if (prime[i]) {
+      for (let j = i * 2; j <= n; j += i) {
+        prime[j] = false;
+      }
+    }
+  }
+  return prime.filter(Boolean);
+}
