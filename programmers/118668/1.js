1cb52d21fd8509c1a2f9952923565e120dd52c25
suyeon woo <woosean999@gmail.com>
2023-02-20 01:23:36 +0900

    코딩 테스트 공부 - 이차원DP배열 /알고력 코딩력

--- programmers/118668/1.js
+++ programmers/118668/1.js
@@ -1,40 +1,69 @@
 function solution(alp, cop, problems) {
   let alpGoal = 0;
   let copGoal = 0;
-
-  const costsDP = Array.from(Array(21), () => Array(21).fill(Infinity));
-  // 알고력 i점, 코딩력 j점을 얻기 위해 드는 최소시간
-
   for (let i = 0; i < problems.length; ++i) {
     const [alp_req, cop_req, alp_rwd, cop_rwd, cost] = problems[i];
     if (alpGoal < alp_req) alpGoal = alp_req;
     if (copGoal < cop_req) copGoal = cop_req;
-    let m = 1;
-    while (alp_rwd * m <= alpGoal || cop_rwd * m <= copGoal) {
-      if (alp_rwd * m <= alpGoal && cop_rwd * m <= copGoal) {
-        costsDP[alp_rwd * m][cop_rwd * m] = Math.min(
-          costsDP[alp_rwd * m][cop_rwd * m],
-          cost * m
-        );
-      } else if (cop_rwd * m > copGoal) {
-        for (let x = 1; x <= copGoal; ++x) {
-          costsDP[alp_rwd * m][x] = Math.min(costsDP[alp_rwd * m][x], cost * m);
-        }
-      } else if (alp_rwd * m > alpGoal) {
-        for (let x = 1; x <= alpGoal; ++x) {
-          costsDP[x][cop_rwd * m] = Math.min(costsDP[x][cop_rwd * m], cost * m);
+  }
+  if (alp > alpGoal) alp = alpGoal;
+  if (cop > copGoal) cop = copGoal;
+
+  const costsDP = Array.from(new Array(151), () =>
+    new Array(151).fill(Infinity)
+  );
+  costsDP[alp][cop] = 0;
+
+  for (let i = alp; i <= alpGoal; i++) {
+    for (let j = cop; j <= copGoal; j++) {
+      if (i == alpGoal && j == copGoal) break;
+
+      if (i < alpGoal) {
+        costsDP[i + 1][j] = Math.min(costsDP[i + 1][j], costsDP[i][j] + 1);
+      }
+
+      if (j < copGoal) {
+        costsDP[i][j + 1] = Math.min(costsDP[i][j + 1], costsDP[i][j] + 1);
+      }
+
+      for (let num = 0; num < problems.length; num++) {
+        const [alp_req, cop_req, alp_rwd, cop_rwd, cost] = problems[num];
+
+        if (i >= alp_req && j >= cop_req) {
+          // 풀 수 있다면
+          const alpSum = i + alp_rwd;
+          const copSum = j + cop_rwd;
+
+          const point = costsDP[i][j] + cost;
+          if (alpSum >= alpGoal && copSum >= copGoal) {
+            // 둘다 만족하게 되었다면
+            // 기존 비용과, i,j능력치를 가지고 cost시간만큼 푼 비용 비교
+            costsDP[alpGoal][copGoal] = Math.min(
+              costsDP[alpGoal][copGoal],
+              point
+            );
+          } else if (alpSum >= alpGoal) {
+            // 알고력만 만족하게 되었다면
+            costsDP[alpGoal][j + cop_rwd] = Math.min(
+              costsDP[alpGoal][j + cop_rwd],
+              point
+            );
+          } else if (copSum >= copGoal) {
+            // 코딩력만 만족하게 되었다면
+            costsDP[i + alp_rwd][copGoal] = Math.min(
+              costsDP[i + alp_rwd][copGoal],
+              point
+            );
+          } else {
+            // 둘다 목표에 도달하지 못했음
+            costsDP[i + alp_rwd][j + cop_rwd] = Math.min(
+              costsDP[i + alp_rwd][j + cop_rwd],
+              point
+            );
+          }
         }
       }
-      m++;
     }
   }
-
-  for (let a = 1; a <= alpGoal; ++a) {
-    for (let b = 1; b <= copGoal; ++b) {
-      costsDP[a][b] = Math.min(costsDP[a][b], Math.max(a, b));
-    }
-  }
-
-  console.log(costsDP);
   return costsDP[alpGoal][copGoal];
 }
