423a574af74e6620dd0977e7ce5962e75c646d0e
suyeon woo <woosean999@gmail.com>
2023-02-20 00:47:34 +0900

    캐싱을 이차원배열로

--- programmers/118668/1.js
+++ programmers/118668/1.js
@@ -1,30 +1,30 @@
 function solution(alp, cop, problems) {
-  // let sorted = [...problems]
-  // const alpGoal = sorted.sort((a,b)=>b[0]-a[0])[0][0]
-  // const copGoal = sorted.sort((a,b)=>b[1]-a[1])[0][1]
-  // console.log(alpGoal, copGoal)
-  let alpGoal = 0;
-  let copGoal = 0;
+  let sorted = [...problems];
+  const alpGoal = sorted.sort((a, b) => b[0] - a[0])[0][0];
+  const copGoal = sorted.sort((a, b) => b[1] - a[1])[0][1];
 
-  const alpCosts = Array.from(Array(21), (x, i) => i); // 알고력 i점을 얻기 위해 드는 최소시간
-  const copCosts = Array.from(Array(21), (x, i) => i); // 코딩력 i점을 얻기 위해 드는 최소시간
+  const costsDP = Array.from(Array(alpGoal + 1), () =>
+    Array(copGoal + 1).fill(Infinity)
+  );
+  // 알고력 i점, 코딩력 j점을 얻기 위해 드는 최소시간
 
   for (let i = 0; i < problems.length; ++i) {
     const num = i + 1;
     const [alp_req, cop_req, alp_rwd, cop_rwd, cost] = problems[i];
-    if (alpGoal < alp_req) alpGoal = alp_req;
-    if (copGoal < cop_req) copGoal = cop_req;
-    let d = 1;
-    while (alp_rwd > cost && alp_rwd * d <= alpGoal) {
-      alpCosts[alp_rwd * d] = Math.min(alpCosts[alp_rwd * d], cost * d);
-      d++;
+    let m = 1;
+    while (alp_rwd * m <= alpGoal && cop_rwd * m <= copGoal) {
+      costsDP[alp_rwd * m][cop_rwd * m] = Math.min(
+        costsDP[alp_rwd * m][cop_rwd * m],
+        cost * m
+      );
+      m++;
     }
-    d = 1;
-    while (cop_rwd > cost && cop_rwd * d <= copGoal) {
-      copCosts[cop_rwd * d] = Math.min(copCosts[cop_rwd * d], cost * d);
-      d++;
+
+    for (let a = 1; a <= alpGoal; ++a) {
+      for (let b = 1; b <= copGoal; ++b) {
+        costsDP[a][b] = Math.min(costsDP[a][b], Math.max(a, b));
+      }
     }
   }
-  console.log(alpCosts);
-  console.log(copCosts);
+  return costsDP[alpGoal][copGoal];
 }
