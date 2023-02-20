d260580d00518f7d7ac9ee2e0a236ef50af846d5
suyeon woo <woosean999@gmail.com>
2023-02-20 11:19:48 +0900

    [WIP] 유클리드 호제법 사용

--- programmers/135807/1.js
+++ programmers/135807/1.js
@@ -2,40 +2,29 @@ function solution(arrayA, arrayB) {
   // 배열길이 최대 500000 => 시간복잡도 logN, N, nLogN까지 허용
   // a 탐색범위 최대 100000000 => logN, N 까지 허용
   // (https://wooono.tistory.com/481)
-  let maxA = Math.max(Math.max(...arrayA), Math.max(...arrayB));
+  // 유클리드 호제법 : a % b -> 0이면 b가 gcd, 나머지가 있으면 gcd(b, a % b)
 
-  let a = maxA;
-  let found = false;
-  while (a >= 2) {
-    let aFlag = [0, 0]; // 없는 것, 있는 것
-    for (let i of arrayA) {
-      // 나눌수있고 없고
-      if (aFlag[0] && aFlag[1]) break;
-      if (i % a) {
-        aFlag[0]++;
-      } else {
-        aFlag[1]++;
-      }
-    }
+  let a1 = getGcd(arrayA, arrayB);
+  let a2 = getGcd(arrayB, arrayA);
+  return Math.max(a1, a2);
+}
 
-    let bFlag = [0, 0];
-    for (let j of arrayB) {
-      // 나눌수있고 없고
-      if (bFlag[0] && bFlag[1]) break;
-      if (j % a) {
-        bFlag[0]++;
-      } else {
-        bFlag[1]++;
-      }
-    }
-    if (
-      (aFlag[0] && !aFlag[1] && !bFlag[0] && bFlag[1]) ||
-      (!aFlag[0] && aFlag[1] && bFlag[0] && !bFlag[1])
-    ) {
-      found = true;
-      break;
-    }
-    a--;
+function getGcd(targetArr, counterArr) {
+  // targetArr, counterArr의 원소들에 대해 gcd를 각각 구한다
+  // gcd중 하나라도 1이면 return 0
+  // targetArr의 gcd가 counterArr 중 하나라도 나눌 수 있으면 0
+  // return gcd
+  const gcdFunc = (a, b) => (a % b === 0 ? b : gcdFunc(b, a % b));
+  let gcd = 1;
+  for (let i = 0; i < targetArr.length - 1; ++i) {
+    const max = Math.max(targetArr[i], targetArr[i + 1]);
+    const min = Math.min(targetArr[i], targetArr[i + 1]);
+    gcd = Math.max(gcd, gcdFunc(max, min));
   }
-  return found ? a : 0;
+  if (gcd === 1) return 0;
+  for (let i = 0; i < counterArr.length; ++i) {
+    if (counterArr[i] % gcd === 0) return 0;
+  }
+
+  return gcd;
 }
