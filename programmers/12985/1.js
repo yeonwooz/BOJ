0027133fff0e3bea79029362876068395efbe087
suyeon woo <woosean999@gmail.com>
2023-02-21 00:04:52 +0900

    예상 대진표

--- programmers/12985/1.js
+++ programmers/12985/1.js
@@ -7,17 +7,11 @@ function solution(n, a, b) {
     ㅁ ㅁ ㅁ ㅁ ㅁ ㅁ ㅁ ㅁ 
     */
   // 2로 나누다가 처음으로 반대편에 위치할 때
-  return recur(0, n, a, b, Math.log2(n));
-}
-
-function recur(from, n, a, b, level) {
-  if (n === 1 || level === 1) return level;
-  if (a <= from + n / 2 && b > from + n / 2) return level;
   let answer = 0;
-  for (let i = 0; i < n; i += n / 2) {
-    // n = 8  -> 0,4
-    // n = 4  -> 0,2,4,6
-    answer = Math.max(answer, recur(i, n / 2, a, b, level - 1));
+  while (a != b) {
+    a = Math.ceil(a / 2);
+    b = Math.ceil(b / 2);
+    answer++;
   }
   return answer;
 }
