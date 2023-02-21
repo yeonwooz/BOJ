38e083faf44b384e5f63e1dcff193ef0233af771
suyeon woo <woosean999@gmail.com>
2023-02-22 02:08:19 +0900

    [WIP]미로 탈출 - dijkstra

--- programmers/159993/1.js
+++ programmers/159993/1.js
@@ -1,3 +1,65 @@
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(v) {
+    this.heap.push(v);
+    let curIdx = this.heap.length - 1;
+    let parentIdx = Math.floor(curIdx / 2);
+    while (curIdx > 1) {
+      if (this.heap[curIdx]?.cost < this.heap[parentIdx]?.cost) {
+        [this.heap[curIdx], this.heap[parentIdx]] = [
+          this.heap[parentIdx],
+          this.heap[curIdx],
+        ];
+        curIdx = parentIdx;
+        parentIdx = Math.floor(curIdx / 2);
+      } else break;
+    }
+  }
+
+  pop() {
+    if (this.isEmpty()) return null;
+    if (this.heap.length === 2) return this.heap.pop();
+    const top = this.heap[1];
+    this.heap[1] = this.heap.pop();
+    this.heapify();
+    return top;
+  }
+
+  heapify() {
+    let curIdx = 1;
+    let left = 2;
+    let right = 3;
+
+    while (
+      this.heap[curIdx]?.cost > this.heap[right]?.cost ||
+      this.heap[curIdx]?.cost > this.heap[left]?.cost
+    ) {
+      if (this.heap[curIdx]?.cost > this.heap[right]?.cost) {
+        [this.heap[curIdx], this.heap[right]] = [
+          this.heap[right],
+          this.heap[curIdx],
+        ];
+        curIdx = right;
+      } else {
+        [this.heap[curIdx], this.heap[left]] = [
+          this.heap[left],
+          this.heap[curIdx],
+        ];
+        curIdx = left;
+      }
+      left = curIdx * 2;
+      right = left + 1;
+    }
+  }
+
+  isEmpty() {
+    return this.heap.length === 1;
+  }
+}
+
 function solution(maps) {
   /*
     L->S, L->E 경로가 존재하지 않으면 -1
@@ -6,10 +68,12 @@ function solution(maps) {
   let S = [];
   let E = [];
   let L = [];
-  const visited = Array.from(Array(maps.length), () => []);
+  const visited1 = Array.from(Array(maps.length), () => []);
+  const visited2 = Array.from(Array(maps.length), () => []);
   for (let i = 0; i < maps.length; ++i) {
     // 지도 스캔
-    visited[i] = Array(maps[i].length).fill(0);
+    visited1[i] = Array(maps[i].length).fill(0);
+    visited2[i] = Array(maps[i].length).fill(0);
     for (let j = 0; j < maps[i].length; ++j) {
       if (maps[i][j] === "S") S = [i, j];
       if (maps[i][j] === "E") E = [i, j];
@@ -17,41 +81,58 @@ function solution(maps) {
     }
   }
 
-  visited[L[0]][L[1]] = 1;
   const dx = [0, 0, -1, 1];
   const dy = [1, -1, 0, 0];
   let startToLeverTime = 10001;
   let leverToExitTime = 10001;
 
-  function DFS([i, j], time) {
-    if (i === S[0] && j === S[1]) {
-      startToLeverTime = Math.min(startToLeverTime, time);
-      return;
-    }
-    if (i === E[0] && j === E[1]) {
-      leverToExitTime = Math.min(leverToExitTime, time);
-      return;
-    }
-    for (let idx = 0; idx < 4; ++idx) {
-      const nx = i + dx[idx];
-      const ny = j + dy[idx];
+  function BFS(v, dest, time, visited) {
+    const heap = new MinHeap();
+    heap.push({
+      node: v, // 레버의 [i,j]
+      cost: time,
+    });
 
-      if (
-        0 <= nx &&
-        nx < maps.length &&
-        0 <= ny &&
-        ny < maps[0].length &&
-        !visited[nx][ny] &&
-        maps[nx][ny] !== "X"
-      ) {
-        visited[nx][ny] = 1;
-        DFS([nx, ny], time + 1);
-        visited[nx][ny] = 0;
+    while (!heap.isEmpty()) {
+      const { node, cost } = heap.pop();
+      const [i, j] = node;
+      visited[i][j] = 1;
+      if (dest === "s") {
+        if (i === S[0] && j === S[1]) {
+          startToLeverTime = Math.min(startToLeverTime, cost);
+          break;
+        }
+      } else if (dest === "e") {
+        if (i === E[0] && j === E[1]) {
+          leverToExitTime = Math.min(leverToExitTime, cost);
+          break;
+        }
+      }
+
+      for (let idx = 0; idx < 4; ++idx) {
+        const nx = i + dx[idx];
+        const ny = j + dy[idx];
+        if (
+          0 <= nx &&
+          nx < maps.length &&
+          0 <= ny &&
+          ny < maps[0].length &&
+          !visited[nx][ny] &&
+          maps[nx][ny] !== "X"
+        ) {
+          heap.push({
+            node: [nx, ny],
+            cost: cost + 1,
+          });
+        }
       }
     }
   }
 
-  DFS(L, 0);
-  if (startToLeverTime === 10001 || leverToExitTime === 10001) return -1;
+  BFS(L, "s", 0, visited1);
+  if (startToLeverTime === 10001) return -1;
+  console.log("=========");
+  BFS(L, "e", 0, visited2);
+  if (leverToExitTime === 10001) return -1;
   return startToLeverTime + leverToExitTime;
 }
