fb61e9a0754f103f6204e585f006c8418ad1c33f
suyeon woo <woosean999@gmail.com>
2023-02-21 13:16:20 +0900

    피로도

--- programmers/87946/1.js
+++ programmers/87946/1.js
@@ -1,23 +1,26 @@
 function solution(k, dungeons) {
-  let cnt = 0;
+  let maxCnt = 0;
+
   const len = dungeons.length;
+  const visited = Array(len).fill(0);
   for (let i = 0; i < len; ++i) {
     if (k < dungeons[i][0]) continue;
-    const visited = Array(len).fill(0);
-    cnt = Math.max(cnt, bruteForce(k, dungeons, i, visited, 0));
+    visited[i] = 1;
+    bruteForce(k, dungeons, i, visited, 0);
+    visited[i] = 0;
   }
-  return cnt;
-}
+  return maxCnt;
 
-function bruteForce(k, dungeons, i, visited, cnt) {
-  visited[i] = 1;
-  k -= dungeons[i][1];
-  cnt += 1;
-  if (k <= 0) return cnt;
-  for (let j = 0; j < dungeons.length; ++j) {
-    if (!visited[j] && k >= dungeons[j][0]) {
-      cnt = Math.max(cnt, bruteForce(k, dungeons, j, visited, cnt));
+  function bruteForce(k, dungeons, i, visited, cnt) {
+    k -= dungeons[i][1];
+    cnt += 1;
+    for (let j = 0; j < dungeons.length; ++j) {
+      if (!visited[j] && k >= dungeons[j][0]) {
+        visited[j] = 1;
+        bruteForce(k, dungeons, j, visited, cnt);
+        visited[j] = 0;
+      }
     }
+    maxCnt = Math.max(maxCnt, cnt);
   }
-  return cnt;
 }
