0f5d245e5be8dee7a5437c684a7f523723a49bf2
suyeon woo <woosean999@gmail.com>
2023-02-25 15:46:29 +0900

    게임 맵 최단거리 - 가까운 곳으로만 바로 이동해야 하므로 최소힙 사용할 수 없음

--- programmers/1844/1.js
+++ programmers/1844/1.js
@@ -1,66 +1,3 @@
-class MinHeap {
-  constructor() {
-    this.heap = [null]; // {i,j,cost}
-  }
-
-  push(val) {
-    this.heap.push(val); // 맨 끝에 넣고, 위로 올라가면서 부모가 더 크면 교체
-    let curIdx = this.heap.length - 1;
-    let parentIdx = Math.floor(curIdx / 2);
-    while (curIdx > 1) {
-      if (this.heap[curIdx]?.cost < this.heap[parentIdx]?.cost) {
-        [this.heap[curIdx], this.heap[parentIdx]] = [
-          this.heap[parentIdx],
-          this.heap[curIdx],
-        ];
-        curIdx = parentIdx;
-        parentIdx = Math.floor(curIdx / 2);
-      } else break;
-    }
-  }
-
-  pop() {
-    // 맨 위를 빼고 맨 아래꺼를 위로 올린 후에, 각 부모에 대해서 자식 중 하나가 더 작다면 좌우 비교하며 더작은쪽과 교체
-    if (this.isEmpty()) return null;
-    if (this.heap.length === 2) return this.heap.pop();
-    const popped = this.heap.pop();
-    this.heap[1] = popped;
-    this.heapify();
-    return popped;
-  }
-
-  heapify() {
-    let curIdx = 1;
-    let left = 2;
-    let right = 3;
-    while (
-      this.heap[curIdx]?.cost > this.heap[right]?.cost ||
-      this.heap[curIdx]?.cost > this.heap[left]?.cost
-    ) {
-      if (this.heap[left]?.cost > this.heap[right]?.cost) {
-        [this.heap[right], this.heap[curIdx]] = [
-          this.heap[curIdx],
-          this.heap[right],
-        ];
-        curIdx = right;
-      } else {
-        [this.heap[left], this.heap[curIdx]] = [
-          this.heap[curIdx],
-          this.heap[left],
-        ];
-        curIdx = left;
-      }
-
-      left = curIdx * 2;
-      right = left + 1;
-    }
-  }
-
-  isEmpty() {
-    return this.heap.length === 1;
-  }
-}
-
 function solution(maps) {
   const n = maps.length;
   const m = maps[0].length;
@@ -76,14 +13,14 @@ function solution(maps) {
   return lastCost === Infinity ? -1 : lastCost;
 
   function BFS() {
-    const heap = new MinHeap();
+    const heap = [];
     costs[0][0] = 1;
     visited[0][0] = 1;
     heap.push({ i: 0, j: 0, cost: 1 });
 
     let cnt = 0;
-    while (!heap.isEmpty()) {
-      const { i, j, cost } = heap.pop();
+    while (heap.length) {
+      const { i, j, cost } = heap.shift();
 
       for (let idx = 0; idx < 4; ++idx) {
         const nr = dr[idx] + i;
