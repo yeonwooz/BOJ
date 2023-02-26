f251ad265a875e43c28cc44b579da5b28ca15120
suyeon woo <woosean999@gmail.com>
2023-02-26 22:45:32 +0900

    for문 감소

--- programmers/12949/1.js
+++ programmers/12949/1.js
@@ -6,11 +6,8 @@ function solution(arr1, arr2) {
   for (let i = 0; i < answer.length; ++i) {
     for (let j = 0; j < answer[0].length; ++j) {
       let compSum = 0;
-      for (let col = 0; col < arr1[0].length; ++col) {
-        for (let row = 0; row < arr2.length; ++row) {
-          if (col !== row) continue;
-          compSum += arr1[i][col] * arr2[row][j];
-        }
+      for (let k = 0; k < arr1[0].length; ++k) {
+        compSum += arr1[i][k] * arr2[k][j];
       }
       answer[i][j] = compSum;
     }
