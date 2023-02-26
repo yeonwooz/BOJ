212c728c994865a60cddb192995143d63285ec87
suyeon woo <woosean999@gmail.com>
2023-02-26 15:19:50 +0900

    [WIP] 퍼즐 조각 채우기-보드경로배열, 퍼즐배열

--- programmers/84021/1.js
+++ programmers/84021/1.js
@@ -2,19 +2,29 @@ function solution(game_board, table) {
   const n = game_board.length;
   const dr = [1, -1, 0, 0];
   const dc = [0, 0, -1, 1];
-  const visited = Array.from(Array(n), () => Array(n).fill(0));
+  const boardVisited = Array.from(Array(n), () => Array(n).fill(0));
+  const boardRoutes = [];
 
-  let answer = 0;
   for (let i = 0; i < n; ++i) {
     for (let j = 0; j < n; ++j) {
-      if (game_board[i][j] || visited[i][j]) continue;
-      const route = DFS(i, j, []);
-      console.log(i, j, "에서 시작 =>", route);
+      if (game_board[i][j] || boardVisited[i][j]) continue;
+      boardRoutes.push(boardDFS(i, j, []));
     }
   }
+  console.log("boardRoutes", boardRoutes);
 
-  function DFS(i, j, path) {
-    visited[i][j] = 1;
+  const tableVisited = Array.from(Array(n), () => Array(n).fill(0));
+  const puzzles = [];
+  for (let i = 0; i < n; ++i) {
+    for (let j = 0; j < n; ++j) {
+      if (!table[i][j] || tableVisited[i][j]) continue;
+      puzzles.push(tableDFS(i, j, []));
+    }
+  }
+  console.log("puzzles", puzzles);
+
+  function boardDFS(i, j, path) {
+    boardVisited[i][j] = 1;
     path.push([i, j]);
     for (let idx = 0; idx < 4; ++idx) {
       const nr = dr[idx] + i;
@@ -24,10 +34,30 @@ function solution(game_board, table) {
         nr < n &&
         0 <= nc &&
         nc < n &&
-        !visited[nr][nc] &&
+        !boardVisited[nr][nc] &&
         !game_board[nr][nc]
       ) {
-        DFS(nr, nc, path);
+        boardDFS(nr, nc, path);
+      }
+    }
+    return path;
+  }
+
+  function tableDFS(i, j, path) {
+    tableVisited[i][j] = 1;
+    path.push([i, j]);
+    for (let idx = 0; idx < 4; ++idx) {
+      const nr = dr[idx] + i;
+      const nc = dc[idx] + j;
+      if (
+        0 <= nr &&
+        nr < n &&
+        0 <= nc &&
+        nc < n &&
+        !tableVisited[nr][nc] &&
+        table[nr][nc]
+      ) {
+        tableDFS(nr, nc, path);
       }
     }
     return path;
