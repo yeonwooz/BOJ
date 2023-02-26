975490d53ec0564cdc8584be47fc3400ef79ff10
suyeon woo <woosean999@gmail.com>
2023-02-26 18:18:24 +0900

    정답 참고

--- programmers/84021/1.js
+++ programmers/84021/1.js
@@ -1,152 +1,111 @@
-function solution(game_board, table) {
-  const n = game_board.length;
-  const dr = [-1, 0, 1, 0];
-  const dc = [0, 1, 0, -1];
-  const directions = ["top", "right", "bottom", "left"];
-  const boardVisited = Array.from(Array(n), () => Array(n).fill(0));
-  const boardRoutes = [];
+// 정답 참고
 
-  for (let i = 0; i < n; ++i) {
-    for (let j = 0; j < n; ++j) {
-      if (game_board[i][j] || boardVisited[i][j]) continue;
-      boardRoutes.push(boardDFS(i, j, []));
+function getPuzzles(origin, number) {
+  const table = [];
+  for (let i = 0; i < origin.length; i++) {
+    const item = [];
+    for (let j = 0; j < origin[i].length; j++) {
+      item.push(origin[i][j]);
     }
+
+    table.push(item);
   }
 
-  const tableVisited = Array.from(Array(n), () => Array(n).fill(0));
-  const puzzles = new Map();
-  for (let i = 0; i < n; ++i) {
-    for (let j = 0; j < n; ++j) {
-      if (!table[i][j] || tableVisited[i][j]) continue;
-      puzzles.set(puzzles.size, tableDFS(i, j, []));
+  const num = number === 0 ? 1 : 0;
+  let arr = [];
+  const setPuzzle = (x, y) => {
+    if (table[x][y] === number) {
+      table[x][y] = num;
+      arr.push([x, y]);
+
+      if (y + 1 < table.length) setPuzzle(x, y + 1);
+      if (y - 1 >= 0) setPuzzle(x, y - 1);
+      if (x + 1 < table.length) setPuzzle(x + 1, y);
+      if (x - 1 >= 0) setPuzzle(x - 1, y);
     }
-  }
-  console.log(puzzles);
-  let answer = 0;
-  for (const route of boardRoutes) {
-    // 이 루트를 채울 수 있는 puzzle이 있다면 루트 길이만큼 보드를 채우고 해당 퍼즐조각은 제거
-    for (let idx = 0; idx < puzzles.size; ++idx) {
-      console.log("checked:", idx);
-      if (checkAllDirections(route, puzzles.get(idx))) {
-        console.log("===check DONE===");
-        answer += route.length;
-        puzzles.set(idx, null);
-        break;
+  };
+
+  const result = [];
+
+  for (let i = 0; i < table.length; i++) {
+    for (let j = 0; j < table[i].length; j++) {
+      if (table[i][j] === number) {
+        setPuzzle(i, j);
+        result.push(arr);
+        arr = [];
       }
     }
   }
-  return answer;
 
-  function boardDFS(i, j, path) {
-    boardVisited[i][j] = 1;
-    path.push([i, j]);
-    for (let idx = 0; idx < 4; ++idx) {
-      const nr = dr[idx] + i;
-      const nc = dc[idx] + j;
-      if (
-        0 <= nr &&
-        nr < n &&
-        0 <= nc &&
-        nc < n &&
-        !boardVisited[nr][nc] &&
-        !game_board[nr][nc]
-      ) {
-        boardDFS(nr, nc, path);
-      }
-    }
-    return path;
-  }
-
-  function tableDFS(i, j, path) {
-    tableVisited[i][j] = 1;
-    path.push([i, j]);
-    for (let idx = 0; idx < 4; ++idx) {
-      const nr = dr[idx] + i;
-      const nc = dc[idx] + j;
-      if (
-        0 <= nr &&
-        nr < n &&
-        0 <= nc &&
-        nc < n &&
-        !tableVisited[nr][nc] &&
-        table[nr][nc]
-      ) {
-        tableDFS(nr, nc, path);
-      }
-    }
-    return path;
-  }
-
-  function checkAllDirections(route, puzzle) {
-    // 퍼즐을 회전시켜서 경로와 일치시킬 수 있는지 체크
-    if (!route || !puzzle) return false;
-    const routeLen = route.length;
-    const puzzleLen = puzzle.length;
-    if (routeLen !== puzzleLen) return false;
-    if (routeLen === 1) return true;
-
-    console.log(route);
-    console.log(puzzle);
-
-    let foundSame = false;
-    for (let rotateCnt = 0; rotateCnt < 4; ++rotateCnt) {
-      // 이번 회전카운트에 대하여
-
-      let isSame = true;
-      for (let idx = 1; idx < puzzleLen; ++idx) {
-        const [prevI, prevJ] = puzzle[idx - 1];
-        const [curI, curJ] = puzzle[idx];
-
-        const diff = [curI - prevI, curJ - prevJ];
-
-        let dir = 0;
-        if (diff[0] < 0 && diff[1] === 0) dir = 0; // top
-        else if (diff[0] === 0 && diff[1] > 0) dir = 1; // right
-        else if (diff[0] > 0 && diff[1] === 0) dir = 2; // bottom
-        else if (diff[0] === 0 && diff[1] < 0) dir = 3; // left
-
-        console.log("변환전", puzzle[idx], dir);
-        dir = (dir + rotateCnt) % 4;
-        puzzle[idx][0] = dr[dir] + puzzle[idx - 1][0];
-        puzzle[idx][1] = dc[dir] + puzzle[idx - 1][1];
-
-        console.log("변환후", puzzle[idx], dir);
-
-        if (
-          route[idx][0] - route[idx - 1][0] !==
-            puzzle[idx][0] - puzzle[idx - 1][0] ||
-          route[idx][1] - route[idx - 1][1] !==
-            puzzle[idx][1] - puzzle[idx - 1][1]
-        ) {
-          isSame = false;
-          break;
-        }
-      }
-      if (isSame) {
-        foundSame = true;
-        break;
-      }
-    }
-
-    return foundSame;
-  }
+  return result
+    .map((data) =>
+      data.map((p, idx) => {
+        return idx === 0 ? p : [data[0][0] - p[0], data[0][1] - p[1]];
+      })
+    )
+    .sort((a, b) => b.length - a.length);
 }
 
 function getRotatedTable(table) {
-    // 90도 회전
-    const arr = new Array(table[0].length).fill(0).map(_ = [])
+  const arr = new Array(table[0].length).fill(0).map((_) => []);
 
-    for (let i = table.length -1; i >= 0; --i ) {
-        for (let j = 0; j < table[0].length; ++j) {
-            arr[j].push(table[i][j])
-        } 
+  for (let i = table.length - 1; i >= 0; i--) {
+    for (let j = 0; j < table[0].length; j++) {
+      arr[j].push(table[i][j]);
     }
-    
-    return arr
+  }
+
+  return arr;
 }
 
 function isMatch(spot, puzzle) {
-    for (let i = 1; i < spot.length; ++i) {
-        
-    }
+  for (let i = 1; i < spot.length; i++) {
+    if (spot[i][0] !== puzzle[i][0] || spot[i][1] !== puzzle[i][1])
+      return false;
+  }
+
+  return true;
+}
+
+function solution(gameBoard, table) {
+  const removePuzzle = (puzzle) => {
+    const originPuzzle = puzzle.map((data, idx) => {
+      return idx === 0
+        ? data
+        : [puzzle[0][0] - data[0], puzzle[0][1] - data[1]];
+    });
+
+    originPuzzle.forEach((data) => {
+      table[data[0]][data[1]] = 0;
+    });
+  };
+
+  let answer = 0;
+
+  // 게임 보드에서 퍼즐을 넣을 수 있는 모양들 파악
+  const slots = getPuzzles(gameBoard, 0);
+  console.log(slots);
+
+  // 3번 회전시켜서 보드에 퍼즐을 맞출 수 있는지 파악
+  for (let i = 0; i <= 3; i++) {
+    if (i !== 0) table = getRotatedTable(table); // 테이블 회전
+    const puzzles = getPuzzles(table, 1);
+
+    // slots vs. puzzles를 비교하고
+    puzzles.forEach((puzzle) => {
+      for (let j = 0; j < slots.length; j++) {
+        if (slots[j].length < puzzle.length) break;
+
+        // 퍼즐이 slots에 맞춰진다면 보드와 테이블에서 지우고 answer을 증가시킬 것
+        if (slots[j].length === puzzle.length && isMatch(slots[j], puzzle)) {
+          answer += puzzle.length;
+          removePuzzle(puzzle);
+          slots.splice(j, 1);
+          break;
+        }
+      }
+    });
+  }
+
+  return answer;
 }
