6d9c63c5187c2417a967e5f6cb5e5a2bb2ddd896
suyeon woo <woosean999@gmail.com>
2023-02-28 10:11:57 +0900

    [WIP] BFS

--- programmers/154540/1.js
+++ programmers/154540/1.js
@@ -10,30 +10,38 @@ function solution(maps) {
   for (let i = 0; i < n; ++i) {
     for (let j = 0; j < m; ++j) {
       if (!visited[i][j] && maps[i][j] !== "X") {
-        sums.push(DFS(i, j, parseInt(maps[i][j])));
+        visited[i][j] = 1;
+        BFS(i, j, +maps[i][j]);
       }
     }
   }
 
   return sums.length ? sums.sort() : [-1];
 
-  function DFS(i, j, sum) {
-    visited[i][j] = 1;
-    for (let idx = 0; idx < 4; ++idx) {
-      const nr = dr[idx] + i;
-      const nc = dc[idx] + j;
+  function BFS(s, e, val) {
+    const q = [];
+    q.push([s, e, val]);
+    let sum = 0;
+    while (q.length) {
+      let [i, j, k] = q.shift();
+      sum += k;
+      for (let idx = 0; idx < 4; ++idx) {
+        const nr = dr[idx] + i;
+        const nc = dc[idx] + j;
 
-      if (
-        0 <= nr &&
-        nr < n &&
-        0 <= nc &&
-        nc < m &&
-        !visited[nr][nc] &&
-        maps[nr][nc] !== "X"
-      ) {
-        sum = DFS(nr, nc, sum + parseInt(maps[nr][nc]));
+        if (
+          0 <= nr &&
+          nr < n &&
+          0 <= nc &&
+          nc < m &&
+          !visited[nr][nc] &&
+          maps[nr][nc] !== "X"
+        ) {
+          visited[nr][nc] = 1;
+          q.push([nr, nc, parseInt(maps[nr][nc])]);
+        }
       }
     }
-    return sum;
+    sums.push(sum);
   }
 }
