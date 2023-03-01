4ba2fe6f3a9e555c9bbe5fc952c281e35a474990
suyeon woo <woosean999@gmail.com>
2023-03-01 15:16:01 +0900

    [WIP] 최고의 집합 - 그리디 - core dumped

--- programmers/12938/1.js
+++ programmers/12938/1.js
@@ -1,21 +1,45 @@
 function solution(n, s) {
   const element = Math.floor(s / n);
   if (element < 1) return [-1];
-  return getBestSet(element, s);
+  const combs = dupcombination(
+    Array(s - 1)
+      .fill(0)
+      .map((_, i) => i + 1),
+    n,
+    s
+  );
+  let maxComp = -1;
+  let maxCompSet = [];
+  // console.log('combs', combs)
+  for (const comb of combs) {
+    if (comb.reduce((prev, cur) => prev + cur, 0) !== s) continue;
+    const comp = comb.reduce((prev, cur) => prev * cur, 1);
+    if (maxComp < comp) {
+      maxComp = comp;
+      maxCompSet = comb;
+    }
+  }
+  return maxCompSet;
 }
 
-function getBestSet(element, sum) {
-  let quot = Math.floor(sum / element);
-  let rest = sum % element;
-  let combination = [];
-  if (rest > 0) {
-    combination = Array(quot).fill(element);
-    combination[quot - 1] = element + rest;
-    // element 가 quot개, rest가 1개
-  } else {
-    combination = Array(quot).fill(element);
-    // element 가 quot개
+function dupcombination(arr, cnt, s) {
+  const result = [];
+
+  if (cnt === 1) {
+    return arr.map((v) => [v]);
   }
 
-  return combination;
+  arr.forEach((value, idx, arr) => {
+    const fixed = value;
+    const restArr = arr.slice(); // 중복허용
+    const comb = dupcombination(restArr, cnt - 1, s);
+
+    let combined = comb.map((v) => [fixed, ...v]);
+    combined = combined.filter(
+      (v) => v.reduce((prev, cur) => prev + cur, 0) === s
+    );
+    combined = [...combined].filter((v) => v.length);
+    result.push(...combined);
+  });
+  return result;
 }
