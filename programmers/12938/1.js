580abfda598c55e0bdb301a3435ec164f46db66b
suyeon woo <woosean999@gmail.com>
2023-03-01 15:21:05 +0900

    쉬운것을 어렵게 생각하지 말자,,

--- programmers/12938/1.js
+++ programmers/12938/1.js
@@ -1,45 +1,12 @@
 function solution(n, s) {
   const element = Math.floor(s / n);
   if (element < 1) return [-1];
-  const combs = dupcombination(
-    Array(s - 1)
-      .fill(0)
-      .map((_, i) => i + 1),
-    n,
-    s
-  );
-  let maxComp = -1;
-  let maxCompSet = [];
-  // console.log('combs', combs)
-  for (const comb of combs) {
-    if (comb.reduce((prev, cur) => prev + cur, 0) !== s) continue;
-    const comp = comb.reduce((prev, cur) => prev * cur, 1);
-    if (maxComp < comp) {
-      maxComp = comp;
-      maxCompSet = comb;
-    }
+  const rest = s % n;
+  const arr = new Array(n).fill(element);
+
+  // 나누어떨어지지 않는다면 배열의 마지막 원소부터 1을 더해준다.
+  for (let i = 0; i < rest; ++i) {
+    arr[arr.length - 1 - i]++;
   }
-  return maxCompSet;
-}
-
-function dupcombination(arr, cnt, s) {
-  const result = [];
-
-  if (cnt === 1) {
-    return arr.map((v) => [v]);
-  }
-
-  arr.forEach((value, idx, arr) => {
-    const fixed = value;
-    const restArr = arr.slice(); // 중복허용
-    const comb = dupcombination(restArr, cnt - 1, s);
-
-    let combined = comb.map((v) => [fixed, ...v]);
-    combined = combined.filter(
-      (v) => v.reduce((prev, cur) => prev + cur, 0) === s
-    );
-    combined = [...combined].filter((v) => v.length);
-    result.push(...combined);
-  });
-  return result;
+  return arr;
 }
