1c6f6df83ae50dd2ec9a1b2144ce2d3ca5783361
suyeon woo <woosean999@gmail.com>
2023-03-03 23:32:04 +0900

    외벽 점검

--- programmers/60062/1.js
+++ programmers/60062/1.js
@@ -1,86 +1,42 @@
 function solution(n, weak, dist) {
-  // 크게 도는 친구부터 각각의 친구가 시계방향 / 반시계 방향으로 최대한 많이
-  dist.sort((a, b) => b - a); // 내림차순 후 pop()
-  let weakCnt = weak.length;
-  let workerCnt = 0;
-  while (dist.length) {
-    if (weakCnt <= 0) break;
-    const range = dist.pop();
-    const fixed = getMaxFixCnt(n, weak, range);
-    const fixedCnt = fixed.length;
-    // console.log('range', range, 'fixed', fixed)
-    if (fixedCnt > 0) {
-      workerCnt++;
-      weakCnt -= fixedCnt;
-      weak = weak.filter((_, idx) => !fixed.includes(idx));
-      // console.log(weak)
+  const len = weak.length;
+  const linear_weak = new Array(len * 2 - 1).fill(0);  // 시계방향만 고려하기 위해 펼친다
+
+  for (let i = 0; i < len * 2 - 1; i++) {
+    linear_weak[i] = i < len ? weak[i] : weak[i - len] + n;
+  }
+
+  dist.sort((a, b) => b - a);
+
+  for (let i = 1; i <= dist.length; i++) {
+    const permutation = getPermutation(dist, i);  // 각 경로순열
+
+    for (const perm of permutation) {
+      for (let j = 0; j < len; j++) {
+        let line = linear_weak.slice(j, len + j); // 남은 작업량
+        for (const p of perm) {
+          const coverage = line[0] + p;  // 현재 작업자가 작업할 수 있는 범위
+          line = line.filter((e) => e > coverage);
+          if (!line.length) return i;
+        }
+      }
     }
   }
-  return workerCnt;
+
+  return -1;
 }
 
-function getMaxFixCnt(n, weak, range) {
-  let maxFixCnt = 0;
-  let fixed = []; //  고쳐진 것
+const getPermutation = (arr, cnt) => {
+  if (cnt === 1) return arr.map((v) => [v]);
+  const result = [];
 
-  // 시계방향
-  for (let i = 0; i < weak.length; ++i) {
-    //weak[i] 부터 시작할 경우
-    let cnt = 1;
-    let cur = i + 1;
+  arr.forEach((value, idx, arr) => {
+    const fixed = value
+    const restArr = arr.filter((_, index) => index !== idx);
+    const perms = getPermutation(restArr, cnt - 1);
+    const combined = perms.map((v) => [fixed, ...v]);
+    result.push(...combined);
+  });
 
-    while (cur !== i) {
-      if (weak[cur] >= weak[i]) {
-        if (weak[cur] - weak[i] > range) {
-          break;
-        }
-      } else {
-        if (n - weak[i] + weak[cur] > range) break;
-      }
-      cnt++;
-      cur = (cur + 1) % weak.length;
-    }
-
-    if (cnt > maxFixCnt) {
-      maxFixCnt = cnt;
-      // 제거할 위크포인트
-      const arr = [];
-      for (let idx = i; idx < cur; ++idx) {
-        arr.push(weak[idx]);
-      }
-      fixed = [...arr];
-    }
-  }
-
-  //     // 반시계방향
-  for (let i = weak.length - 1; i >= 0; --i) {
-    //weak[i] 부터 시작할 경우
-    let cnt = 1;
-    let cur = i + 1;
-
-    while (cur !== i) {
-      // i는 시작점(10)  cur는 9
-      if (weak[cur] <= weak[i]) {
-        if (weak[i] - weak[cur] > range) {
-          break;
-        }
-      } else {
-        // i는 시작점(1) cur는 10
-        if (weak[i] + (n - weak[cur]) > range) break;
-      }
-      cnt++;
-      cur = (cur - 1) % weak.length;
-    }
-
-    if (cnt > maxFixCnt) {
-      maxFixCnt = cnt;
-      const arr = [];
-      for (let idx = cur - 1; idx >= i; --idx) {
-        arr.push(weak[idx]);
-      }
-      fixed = [...arr];
-    }
-  }
-
-  return fixed;
-}
+  return result;
+};
