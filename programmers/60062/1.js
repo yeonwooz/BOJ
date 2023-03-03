300598c89e13a49e1c529b47dbcbed30c4829de4
suyeon woo <woosean999@gmail.com>
2023-03-03 23:22:44 +0900

    [WIP] dist작은 친구부터 시작

--- programmers/60062/1.js
+++ programmers/60062/1.js
@@ -1,15 +1,19 @@
 function solution(n, weak, dist) {
   // 크게 도는 친구부터 각각의 친구가 시계방향 / 반시계 방향으로 최대한 많이
-  dist.sort((a, b) => a - b); // 오름차순 후 pop()
+  dist.sort((a, b) => b - a); // 내림차순 후 pop()
   let weakCnt = weak.length;
   let workerCnt = 0;
   while (dist.length) {
     if (weakCnt <= 0) break;
     const range = dist.pop();
-    const fixedCnt = getMaxFixCnt(n, weak, range);
+    const fixed = getMaxFixCnt(n, weak, range);
+    const fixedCnt = fixed.length;
+    // console.log('range', range, 'fixed', fixed)
     if (fixedCnt > 0) {
       workerCnt++;
       weakCnt -= fixedCnt;
+      weak = weak.filter((_, idx) => !fixed.includes(idx));
+      // console.log(weak)
     }
   }
   return workerCnt;
@@ -17,6 +21,7 @@ function solution(n, weak, dist) {
 
 function getMaxFixCnt(n, weak, range) {
   let maxFixCnt = 0;
+  let fixed = []; //  고쳐진 것
 
   // 시계방향
   for (let i = 0; i < weak.length; ++i) {
@@ -38,6 +43,12 @@ function getMaxFixCnt(n, weak, range) {
 
     if (cnt > maxFixCnt) {
       maxFixCnt = cnt;
+      // 제거할 위크포인트
+      const arr = [];
+      for (let idx = i; idx < cur; ++idx) {
+        arr.push(weak[idx]);
+      }
+      fixed = [...arr];
     }
   }
 
@@ -63,8 +74,13 @@ function getMaxFixCnt(n, weak, range) {
 
     if (cnt > maxFixCnt) {
       maxFixCnt = cnt;
+      const arr = [];
+      for (let idx = cur - 1; idx >= i; --idx) {
+        arr.push(weak[idx]);
+      }
+      fixed = [...arr];
     }
   }
 
-  return maxFixCnt;
+  return fixed;
 }
