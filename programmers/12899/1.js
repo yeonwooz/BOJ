667218a959dc28e87cd9bc617dbcf578fbe381a9
suyeon woo <woosean999@gmail.com>
2023-03-14 12:56:04 +0900

    124 나라의 숫자

---
+++ programmers/12899/1.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  var answer = "";
+  let mod = ["4", "1", "2"];
+
+  while (n > 0) {
+    answer = mod[n % 3] + answer;
+    n = parseInt((n - 1) / 3);
+  }
+  return answer;
+}
