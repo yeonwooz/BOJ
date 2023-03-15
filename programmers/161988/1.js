823e916f653722eb2c1ebd3b0e5ed2f9f3ca00f5
suyeon woo <woosean999@gmail.com>
2023-03-15 10:48:07 +0900

    연속 펄스 부분 수열의 합 - 동적계획법 - 탐색과 동시에 최대값  갱신

--- programmers/161988/1.js
+++ programmers/161988/1.js
@@ -12,13 +12,15 @@ function solution(sequence) {
   dp2[0] = sequence[0] * pulse2;
   pulse2 *= -1;
 
+  let max1 = dp1[0];
+  let max2 = dp2[0];
   for (let i = 1; i < n; ++i) {
     dp1[i] = Math.max(dp1[i - 1], 0) + sequence[i] * pulse1;
     pulse1 *= -1;
-
+    if (dp1[i] > max1) max1 = dp1[i];
     dp2[i] = Math.max(dp2[i - 1], 0) + sequence[i] * pulse2;
     pulse2 *= -1;
+    if (dp2[i] > max2) max2 = dp2[i];
   }
-
-  return Math.max(...dp1, ...dp2);
+  return Math.max(max1, max2);
 }
