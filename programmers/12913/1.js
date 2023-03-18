31b7a30c5615c1707238fba6957d38f725bd646d
suyeon woo <woosean999@gmail.com>
2023-03-18 19:27:57 +0900

    땅따먹기 - DP

--- programmers/12913/1.js
+++ programmers/12913/1.js
@@ -1,26 +1,13 @@
 function solution(land) {
-  let answer = 0;
-  for (let col = 0; col < 4; ++col) {
-    // 0행에서 몇열을 밟고 시작할지
-    const colArr = Array(4).fill(null);
-    search(0, col, colArr, 0);
-  }
-  return answer;
+  const n = land.length;
+  const dp = Array.from(Array(n), () => Array(4).fill(0));
+  dp[0] = [...land[0]];
 
-  function search(row, cur_col, colArr, sum) {
-    if (row === land.length) {
-      if (sum > answer) {
-        answer = sum;
-      }
-      return;
-    }
-    sum += land[row][cur_col];
-    colArr[cur_col] = row;
-
-    for (let col = 0; col < 4; ++col) {
-      // row행에서 몇열을 밟고 시작할지
-      if (col === cur_col) continue;
-      search(row + 1, col, colArr, sum);
+  for (let i = 1; i < n; ++i) {
+    for (let next = 0; next < 4; ++next) {
+      const others = dp[i - 1].filter((_, idx) => idx !== next);
+      dp[i][next] = Math.max(...others) + land[i][next];
     }
   }
+  return Math.max(...dp[n - 1]);
 }
