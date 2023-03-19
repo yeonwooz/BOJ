b7316aaabf72b25fbdd5e0879ec6bea5545faa09
suyeon woo <woosean999@gmail.com>
2023-03-19 12:41:35 +0900

    점 찍기 - 나눗셈으로 변경

--- programmers/140107/1.js
+++ programmers/140107/1.js
@@ -1,9 +1,10 @@
 function solution(k, d) {
   let cnt = 0;
   for (let a = 0; (a * k) ** 2 <= d ** 2; ++a) {
-    for (let b = 0; (a * k) ** 2 + (b * k) ** 2 <= d ** 2; ++b) {
-      cnt++;
-    }
+    const x = a * k;
+    const maxY = Math.floor(Math.sqrt(d ** 2 - x ** 2, 2));
+    const ycnt = Math.floor(maxY / k);
+    cnt += ycnt + 1;
   }
   return cnt;
 }
