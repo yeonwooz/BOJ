48f7d251a428d7d52b44361a33b26549f8d90dc5
suyeon woo <woosean999@gmail.com>
2023-04-02 11:43:16 +0900

    뉴스 클러스터링

--- programmers/17677/1.js
+++ programmers/17677/1.js
@@ -1,11 +1,12 @@
 function solution(str1, str2) {
   if (!str1 && !str2) return 65536;
-
+  str1 = str1.toLowerCase();
+  str2 = str2.toLowerCase();
   let map1 = new Map();
   let map2 = new Map();
 
   for (let i = 0; i < str1.length - 1; ++i) {
-    const s = (str1[i] + str1[i + 1]).toLowerCase();
+    const s = str1[i] + str1[i + 1];
     if (s.match(/[^a-zA-Z]/g)) continue;
     if (map1.has(s)) {
       map1.set(s, map1.get(s) + 1);
@@ -15,7 +16,7 @@ function solution(str1, str2) {
   }
 
   for (let i = 0; i < str2.length - 1; ++i) {
-    const s = (str2[i] + str2[i + 1]).toLowerCase();
+    const s = str2[i] + str2[i + 1];
     if (s.match(/[^a-zA-Z]/g)) continue;
     if (map2.has(s)) {
       map2.set(s, map2.get(s) + 1);
@@ -23,17 +24,22 @@ function solution(str1, str2) {
       map2.set(s, 1);
     }
   }
-  const unionMap = [...new Map([...map1, ...map2])];
 
-  let intserSize = 0;
-  let unionSize = 0;
-  for (const [el, cnt] of unionMap) {
-    unionSize += cnt;
-    if (map1.has(el) && map2.has(el)) {
-      intserSize += Math.min(map1.get(el), map2.get(el));
+  let inter = 0;
+  let union = 0;
+  for (const [el, cnt] of map1) {
+    if (map2.has(el)) {
+      inter += Math.min(cnt, map2.get(el));
+      union += Math.max(cnt, map2.get(el));
+    } else {
+      union += cnt;
     }
   }
 
-  if (intserSize === unionSize) return 65536;
-  return parseInt((intserSize / unionSize) * 65536);
+  for (const [el, cnt] of map2) {
+    if (!map1.has(el)) union += cnt;
+  }
+
+  if (inter === union) return 65536;
+  return Math.floor((inter / union) * 65536);
 }
