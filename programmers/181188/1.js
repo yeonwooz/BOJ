ac01863ab388b71baee990fd4178e088263018ce
suyeon woo <woosean999@gmail.com>
2023-04-15 21:30:41 +0900

    요격 시스템

--- programmers/181188/1.js
+++ programmers/181188/1.js
@@ -1,19 +1,18 @@
 function solution(targets) {
-  targets.sort((a, b) => {
-    if (a[0] < b[0]) return -1;
-    if (a[0] > b[0]) return 1;
-    if (a[1] < b[1]) return -1;
-    return 1;
-  });
-  let cnt = 0;
-  let curPos = -1;
-  for (const target of targets) {
-    if (target[0] < curPos && curPos < target[1]) {
-      curPos = target[1] - 0.1;
-    } else {
-      cnt++;
-      curPos = target[1] - 0.1;
+    targets.sort((a,b) => {
+        if (a[1] < b[1]) return -1
+        if (a[1] > b[1]) return 1
+        if (a[0] < b[0]) return -1
+        return 1
+    })
+    let cnt = 0
+    let curPos = -1
+    for (const target of targets) {
+        const [l,r] = target
+        if (curPos <= l) {
+            cnt++
+            curPos = r
+        }  
     }
-  }
-  return cnt;
+    return cnt
 }
