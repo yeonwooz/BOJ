8b04e33cac5b2d83e950decb1836e06494164f36
suyeon woo <woosean999@gmail.com>
2023-04-16 12:02:46 +0900

    두 원 사이의 정수쌍 - 다른 풀이 참고

--- programmers/181187/1.js
+++ programmers/181187/1.js
@@ -1,29 +1,10 @@
-// https://mentum.tistory.com/735
+// https://school.programmers.co.kr/learn/courses/30/lessons/181187/solution_groups?language=javascript
 function solution(r1, r2) {
-    let cnt = 0
-    for (let x = 1; x <= r2; ++x) {   
-        if (x >= r1) {
-            let y = Math.sqrt(r2 ** 2 - x **2)
-            cnt += Math.floor(y)     
-        } else {
-            let y2 = Math.sqrt(r2 ** 2 - x ** 2)   
-            let y1 = Math.sqrt(r1 ** 2 - x ** 2)   
-            
-            const y2bottom = Math.floor(y2)
-            const y1top = Math.ceil(y1)
-            cnt += y2bottom - y1top + 1
-        }
-    }
-
-    // x = 0
-    let x = 0
-    for (let y = 1; y <= r2; ++y) {
-        if (r1 **2 <= x ** 2 + y ** 2 && x ** 2 + y ** 2 <= r2 **2) {
-            // console.log(x,y)
-            cnt++
-        }        
-    } 
-   
-    cnt *= 4
-    return cnt
+  var answer = 0;
+  for (var x = 1; x <= r2; x++) {
+    var min = r1 >= x ? Math.ceil(Math.sqrt(r1 * r1 - x * x)) : 0;
+    var max = Math.floor(Math.sqrt(r2 * r2 - x * x));
+    answer += max - min + 1;
+  }
+  return 4 * answer;
 }
