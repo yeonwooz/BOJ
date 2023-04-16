47c08b879c5b598f57e123c9108800f4def2dc60
suyeon woo <woosean999@gmail.com>
2023-04-16 11:31:49 +0900

    [WIP2]두 원 사이의 정수 쌍

--- programmers/181187/1.js
+++ programmers/181187/1.js
@@ -1,11 +1,23 @@
 function solution(r1, r2) {
   let cnt = 0;
-  for (let x = -1 * r2; x <= r2; ++x) {
-    for (let y = -1 * r2; y <= r2; ++y) {
+  for (let x = 1; x <= r2; ++x) {
+    for (let y = 1; y <= r2; ++y) {
       if (r1 ** 2 <= x ** 2 + y ** 2 && x ** 2 + y ** 2 <= r2 ** 2) {
+        // console.log(x,y)
         cnt++;
       }
     }
   }
+
+  // x = 0
+  let x = 0;
+  for (let y = 1; y <= r2; ++y) {
+    if (r1 ** 2 <= x ** 2 + y ** 2 && x ** 2 + y ** 2 <= r2 ** 2) {
+      // console.log(x,y)
+      cnt++;
+    }
+  }
+
+  cnt *= 4;
   return cnt;
 }
