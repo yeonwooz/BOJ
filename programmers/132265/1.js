1d7afcf2e60931a5a71ca423cc5268623c6122e1
suyeon woo <woosean999@gmail.com>
2023-04-25 09:51:01 +0900

    WIP

--- programmers/132265/1.js
+++ programmers/132265/1.js
@@ -1,9 +1,18 @@
 function solution(topping) {
-  let cnt = 0;
-  for (let cut = 1; cut <= topping.length; ++cut) {
-    const set1 = new Set(topping.slice(0, cut));
-    const set2 = new Set(topping.slice(cut));
-    if (set1.size === set2.size) cnt++;
-  }
-  return cnt;
+    const n = topping.length
+    const set = new Set(topping)
+    let answer = 0
+    
+    // 가짓수 캐싱하기
+    const leftdp = [0]    // idx를 기준으로 자를때 왼쪽 가짓수
+    const rightdp = [set.size]    // idx를 기준으로 자를때 오른쪽 가짓수
+    
+    const left = Array(n+1).fill(0)
+    const right = Array(n+1).fill(0)
+    
+    for (let cut = 1; cut <= topping.length; ++cut) {
+        left[topping[cut]]++
+        right[topping[cut]]--
+    }
+    return answer
 }
